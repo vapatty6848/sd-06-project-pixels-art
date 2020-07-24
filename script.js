2066a9e313f5e8eae1c0a5cc3969c3ea4698225a
Paulo Afonso Volpin <paulo.volpin@gmail.com>
2020-07-24 12:01:52 -0300

    Task 4: adding the pixel board

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
+//Colors Palette
 let colorPalette = document.querySelector('#color-palette');
 let colorsArray = ['black', 'blue', 'red' , 'green'];
 for (let i = 0; i < 4; i += 1) {
     let colorItem = document.createElement('li');
     colorItem.className = 'color';
     colorPalette.appendChild(colorItem);
-
     colorItem.style.background = colorsArray[i];
     colorItem.style.border = 'solid black 1px';
     colorItem.style.borderRadius = '100%';
     colorItem.style.display = 'inline-block';
     colorItem.style.padding = '2%';
     colorItem.style.margin = '1%';
+    colorItem.style.width = '1px';
+}
+
+//Pixel Board
+let pixelBoard = document.createElement('table');
+pixelBoard.id = 'pixel-board';
+document.querySelector('section').appendChild(pixelBoard);
+pixelBoard.style.display = 'block';
+pixelBoard.style.lineHeight = 0;
+
+for (let row = 0; row < 5; row += 1) {
+    let tableRow = document.createElement('tr');
+    tableRow.style.display = 'block';
+    pixelBoard.appendChild(tableRow);
+   for (let column  = 0; column < 5; column += 1) {
+        let tablePixel = document.createElement('td');
+        tablePixel.className = 'pixel';
+        tablePixel.style.display = 'inline-block';
+        tablePixel.style.border = 'solid black 1px';
+        tablePixel.style.background = 'white';
+        tableRow.appendChild(tablePixel);
+    }
 }
