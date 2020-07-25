a0e2433cd169c399e371a0d8fceb3738704869ca
Havyner Caetano <hrcgeo@gmail.com>
2020-07-25 19:05:21 -0300

    Add selected class to first color option

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let pixelBoard = document.querySelector('#pixel-board');
+let selectedColor = document.querySelector('.color');
 
 for (let i = 0; i < 25; i++) {
   const div = document.createElement('div');
   div.className = 'pixel';
   pixelBoard.appendChild(div);
 }
+
+selectedColor.classList.add('selected');
