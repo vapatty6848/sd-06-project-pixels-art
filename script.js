9b0f1f04b913cfd9540406255a12be6e64fed962
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-07-25 11:56:42 -0300

    Acredito que o HTML e CSS ok, JS já pegando cor

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  let colorPalette = document.querySelector("#color-palette");
+  colorPalette.addEventListener("click", function (event) {
+    console.log(event.target.id)
+  });
+
+
+
+
+}
