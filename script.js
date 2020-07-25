36ec6fd6d26c9e61daa92dffd31dc4cdb7613910
Anderson <and.stuber@gmail.com>
2020-07-25 11:21:25 -0300

    Learned a bounch about VSCode

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function createColorButton(color) {
   const colorBtn = document.createElement('button');
   colorBtn.classList.add('color');
   if (color === 'black') {
-      colorBtn.classList.add('selected');
+    colorBtn.classList.add('selected');
   }
   colorBtn.id = color;
   colorBtn.style.backgroundColor = color;
@@ -27,13 +27,13 @@ function getColor() {
   selected = this.id;
 }
 function createPixelBoard(size) {
-  let name = 1;
-	const totalSize = size * size;
-	let boardWidth = (size * 40) + (size * 2);
-  const pixelBoard = document.getElementById('pixel-board');
+  const totalSize = size * size;
+	let name = 1;
+  const boardWidth = (size * 40) + (size * 2);
+	const pixelBoard = document.getElementById('pixel-board');
   pixelBoard.style.borderStyle = 'solid';
   pixelBoard.style.borderWidth = '2px';
-  pixelBoard.style.width = boardWidth + 'px';
+  pixelBoard.style.width = `${boardWidth}px`;
   for (let i = 0; i < totalSize; i += 1) {
     const pixel = document.createElement('div');
     pixel.classList.add('pixel');
@@ -48,22 +48,22 @@ function clearButton() {
   clearButon.addEventListener('click', clearBoard);
 }
 function clearBoard() {
-  let pixels = document.querySelectorAll('.pixel');
+  const pixels = document.querySelectorAll('.pixel');
   pixels.forEach(function (pixel) {
     pixel.style.backgroundColor = 'white';
   });
 }
 window.onload = function () {
-	// console.log('page loaded')
-	// global variables
-	// selected = 'black';
-	// calls
-
-	colorPalete();
-	createPixelBoard(5);
-	clearButton();
-	getColor();
-	clearBoard();
-	selected = 'black';
+  // console.log('page loaded')
+  // global variables
+  // selected = 'black';
+  // calls
+  getColor();
+  clearBoard();
+  colorPalete();
+  createPixelBoard(5);
+  clearButton();
+  getColor();
+  clearBoard();
+  selected = 'black';
 };
-
