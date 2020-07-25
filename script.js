4e013b98f9addb4537755f22fe1804701265ba95
Havyner Caetano <hrcgeo@gmail.com>
2020-07-25 19:58:17 -0300

    Add eventListener to all color options, create function to hold the clicked color

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let pixelBoard = document.querySelector('#pixel-board');
 let selectedColor = document.querySelector('.color');
+let onClickColor = {};
+const selectedClass = 'selected';
 
 for (let i = 0; i < 25; i++) {
   const div = document.createElement('div');
@@ -7,4 +9,20 @@ for (let i = 0; i < 25; i++) {
   pixelBoard.appendChild(div);
 }
 
-selectedColor.classList.add('selected');
+function addSelectedClass() {
+  selectedColor.classList.add(selectedClass);
+}
+
+for (let i = 0; i < document.querySelectorAll('.color').length; i++) {
+  const currentColorOption = document.querySelectorAll('.color')[i];
+
+  currentColorOption.addEventListener('click', function (event) {
+    const colorOfClickedOption = window.getComputedStyle(event.target, null).getPropertyValue("background-color");
+
+    onClickColor = colorOfClickedOption;
+    selectedColor.classList.remove(selectedClass);
+    selectedColor = currentColorOption;
+    addSelectedClass();
+  })
+}
+
