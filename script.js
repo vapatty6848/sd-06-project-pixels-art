3eef6bcf2facb401b5037b7bf192c6c78cd08bb8
Anderson <and.stuber@gmail.com>
2020-07-25 01:04:55 -0300

    black color auto picked

--- script.js
+++ script.js
@@ -3,18 +3,21 @@ window.onload = function() {
     //console.log('page loaded')
     
     //global variables
-    
+    pickedColor = 'black';
 
     //calls
+
     colorPalete();
     createPixelBoard(5);
+    
 }
+
+
 function colorPalete() {
     let colors = ['black', 'green', 'blue', 'purple']
     for (let i = 0; i < colors.length; i += 1) {
         let color = colors[i];
         createColorButton(color);
-
     }
 }
 function createColorButton(color) {
@@ -27,11 +30,10 @@ function createColorButton(color) {
     colorContainer.appendChild(colorBtn);
 }
 function getColor() {
-    let pickedColor = this.id;
-    console.log(this.id)
+    pickedColor = this.id;
 }
-
 function createPixelBoard(size) {
+    let name = 1;
     let totalSize = size * size;
     let pixelBoard = document.getElementById('pixel-board');
     pixelBoard.style.borderStyle = 'solid';
@@ -40,8 +42,15 @@ function createPixelBoard(size) {
     for (let i = 0; i < totalSize; i += 1) {
         let pixel = document.createElement('div');
         pixel.classList.add('pixel');
+        pixel.id = name;
+        pixel.addEventListener('click', giveColor);
         pixelBoard.appendChild(pixel);
+        name += 1;
     }
+}
+function giveColor() {
+    let clickedPixel = document.getElementById(this.id);
+    clickedPixel.style.backgroundColor = pickedColor;
 }
 
 
