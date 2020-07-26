675a86db43cda29c9574e1f8294267c09665c650
higoramorim <1810042@aluno.univesp.br>
2020-07-26 12:16:01 -0300

    alterado index script style

---
+++ script.js
@@ -0,0 +1,27 @@
+window.onload = function(){
+    //catchCorPaleta
+    const catchCorPaletaColor1 = document.querySelector("#color-1");
+    const catchCorPaletaColor2 = document.querySelector("#color-2");
+    const catchCorPaletaColor3 = document.querySelector("#color-3");
+    const catchCorPaletaColor4 = document.querySelector("#color-4");
+    const catchButton = document.querySelector("#clear-board");
+    const catchPixel = document.getElementsByClassName("pixel");
+
+    catchCorPaletaColor1.addEventListener('click', function(){
+        //codigo
+    });
+    catchCorPaletaColor2.addEventListener('click', function(){
+        //codigo
+    });
+    catchCorPaletaColor3.addEventListener('click', function(){
+        //codigo
+    });
+    catchCorPaletaColor4.addEventListener('click', function(){
+        //codigo
+    });
+    catchButton.addEventListener('click', function(){
+        catchPixel.style.backgroundColor = "white";
+    });
+
+
+}
