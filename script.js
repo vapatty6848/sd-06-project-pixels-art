de8b405589ef7288bfa9312a459067eaf2082aa7
Elias Carvalho <eliascarvalho01@hotmail.com>
2020-07-25 21:41:15 -0300

    Fixing codeclimate

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-let DQSEL = (params) => { return document.querySelector(params); };
+const DQSEL = function (params) { return document.querySelector(params); };
+const divBoard = DQSEL('.board');
+const divPalette = DQSEL('#color-palette');
+const btnReset = DQSEL('.btn-reset');
+let divWithSelection = {};
 
 window.onload = () => {
   divWithSelection = DQSEL('#black');
   divWithSelection.classList.add('selected');
 };
 
-const divBoard = DQSEL('.board');
-const divPalette = DQSEL('#color-palette');
-const btnReset = DQSEL('.btn-reset');
-let divWithSelection = {};
-
 function getCurrentColor() {
   return divWithSelection.style.backgroundColor;
 }
 
 divPalette.addEventListener('click', (e) => {
-  divName = e.target;
+  let divName = e.target;
   divWithSelection.classList.remove('selected');
   divName.classList.add('selected');
   divWithSelection = divName;
 });
 
 divBoard.addEventListener('click', (e) => {
-  let clicked = e.target;
-  clicked.style.backgroundColor = getCurrentColor();
+  e.target.style.backgroundColor = getCurrentColor();
 });
 
 btnReset.addEventListener('click', () => {
-  let pixel = document.querySelectorAll('.pixel');
+  const pixel = document.querySelectorAll('.pixel');
 
-  for (p in pixel) {
-    pixel[p].style.backgroundColor = 'white';
-  }
+  pixel.forEach((p) => {
+    p.style.backgroundColor = 'white';
+  });
 });
