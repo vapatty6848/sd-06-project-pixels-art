5d17149d5ab3a8a000e81152dd6e50ed6420a806
Igor Leão <igorleao.eng@gmail.com>
2020-07-26 16:07:34 -0300

    teste

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const selectColor = document.getElementById('black');
 const selectColor = document.getElementById('red');
 const selectColor = document.getElementById('blue');
 const selectColor = document.getElementById('green');
-  selectColor.addEventListener("click", function() {
+  selectColor.addEventListener('click', function() {
       optColor(selectColor)
 
-  })
+  });
   function defineColor(pixel) {
     pixel.addEventListener('click', function () {
       pixel.style.backgroundColor = selectColor;
