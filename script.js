12778dc1f59947775d7ee8e8cdf87b1414b22a72
Dilenio Enderle <dilenio@gmail.com>
2020-07-26 16:11:37 -0300

    Code Climate issues #1

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 // Starting Pixel Art
-let btnColorBlack = document.querySelector(".black");
-btnColorBlack.addEventListener("click", function () {
-  addColorSelected("black");
-});
-
-let btnColorRed = document.querySelector(".red");
-btnColorRed.addEventListener("click", function () {
-  addColorSelected("red");
-});
-
-let btnColorGreen = document.querySelector(".green");
-btnColorGreen.addEventListener("click", function () {
-  addColorSelected("green");
-});
-
-let btnColorBlue = document.querySelector(".blue");
-btnColorBlue.addEventListener("click", function () {
-  addColorSelected("blue");
-});
-
 function addColorSelected(color) {
-  removeColorSelected();
-  let element = document.querySelector("." + color);
-  element.className = "color " + color + " selected";
+  const point = '.';
+  let element = document.querySelector('.selected');
+  element.classList.remove('selected');
+  let element = document.querySelector(point + color);
+  element.className = 'color ' + color + ' selected';
 }
 
-function removeColorSelected() {
-  let element = document.querySelector(".selected");
-  element.classList.remove("selected");
-}
+const btnColorBlack = document.querySelector('.black');
+btnColorBlack.addEventListener('click', function () {
+  addColorSelected('black');
+});
+
+const btnColorRed = document.querySelector('.red');
+btnColorRed.addEventListener('click', function () {
+  addColorSelected('red');
+});
+
+const btnColorGreen = document.querySelector('.green');
+btnColorGreen.addEventListener('click', function () {
+  addColorSelected('green');
+});
+
+const btnColorBlue = document.querySelector('.blue');
+btnColorBlue.addEventListener('click', function () {
+  addColorSelected('blue');
+});
