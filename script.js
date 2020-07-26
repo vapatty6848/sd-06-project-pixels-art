c32e057a4e7e15e632c09e34553df5e2d35cb7e3
rafMewtwo <ponciano.raf@gmail.com>
2020-07-26 15:56:55 -0300

    testando functions

--- script.js
+++ script.js
@@ -4,23 +4,18 @@ window.onload = function(){
    
     let divGrande = document.getElementById("pixel-board")
     let divLinha = document.getElementsByClassName("row")
+    
+
+    //limpa tela
     buttonClear.addEventListener("click", function(){
 
         
         arrayOfPixels = document.querySelectorAll('.pixel');
-        for (let i = 0; i < arrayOfPixels.length; i += 1) {
-        arrayOfPixels[i].style.background = "rgb(255,255,255)";
-    }
-
-
-        divGrande.style.backgroundColor = "rgb(255,255,255)"
-        for(i=0;i<5;i++){
-        divLinha[i].style.backgroundColor = "rgb(255,255,255)"
-    }
-        
-    
-        
-
+        for(i=0;i<arrayOfPixels.length;i++){
+            arrayOfPixels[i] = arrayOfPixels[i].classList.remove("select")
+        }
+      
     })
+       
 
 }
