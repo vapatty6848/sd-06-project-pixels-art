b58dd7fa2546796b6e514dd99feb57f74d8c4e14
Rods <rods.leite27@gmail.com>
2020-07-25 21:29:14 -0300

    Requisito 6 e 7

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-const pixels = document.querySelectorAll('.pixel');
-const colors = document.querySelectorAll('.color');
-let corSelecionada = 'black';
-for (let i = 0; i < pixels.length; i+= 1) {
-    pixels[i].addEventListener('click', function () {
-        pixels[i].style.backgroundColor = corSelecionada;
-    });
+window.onload = function(){
+    const pixels = document.querySelectorAll('.pixel');
+    const colors = document.querySelectorAll('.color');
+    let corSelecionada = 'black';
+    
+    for (let i = 0; i < pixels.length; i+= 1) {
+        pixels[i].addEventListener('click', function () {
+            pixels[i].style.backgroundColor = corSelecionada;
+        });
+    }
+
+    for (let i = 0; i < colors.length; i+= 1) {
+        let cor = colors[i]
+        cor.addEventListener('click', function(){
+            if(!cor.classList.contains('selected')){
+                for (let j = 0; j < colors.length; j=+ 1) {
+                    colors[i].classList.remove('selected');
+                }
+                cor.className += ' selected'
+                corSelecionada = colors[color].classList[1];
+            }
+        })
+    }  
+    console.log(colors.classList.entries())
 }
 
-/*/for (let i = 0; i < colors.length; i+= 1) {
-    colors[i].addEventListener('click', function () {
-        corSelecionada = colors[i].style.backgroundColor
-    })
-    
-}/*/
+
