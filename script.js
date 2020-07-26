88144fe6f20df5c9890b502d9b8a1aa22f9aa11d
Cássio Rubens <cassiorubensadv@gmail.com>
2020-07-25 22:45:47 -0300

    Ajustes Desafios

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-let corSelecionada = "";
+let corSelecionada = '';
 
-let idBlack = document.getElementById('black');
+const idBlack = document.getElementById('black');
 idBlack.addEventListener('click', function () {
   corSelecionada = 'black';
 });
 
-let idRed = document.getElementById('red');
+const idRed = document.getElementById('red');
 idRed.addEventListener('click', function () {
   corSelecionada = 'red';
 });
 
-let idBlue = document.getElementById('blue');
+const idBlue = document.getElementById('blue');
 idBlue.addEventListener('click', function () {
   corSelecionada = 'blue';
 });
 
-let idGreen = document.getElementById('green');
+const idGreen = document.getElementById('green');
 idGreen.addEventListener('click', function () {
   corSelecionada = 'green';
 });
 
-let arrayPixel = document.getElementsByClassName('pixel');
+const arrayPixel = document.getElementsByClassName('pixel');
 for (let i = 0; i < arrayPixel.length; i += 1) {
   arrayPixel[i].addEventListener('click', function () {
     arrayPixel[i].style.backgroundColor = corSelecionada;
