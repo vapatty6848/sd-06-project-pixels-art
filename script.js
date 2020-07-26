0ffe79b6e8b2b36031e4c8071e66fb95d92df48b
rafMewtwo <ponciano.raf@gmail.com>
2020-07-26 04:34:05 -0300

    consertando o botao

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 window.onload = function(){
 
-    let buttonClear = document.getElementById("clear")
+    let buttonClear = document.getElementById("clear-board")
    
 
     buttonClear.addEventListener("click", function(){
 
-        //document.querySelector(".pixel").style.backgroundColor = "red"
+        
         arrayOfPixels = document.querySelectorAll('.pixel');
         for (let i = 0; i < arrayOfPixels.length; i += 1) {
         arrayOfPixels[i].style.background = 'white';
