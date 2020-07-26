34a749e9c0d33bc3f079c87cf60e6709c32e5f80
Igor Leão <igorleao.eng@gmail.com>
2020-07-26 15:41:42 -0300

    html

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-const selectColor =document.getElementById("black");
-const selectColor =document.getElementById("red");
-const selectColor =document.getElementById("blue");
-const selectColor =document.getElementById("green");
- selectColor.addEventListener("click", function() {
-     optColor(selectColor)
+const selectColor = document.getElementById('black');
+const selectColor = document.getElementById('red');
+const selectColor = document.getElementById('blue');
+const selectColor = document.getElementById('green');
+  selectColor.addEventListener("click", function() {
+      optColor(selectColor)
 
- })
- function defineColor(pixel) {
-    pixel.addEventListener("click", function () {
+  })
+  function defineColor(pixel) {
+    pixel.addEventListener('click', function () {
       pixel.style.backgroundColor = selectColor;
     });
   }
 
- function optColor() {
-    const selectorDiv = document.getElementsByTagName(".selected")[0];
-    selectorDiv.classList.remove("selected");
-    div.classList.add("selected");
+  function optColor(div) {
+    const selectorDiv = document.getElementsByClassName('selected')[0];
+    selectorDiv.classList.remove('selected');
+    div.classList.add('selected');
 
- }
- 
+  }
