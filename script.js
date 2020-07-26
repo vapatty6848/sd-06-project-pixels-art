71d2c894b8c6d16cf15afd9af79ecdb2d79d5437
Dilenio Enderle <dilenio@gmail.com>
2020-07-26 16:04:00 -0300

    7 - Ao clicar em uma das cores da paleta, a cor selecionada é que vai ser usada para preencher os pixels no quadro.

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 // Starting Pixel Art
+let btnColorBlack = document.querySelector(".black");
+btnColorBlack.addEventListener("click", function () {
+  addColorSelected("black");
+});
+
+let btnColorRed = document.querySelector(".red");
+btnColorRed.addEventListener("click", function () {
+  addColorSelected("red");
+});
+
+let btnColorGreen = document.querySelector(".green");
+btnColorGreen.addEventListener("click", function () {
+  addColorSelected("green");
+});
+
+let btnColorBlue = document.querySelector(".blue");
+btnColorBlue.addEventListener("click", function () {
+  addColorSelected("blue");
+});
+
+function addColorSelected(color) {
+  removeColorSelected();
+  let element = document.querySelector("." + color);
+  element.className = "color " + color + " selected";
+}
+
+function removeColorSelected() {
+  let element = document.querySelector(".selected");
+  element.classList.remove("selected");
+}
