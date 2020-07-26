bfb315a3254ef3bcc4aca269081e3ec2605bd3fc
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-07-26 20:36:03 -0300

    Adding firstrequirement check

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 window.onload = function () {
+  
+  let colors = document.getElementById("color-palette").childNodes
+
+  let colorBlack = colors[1]
+  colorBlack.style.backgroundColor = "black"
+
+  let colorGreen = colors[3]
+  colorGreen.style.backgroundColor = "green"
+
+  let colorBlue = colors[5]
+  colorBlue.style.backgroundColor = "blue"
+
+  let colorRed = colors[7]
+  colorRed.style.backgroundColor = "red"
+
 }
