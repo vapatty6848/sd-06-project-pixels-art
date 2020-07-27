07a1dec55822ed96d338cfa15e3add33f1dab366
Rods <rods.leite27@gmail.com>
2020-07-27 17:38:32 -0300

    atualizacoes nas variaveis

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-window.onload = function(){
+window.onload = function () {
     const pixels = document.querySelectorAll('.pixel');
     const colors = document.querySelectorAll('.color');
-    let corSelecionada = 'black';
-    
-    for (let i = 0; i < pixels.length; i+= 1) {
-        pixels[i].addEventListener('click', function () {
-            pixels[i].style.backgroundColor = corSelecionada;
-        });
+    let = corSelecionada = 'black';
+    for(let pixel = 0; pixel < pixels.length; pixel += 1){
+      pixels[pixel].addEventListener('click', function () {
+        pixels[pixel].style.backgroundColor = corSelecionada;
+      });
     }
-
-    for (let i = 0; i < colors.length; i+= 1) {
-        let cor = colors[i]
-        cor.addEventListener('click', function(){
-            if(cor.classList.contains('selected')){
-                for (let j = 0; j < colors.length; j=+ 1) {
-                    colors[i].classList.remove('selected');
-                }
-                cor.className += ' selected'
-                corSelecionada = colors[color].classList[1];
-            }
-        })
-    }  
-}
+    for(let color = 0; color < colors.length; color += 1){
+      let cores = colors[color]; 
+      cores.addEventListener('click', function () {
+        if(cores.classList.contains('selected')){
+          for (let i = 0; i < colors.length; i++) {
+            colors[i].classList.remove('selected');
+          }
+          cores.className += ' selected';
+          corSelecionada = colors[color].classList[1];
+        }
+      }) 
+    }
+  }
 
 
