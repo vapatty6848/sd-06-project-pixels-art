b239054ed914004acfef67dbcced9d1fe97f8cf9
ricknois <rickssti@outlook.com>
2020-07-27 15:57:11 -0300

    pixel 40 px v2

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 const pixelBoard = document.querySelector('#pixel-board');
-const colorPalet =  document.querySelector('#color-palette');
+const colorPalet = document.querySelector('#color-palette');
 const teste = document.getElementsByTagName('div')
-let classSel = 'preto'; 
+let classSel = 'preto';
 
 function createPixelBoard() {
-for (let i = 0; i < 25; i+=1) {
+  for (let i = 0; i < 25; i += 1) {
     let create = document.createElement('span')
     create.setAttribute('class', 'pixel bgbranco')
-    pixelBoard.appendChild(create);   
-}   
+    pixelBoard.appendChild(create);
+  }
 }
 
 colorPalet.addEventListener('click', function (event) {
-     classSel = event.target.classList[1]
-    for (const j of document.getElementsByTagName('div')) {
-        if (j.classList.contains('selected')) {
-            j.classList.remove('selected')
-        }
+  classSel = event.target.classList[1]
+  for (const j of document.getElementsByTagName('div')) {
+    if (j.classList.contains('selected')) {
+      j.classList.remove('selected')
     }
-     event.target.classList.add('selected')
+  }
+  event.target.classList.add('selected')
 })
 
 pixelBoard.addEventListener('click', function (event) {
-    if(event.target.classList.contains('pixel')){
-        event.target.className = "";
-    }
-    event.target.classList.add('pixel')
-    event.target.classList.add(classSel)
+  if (event.target.classList.contains('pixel')) {
+    event.target.className = "";
+  }
+  event.target.classList.add('pixel')
+  event.target.classList.add(classSel)
 })
