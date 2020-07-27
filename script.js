6b78ed47253dd6377b2018e231ca7406a45d05a7
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-07-26 23:06:48 -0300

    Adding new requirement check :)

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 window.onload = function () {
   
-  let colors = document.getElementById("color-palette").childNodes
+  let colors = document.getElementById  ("color-palette").childNodes;
+  
+  let selected = document.getElementsByClassName("selected")
 
   let colorBlack = colors[1]
-  colorBlack.style.backgroundColor = "black"
+  colorBlack.className = "selected"
+  colorBlack.style.backgroundColor = "black";
 
-  let colorGreen = colors[3]
-  colorGreen.style.backgroundColor = "green"
+  console.log(selected.length)
+  let getColorRGB = colorBlack.style.backgroundColor
 
-  let colorBlue = colors[5]
-  colorBlue.style.backgroundColor = "blue"
+  let colorGreen = colors[3];
+  colorGreen.style.backgroundColor = "green";
 
-  let colorRed = colors[7]
-  colorRed.style.backgroundColor = "red"
+  let colorBlue = colors[5];
+  colorBlue.style.backgroundColor = "blue";
 
-}
+  let colorRed = colors[7];
+  colorRed.style.backgroundColor = "red";
+
+  document.addEventListener('click', function (event) {
+    if (event.target.classList.contains ('color') && selected.length >= 1) {
+      console.log(selected)
+      selected[0].className = "color";
+      getColorRGB = event.target.style.backgroundColor
+      event.target.className = "selected";
+      console.log(selected.length)
+      console.log(getColorRGB)
+    } else if (event.target.classList.contains ('pixel') && selected.length === 1) {
+      event.target.style.backgroundColor = getColorRGB
+    }
+  });
+
+};
