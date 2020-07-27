9d8d3ca2f762974ba199159f8ce75412cc848263
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-07-27 00:25:57 -0300

    Adding test again

--- script.js
+++ script.js
@@ -3,48 +3,48 @@ window.onload = function () {
   let colors = document.getElementById  ("color-palette").childNodes;
 
   let colorBlack = colors[1]
-  colorBlack.style.backgroundColor = "black";
-  colorBlack.className = "selected"
+  colorBlack.style.backgroundColor = 'rgb(0, 0, 0)';
+  colorBlack.className = 'selected';
 
   let getColorRGB = colorBlack.style.backgroundColor
 
   let colorGreen = colors[3];
-  colorGreen.style.backgroundColor = "green";
+  colorGreen.style.backgroundColor = 'green';
 
   let colorBlue = colors[5];
-  colorBlue.style.backgroundColor = "blue";
+  colorBlue.style.backgroundColor = 'blue';
 
   let colorRed = colors[7];
-  colorRed.style.backgroundColor = "red";
+  colorRed.style.backgroundColor = 'red';
 
   let colorYellow = colors[9];
-  colorYellow.style.backgroundColor = "yellow";
+  colorYellow.style.backgroundColor = 'yellow';
 
-  let selected = document.getElementsByClassName("selected")
+  let selected = document.getElementsByClassName('selected')
 
   document.addEventListener('click', function (event) {
     if (event.target.classList.contains ('color') && selected.length >= 1) {
       console.log(selected);
-      selected[0].className = "color";
+      selected[0].className = 'color';
       getColorRGB = event.target.style.backgroundColor;
-      event.target.className = "selected";
+      event.target.className = 'selected';
       console.log(selected.length);
       //console.log(getColorRGB);
     } else if (event.target.classList.contains ('pixel') && selected.length === 1) {
       event.target.style.backgroundColor = getColorRGB;
       //console.log(event.target)
     } else if (event.target.classList.contains ('pixel') && selected.length === 0) {
-      event.target.style.backgroundColor = "black";
+      event.target.style.backgroundColor = 'black';
     };
   });
 
-  let clearButton = document.querySelector("#clear-board");
+  let clearButton = document.querySelector('#clear-board');
 
-  let pixel = document.getElementsByClassName("pixel");
+  let pixel = document.getElementsByClassName('pixel');
 
   clearButton.addEventListener("click", function () {
     for (i = 0; i < pixel.length; i += 1)
-    pixel[i].style.backgroundColor = "white"
+    pixel[i].style.backgroundColor = 'white'
   })
 
 };
