26332d412f131c04dbdc499431e057098e4f54ff
higoramorim <1810042@aluno.univesp.br>
2020-07-26 21:55:08 -0300

    baby steps

--- script.js
+++ script.js
@@ -5,23 +5,34 @@ window.onload = function(){
     const catchCorPaletaColor3 = document.querySelector("#color-3");
     const catchCorPaletaColor4 = document.querySelector("#color-4");
     const catchButton = document.querySelector("#clear-board");
-    const catchPixel = document.getElementsByClassName("pixel");
+    const catchPixel = document.querySelector(".pixel");
 
     catchCorPaletaColor1.addEventListener('click', function(){
-        //codigo
+        catchPixel.style.backgroundColor = "black";
     });
     catchCorPaletaColor2.addEventListener('click', function(){
-        //codigo
+        catchPixel.style.backgroundColor = "blueviolet";
     });
     catchCorPaletaColor3.addEventListener('click', function(){
-        //codigo
+        catchPixel.style.backgroundColor = "chartreuse";
     });
     catchCorPaletaColor4.addEventListener('click', function(){
-        //codigo
+        catchPixel.style.backgroundColor = "darkgoldenrod";
     });
     catchButton.addEventListener('click', function(){
         catchPixel.style.backgroundColor = "white";
     });
+
+
+    capturaClique.addEventListener('click', function(){
+        catchCorPaletaColor1.nodeValue
+    })
+    catchCorPaletaColor1.addEventListener('click', function(){
+        catchPixel.style.backgroundColor = "black";
+    });
+
+    
+
 
 
 }
