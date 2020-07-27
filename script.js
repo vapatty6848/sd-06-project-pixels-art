d8be307ca5ae6d75cc2d060240def7a71c5f9248
Julia Porto <juportob@gmail.com>
2020-07-27 13:34:03 -0300

    Nova tentativa botao

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ let corBlack = document.getElementById("black")
 corBlack.classList.add("selected")
 const cores = document.querySelectorAll(".color")
 const pixel = document.querySelectorAll(".pixel")
+const pixelQuadro = document.querySelector("#pixel-board")
 let botaoApagar = document.querySelector("#clear-board")
 
 botaoApagar.addEventListener("click", function () {
-  pixel.style.backgroundColor = "white";
+  pixelQuadro.style.backgroundColor = "white";
 })
 }
