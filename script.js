1b861af8aebe842f6bc0401aa8a6755883186468
Julia Porto <juportob@gmail.com>
2020-07-27 13:11:48 -0300

    Add parte 1 requisito 7

--- script.js
+++ script.js
@@ -2,5 +2,14 @@ window.onload = function () {
 
 let corBlack = document.getElementById("black")
 corBlack.classList.add("selected")
+const cores = document.querySelectorAll(".color")
 
+function addSelectedClass(cor, div) {
+  corBlack = cor;
+  const selecionado = document.getElementsByClassName('selected')[0];
+  selecionado.classList.remove('selected');
+  div.classList.add('selected');
+
+}
+return addSelectedClass;
 }
