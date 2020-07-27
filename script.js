cae634949a721e3f4922d60cb171418f61c01f17
Rods <rods.leite27@gmail.com>
2020-07-27 20:18:08 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-window.onload = function () {
-    const pixels = document.querySelectorAll('.pixel');
-    const colors = document.querySelectorAll('.color');
-    let = corSelecionada = 'black';
-    for(let pixel = 0; pixel < pixels.length; pixel += 1){
-      pixels[pixel].addEventListener('click', function () {
-        pixels[pixel].style.backgroundColor = corSelecionada;
-      });
-    }
-    for(let color = 0; color < colors.length; color += 1){
-      let cores = colors[color]; 
-      cores.addEventListener('click', function () {
-        if(cores.classList.contains('selected')){
-          for (let i = 0; i < colors.length; i++) {
-            colors[i].classList.remove('selected');
-          }
-          cores.className += ' selected';
-          corSelecionada = colors[color].classList[1];
-        }
-      }) 
-    }
-  }
+const pixels = document.querySelectorAll('.pixel');
+const colors = document.querySelectorAll('.color');
+let = corSelecionada = 'black';
+colors[0].classList.add('selected');
+
+for (let cor = 0; cor < colors.length; cor++) {
+  colors[cor].addEventListener('click', function () {
+   for (let i = 0; i < colors.length; i++) {
+    colors[i].classList.remove('selected')
+   }
+    colors[cor].classList.add('selected')
+    corSelecionada = colors[cor].id
+  })
+}
+
+for(let pixel = 0; pixel < pixels.length; pixel += 1){
+  pixels[pixel].addEventListener('click', function () {
+    pixels[pixel].style.backgroundColor = corSelecionada;
+  });
+}
+
+
+
+
+
 
 
