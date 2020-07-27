8164edfee8d34e113c7e0be5ea45c6bc31922011
Julia Porto <juportob@gmail.com>
2020-07-27 08:12:41 -0300

    Add } que estava faltando no script

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ varPixel.addEventListener("click", function () {
 
 })
 
-// function btnClick() {    
+// function btnClick()     
 //   var x = document.getElementById("mytable").getElementsByTagName("td");
 //   x[0].innerHTML = "i want to change my cell color";
 //   x[0].style.backgroundColor = "yellow";
-// }
+}
