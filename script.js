815a6ffac4333678cef1c37653c7061ba1aa0a00
higoramorim <1810042@aluno.univesp.br>
2020-07-26 22:41:42 -0300

    baby steps 3

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-window.onload = function(){
-    //catchCorPaleta
+window.onload = function() {
   const CorPaletaColor1 = document.querySelector('#color-1');
   const CorPaletaColor2 = document.querySelector('#color-2');
   const CorPaletaColor3 = document.querySelector('#color-3');
@@ -7,23 +6,19 @@ window.onload = function(){
   const Button = document.querySelector('#clear-board');
   const Pixel = document.querySelector('.pixel');
 
-  CorPaletaColor1.addEventListener('click', function(){
+  CorPaletaColor1.addEventListener('click', function() {
     Pixel.style.backgroundColor = 'black';
   });
-  CorPaletaColor2.addEventListener('click', function(){
+  CorPaletaColor2.addEventListener('click', function() {
     Pixel.style.backgroundColor = 'blueviolet';
   });
-  CorPaletaColor3.addEventListener('click', function(){
+  CorPaletaColor3.addEventListener('click', function() {
     Pixel.style.backgroundColor = 'chartreuse';
   });
-  CorPaletaColor4.addEventListener('click', function(){
+  CorPaletaColor4.addEventListener('click', function() {
     Pixel.style.backgroundColor = 'darkgoldenrod';
   });
-  Button.addEventListener('click', function(){
+  Button.addEventListener('click', function() {
     Pixel.style.backgroundColor = 'white';
   });
-
-
-    
-
 }
