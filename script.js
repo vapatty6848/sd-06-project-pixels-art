b60c1fa49fc30bf3c3eff6466a237c0b5819c3a8
rafMewtwo <ponciano.raf@gmail.com>
2020-07-27 18:09:27 -0300

    testando novo metodo

--- script.js
+++ script.js
@@ -1,21 +1,64 @@
 window.onload = function(){
 
-    let buttonClear = document.getElementById("clear-board")
-   
-    let divGrande = document.getElementById("pixel-board")
-    let divLinha = document.getElementsByClassName("row")
-    
+createColorPallet(['black', 'red', 'blue', 'green']);
 
-    //limpa tela
-    buttonClear.addEventListener("click", function(){
+function createColorPallet(colors){
+    //recupera a pallet
+    let colorPaletContainer = document.getElementById("color-palet")
+    //add as cores
+    for (let index in colors){
+        let palletItemDiv = createPalletItem(colors[index]);
+        colorPaletContainer.appendChild(palletItemDiv);
 
-        
-        arrayOfPixels = document.querySelectorAll('.pixel');
-        for(i=0;i<arrayOfPixels.length;i++){
-            arrayOfPixels[i] = arrayOfPixels[i].classList.remove("select")
+    }
+
+}
+//criar as divs p cada array add no array de cores
+function createPalletItem(color){
+    let palletItemDiv = document.createElement('div');
+    palletItemDiv.style.backgroundColor = color;
+    palletItemDiv.className = "pallet-item";
+    palletItemDiv.addEventListener("click", handlePalletItemEvent);
+
+    // palletItemDiv.addEventListener("click", function(event){
+
+    // })
+    if (color ==="black"){
+        palletItemDiv.classList.add("selected")
+    }
+    return palletItemDiv;
+
+}
+
+function handlePalletItemEvent(event){
+    let oldSelectedDiv = document.querySelector(".selected")
+    let currentSelectedDiv = event.target;
+
+    oldSelectedDiv.classList.remove("selected")
+    currentSelectedDiv.classList.add("selected")
+    // let div = event.target
+    // let backgroundColor = div.style.backgroundColor;
+    // console.log(backgroundColor);
+
+}
+
+let elements = document.getElementsByClassName("pixel");
+        for (var i = 0; i <elements.length;i++){
+            elements[i].onclick = function(){
+                this.style.backgroundColor = document.querySelector(".selected").style.backgroundColor
+            };
         }
-      
-    })
-       
 
+    let buttonCLear = document.getElementById("clear-board")
+    buttonCLear.addEventListener("click", clearBoard)
+
+
+   function clearBoard(){
+        let div = document.getElementsByClassName("pixel")
+        for(let i =0; i<25;i++){
+            div[i].style.backgroundColor = "white"
+
+       }
+
+    }
 }
