7e57bc4f98ef502029cfeae5747940ef335b12b9
ricknois <rickssti@outlook.com>
2020-07-27 18:20:52 -0300

    Gerador de pixel dinamico

--- script.js
+++ script.js
@@ -4,11 +4,22 @@ const teste = document.querySelectorAll('.color');
 let classSel = 'preto';
 
 function createPixelBoard() {
-  for (let i = 0; i < 25; i += 1) {
-    let create = document.createElement('div')
-    create.setAttribute('class', 'pixel bgbranco')
-    pixelBoard.appendChild(create);
+  
+    for (let j = 0; j < 5; j+=1) {
+      let createSection = document.createElement('section');
+      createSection.setAttribute('class', 'sectionPB');
+      pixelBoard.appendChild(createSection);    
   }
+  
+  const sect = document.getElementsByClassName('sectionPB');
+    for (let l = 0; l < 5; l++) {
+        for (let i = 0; i < 5; i += 1) {
+            let createDiv = document.createElement('div')
+            createDiv.setAttribute('class', 'pixel bgbranco')
+            sect[l].appendChild(createDiv);
+          } 
+    }
+      
 }
 
 colorPalet.addEventListener('click', function (event) {
