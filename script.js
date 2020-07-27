bbfe21a8e7b38562a500cc43aa14476fd369b955
fefarina <wfelipefarina@gmail.com>
2020-07-26 22:10:11 -0300

    Update all files

--- script.js
+++ script.js
@@ -1,53 +1,54 @@
 let pixel = document.querySelectorAll('.pixel');
+let color = document.querySelectorAll('.color');
 
 
+window.onload = selectedColor();
 function selectedColor() {
-    let color = document.querySelectorAll('.color');
     let selected = document.querySelector('selected');
     for (let i = 0; i < color.length; i++) {
         color[0].classList.add('selected')
-        color[i].addEventListener('click', function (e) {
+        color[i].addEventListener('click', function () {
             for (let index = 0; index < color.length; index++) {
                 if (color[index].classList.contains('selected')) {
                     color[index].classList.remove('selected')
                 } else {
                     color[i].classList.add('selected');
                 }
-                for (let i = 0; i < pixel.length; i ++) {
-                    pixel[i].addEventListener('click', function() {
-                        if(color[0].classList.contains('selected')) {
-                            pixel[i].classList.add('color1')
-                        }else if (color[1].classList.contains('selected')) {
-                            pixel[i].classList.add('color2')
-                        } else if(color[2].classList.contains('selected')){
-                            pixel[i].classList.add('color3')
+                for (let i = 0; i < pixel.length; i++) {
+                    pixel[i].addEventListener('click', function () {
+                        if (color[0].classList.contains('selected')) {
+                            pixel[i].className = 'pixel color1'
+                        } else if (color[1].classList.contains('selected')) {
+                            pixel[i].className = 'pixel color2'
+                        } else if (color[2].classList.contains('selected')) {
+                            pixel[i].className = 'pixel color3';
                         } else {
-                            pixel[i].classList.add('color4')
+                            pixel[i].className = 'pixel color4';
                         }
                     })
                 }
             }
-           
+
         });
     };
-       
+
 };
 
 
 
 
 
-// // Resetando a pagina para pixels brancos
+// Resetando a pagina para pixels brancos
 
 // function whitePixels() {
 //     for (let i = 0; i < pixel.length; i++) {
-//         pixel[i].style.backgroundColor = 'white'
+//         pixel[i].style.backgroundColor = 'white';
 //     }
 // }
 
 // window.onload = whitePixels();
 
-// ////
+////
 
 
 // Criando botao ClearBoard
