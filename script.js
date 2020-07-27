98dc04b98c3baeff9de601eb17ad3d74de1a8c3e
thalesgr <thalesgr@gmail.com>
2020-07-27 12:21:02 -0300

    acertando html e css para o evaluator

---
+++ script.js
@@ -0,0 +1,13 @@
+// let palette = document.querySelectorAll('.color');
+
+
+// console.log(palette)
+
+
+// const pixelBoard = document.querySelectorAll('.pixel');
+// console.log(pixelBoard)
+// pixelBoard.forEach(pixel => {
+//     pixel.addEventListener('click', () => {
+//         paint
+//     });
+// })
