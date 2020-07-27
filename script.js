0666c828df5d02f84fef58925f4d8c6be37cba94
higoramorim <1810042@aluno.univesp.br>
2020-07-27 19:16:16 -0300

    js button applied

--- script.js
+++ script.js
@@ -1,49 +1,38 @@
-window.onload = function() {
-  const CorPaletaColor1 = document.querySelector('#color-1');
-  const CorPaletaColor2 = document.querySelector('#color-2');
-  const CorPaletaColor3 = document.querySelector('#color-3');
-  const CorPaletaColor4 = document.querySelector('#color-4');
-  const Button = document.querySelector('#clear-board');
-  const Pixel = document.querySelector('.pixel');
-  const Pixel1 = document.querySelector('#pixel-1');
-  const Pixel2 = document.querySelector('#pixel-2');
-  const Pixel3 = document.querySelector('#pixel-3');
-  const Pixel4 = document.querySelector('#pixel-4');
-  const Pixel5 = document.querySelector('#pixel-5');
-  const Pixel6 = document.querySelector('#pixel-6');
-  const Pixel7 = document.querySelector('#pixel-7');
-  const Pixel8 = document.querySelector('#pixel-8');
-  const Pixel9 = document.querySelector('#pixel-9');
-  const Pixel10 = document.querySelector('#pixel-10');
-  const Pixel11 = document.querySelector('#pixel-11');
-  const Pixel12 = document.querySelector('#pixel-12');
-  const Pixel13 = document.querySelector('#pixel-13');
-  const Pixel14 = document.querySelector('#pixel-14');
-  const Pixel15 = document.querySelector('#pixel-15');
-  const Pixel16 = document.querySelector('#pixel-16');
-  const Pixel17 = document.querySelector('#pixel-17');
-  const Pixel18 = document.querySelector('#pixel-18');
-  const Pixel19 = document.querySelector('#pixel-19');
-  const Pixel20 = document.querySelector('#pixel-20');
-  const Pixel21 = document.querySelector('#pixel-21');
-  const Pixel22 = document.querySelector('#pixel-22');
-  const Pixel23 = document.querySelector('#pixel-23');
-  const Pixel24 = document.querySelector('#pixel-24');
-  const Pixel25 = document.querySelector('#pixel-25');
-
-  CorPaletaColor1.addEventListener('click', function() {
-    Pixel.style.backgroundColor = 'black';
-  });
-  CorPaletaColor2.addEventListener('click', function() {
-    Pixel.style.backgroundColor = 'blueviolet';
-  });
-  CorPaletaColor3.addEventListener('click', function() {
-    Pixel.style.backgroundColor = 'chartreuse';
-  });
-  CorPaletaColor4.addEventListener('click', function() {
-    Pixel.style.backgroundColor = 'darkgoldenrod';
-  });
-  Button.addEventListener('click', function() {
-    Pixel.style.backgroundColor = 'white';
-  });
+function returnColorPallet(number){
+  const rPallet = document.querySelectorAll("#color-" + number);
+  return rPallet;
 }
+
+function returnPixelBox(number){
+  const rBox = document.querySelectorAll("#pixel-" + number);
+  return rBox;
+}
+
+const clearButton = document.querySelector("#clear-board");
+clearButton.addEventListener('click', function(){
+  const selectedPixel = document.querySelectorAll(".pixel");
+  for(let index = 0; index < selectedPixel.length; index +=1){
+    selectedPixel[index].style.backgroundColor = 'white';
+  }
+});
+
+/*const btnColorBlack = document.querySelector('.black');
+btnColorBlack.addEventListener('click', function () {
+  addColorSelected('black');
+});
+
+const btnColorRed = document.querySelector('.red');
+btnColorRed.addEventListener('click', function () {
+  addColorSelected('red');
+});
+
+const btnColorGreen = document.querySelector('.green');
+btnColorGreen.addEventListener('click', function () {
+  addColorSelected('green');
+});
+
+const btnColorBlue = document.querySelector('.blue');
+btnColorBlue.addEventListener('click', function () {
+  addColorSelected('blue');
+});
+*/
