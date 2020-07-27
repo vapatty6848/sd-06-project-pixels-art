f735436663ae8605d6da9c7965d19af93708f224
fefarina <wfelipefarina@gmail.com>
2020-07-26 21:23:24 -0300

    update js files

--- script.js
+++ script.js
@@ -3,37 +3,51 @@ let pixel = document.querySelectorAll('.pixel');
 
 function selectedColor() {
     let color = document.querySelectorAll('.color');
-
+    let selected = document.querySelector('selected');
     for (let i = 0; i < color.length; i++) {
-        color[i].addEventListener('click', function(e) {
-            e.className = 'color'
-            e.target.classList.add('selected');
-        })
+        color[0].classList.add('selected')
+        color[i].addEventListener('click', function (e) {
+            for (let index = 0; index < color.length; index++) {
+                if (color[index].classList.contains('selected')) {
+                    color[index].classList.remove('selected')
+                } else {
+                    color[i].classList.add('selected');
+                }
+                for (let i = 0; i < pixel.length; i ++) {
+                    pixel[i].addEventListener('click', function() {
+                        if(color[0]) {
+                            pixel[i].classList.add('color1')
+                        }else if (color[1]) {
+                            pixel[i].classList.add('color2')
+                        } else if(color[3]){
+                            pixel[i].classList.add('color3')
+                        } else {
+                            pixel[i].classList.add('color4')
+                        }
+                    })
+                }
+            }
+           
+        });
     };
- }
+       
+};
 
 
 
-function alternerdColors() {
-    for (let i = 0; i < pixel.length; i++) {
-        pixel[i].addEventListener('click', function () {
-            pixel[i].classList.add('teste');
-        })
-    }
-}
 
 
-// Resetando a pagina para pixels brancos
+// // Resetando a pagina para pixels brancos
 
-function whitePixels() {
-    for(let i = 0; i < pixel.length; i++) {
-        pixel[i].style.backgroundColor = 'white'
-    }
-}
+// function whitePixels() {
+//     for (let i = 0; i < pixel.length; i++) {
+//         pixel[i].style.backgroundColor = 'white'
+//     }
+// }
 
-window.onload = whitePixels();
+// window.onload = whitePixels();
 
-////
+// ////
 
 
 // Criando botao ClearBoard
@@ -41,8 +55,8 @@ window.onload = whitePixels();
 let button = document.querySelector('#clear-board');
 
 function buttonClear() {
-    button.addEventListener('click', function() {
-        for(let i = 0; i < pixel.length; i++) {
+    button.addEventListener('click', function () {
+        for (let i = 0; i < pixel.length; i++) {
             pixel[i].style.backgroundColor = '#ffffff';
         }
     })
@@ -50,6 +64,5 @@ function buttonClear() {
 /////
 
 buttonClear();
-alternerdColors();
 selectedColor();
-whitePixels();
+// whitePixels();
