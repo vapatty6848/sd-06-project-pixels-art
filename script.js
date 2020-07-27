8eb73952142358344ea6ac77187c6cc7c951fb0f
Daniele Perse <danieleperse3@gmail.com>
2020-07-27 09:47:19 -0300

    corrigi codeclimate do ultimo commit

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 window.onload = function () {
-    const color1 = document.getElementsByClassName('color')[0];
-    color1.style.backgroundColor = 'red';
+  const color1 = document.getElementsByClassName('color')[0];
+  color1.style.backgroundColor = 'red';
 
-    const color2 = document.getElementsByClassName('color')[1];
-    color2.style.backgroundColor = 'green';
+  const color2 = document.getElementsByClassName('color')[1];
+  color2.style.backgroundColor = 'green';
 
-    const color3 = document.getElementsByClassName('color')[2];
-    color3.style.backgroundColor = 'yellow';
+  const color3 = document.getElementsByClassName('color')[2];
+  color3.style.backgroundColor = 'yellow';
 
-    const color4 = document.getElementsByClassName('color')[3];
-    color4.style.backgroundColor = 'blue';
-}
-
+  const color4 = document.getElementsByClassName('color')[3];
+  color4.style.backgroundColor = 'blue';
+};
