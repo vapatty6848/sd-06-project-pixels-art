e965fa4f650f168a54612a02e6eb3b27ad01393f
ricknois <rickssti@outlook.com>
2020-07-27 14:59:24 -0300

    Cor padrão preto

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const pixelBoard = document.querySelector('#pixel-board');
 const colorPalet =  document.querySelector('#color-palette');
 const teste = document.getElementsByTagName('div')
-let classSel; 
+let classSel = 'preto'; 
 
 function createPixelBoard() {
 for (let i = 0; i < 25; i+=1) {
     let create = document.createElement('span')
-    create.setAttribute('class', 'pixel')
+    create.setAttribute('class', 'pixel ')
     pixelBoard.appendChild(create);   
 }   
 }
