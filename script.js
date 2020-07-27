d4602d99ceb274d9dfb50e669bc40bfaef393b83
fefarina <wfelipefarina@gmail.com>
2020-07-27 09:05:31 -0300

    Update js

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 let pixel = document.querySelectorAll('.pixel');
 let color = document.querySelectorAll('.color');
 let selected = document.querySelector('selected');
+let colorPallete = document.querySelector('#color-palette')
+
+let color2 = document.querySelector('.color2');
+let color3 = document.querySelector('.color3');
+let color4 = document.querySelector('.color4');
 
 
-window.onload = function () {
 
+function generateAleatoryColor() {
+    let color = '#';
+    
+    for(let i = 0; i < 3; i++) {
+        let sub = Math.floor(Math.random() * 256).toString(16);
+        color += (sub.length == 1 ? '0' + sub : sub);
+    }
+    console.log(color)
+    return color
 }
+
+for(let i = 0; i < color.length; i ++) {
+    color[0].style.backgroundColor = "#000"
+    color[i].style.backgroundColor = generateAleatoryColor();
+}
+
+
 
 function selectedColor() {
     for (let i = 0; i < color.length; i++) {
@@ -15,26 +35,30 @@ function selectedColor() {
                 pixel[i].className = 'pixel color1'
             })
         };
-        color[i].addEventListener('click', function (e) {
+        color[i].addEventListener('click', function() {
             for (let index = 0; index < color.length; index++) {
                 if (color[index].classList.contains('selected')) {
                     color[index].classList.remove('selected')
                 } else {
                     color[i].classList.add('selected');
                 }
-                
+
                 for (let i = 0; i < pixel.length; i++) {
                     pixel[i].addEventListener('click', function () {
                         if (color[0].classList.contains('selected')) {
                             pixel[i].className = 'pixel color1'
+                            pixel[i].style.backgroundColor = color[0].style.backgroundColor
                         } else if (color[1].classList.contains('selected')) {
                             pixel[i].className = 'pixel color2'
+                            pixel[i].style.backgroundColor = color[1].style.backgroundColor
                         } else if (color[2].classList.contains('selected')) {
                             pixel[i].className = 'pixel color3';
+                            pixel[i].style.backgroundColor = color[2].style.backgroundColor
+
                         } else {
-                            color[3].classList.add('selected')
                             pixel[i].className = 'pixel color4';
-                            console.log(color[3]);
+                            pixel[i].style.backgroundColor = color[3].style.backgroundColor
+
                         }
                     })
                 }
@@ -72,11 +96,13 @@ function buttonClear() {
     button.addEventListener('click', function () {
         for (let i = 0; i < pixel.length; i++) {
             pixel[i].className = 'pixel white'
+            pixel[i].style.backgroundColor = ''
         }
     });
 }
 /////
 
+generateAleatoryColor()
 buttonClear();
 selectedColor();
 // whitePixels();
