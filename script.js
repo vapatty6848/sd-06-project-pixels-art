eca5b0a0f8d503768faca94aec6cc58d67d80127
Macarena Rodriguez <macarenar697@gmail.com>
2020-07-27 19:36:19 -0300

    Paleta de cor criada

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  let color_Palette = document.querySelector('#color-palette');
+  let colorsArray = color_Palette.children;
+  colorsArray[0].style.backgroundColor = 'black';
+  colorsArray[1].style.backgroundColor = 'red';
+  colorsArray[2].style.backgroundColor = 'orange';
+  colorsArray[3].style.backgroundColor = 'green';
+  
+}
