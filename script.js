e418772a1797f906962e5cd8d94c5e42df449991
fefarina <wfelipefarina@gmail.com>
2020-07-27 07:29:26 -0300

    Updade js files

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let pixel = document.querySelectorAll('.pixel');
 let color = document.querySelectorAll('.color');
+let selected = document.querySelector('selected');
 
 
-window.onload = selectedColor();
+window.onload = function () {
+
+}
+
 function selectedColor() {
-    let selected = document.querySelector('selected');
     for (let i = 0; i < color.length; i++) {
-        color[0].classList.add('selected')
-        color[i].addEventListener('click', function () {
+        color[0].classList.add('selected');
+        for (let i = 0; i < pixel.length; i++) {
+            pixel[i].addEventListener('click', function () {
+                pixel[i].className = 'pixel color1'
+            })
+        };
+        color[i].addEventListener('click', function (e) {
             for (let index = 0; index < color.length; index++) {
                 if (color[index].classList.contains('selected')) {
                     color[index].classList.remove('selected')
@@ -23,7 +31,9 @@ function selectedColor() {
                         } else if (color[2].classList.contains('selected')) {
                             pixel[i].className = 'pixel color3';
                         } else {
+                            color[3].classList.add('selected')
                             pixel[i].className = 'pixel color4';
+                            console.log(color[3]);
                         }
                     })
                 }
@@ -54,13 +64,15 @@ function selectedColor() {
 // Criando botao ClearBoard
 
 let button = document.querySelector('#clear-board');
+let clearBoard = document.querySelector('#pixel-board');
+let whitePixel = document.querySelector('.white');
 
 function buttonClear() {
     button.addEventListener('click', function () {
         for (let i = 0; i < pixel.length; i++) {
-            pixel[i].className = 'pixel';
+            pixel[i].className = 'pixel white'
         }
-    })
+    });
 }
 /////
 
