d2754c840ae2ac98df58cc08cd870be17d111974
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-07-27 13:34:05 -0300

    Consegui selecionar a classe da cor, consigo chamar a função quando clico na pixelBord, mas não colore. Revisar

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 window.onload = function () {
   const colorPalette = document.querySelector('#color-palette');
-  colorPalette.addEventListener('click', function (event) {
-    console.log(event.target.id);
-  });
+  const pixelsBoard = document.getElementById('pixel-board');
+  const clearButton = document.getElementById('clear-board');
+
+
+  function changeClass(event) {
+    document.querySelector('.selected').classList.remove('selected');
+    event.target.classList.add('selected');
+  }
+  colorPalette.addEventListener('click', changeClass);
+
+  function pixelColoring (event) {
+    const selectedColor = document.querySelector('.selected').style.backgroundColor;
+    event.target.style.backgroundColor = selectedColor;
+    console.log("teste")
+  }
+  pixelsBoard.addEventListener('click', pixelColoring);
+
+  function clearBoard() {
+    const pixels = document.querySelectorAll('.pixel');
+    for (let i = 0; i < pixels.length; i += 1) {
+      pixels[i].style.backgroundColor = '';
+    }
+  }
+  clearButton.addEventListener('click', clearBoard);
+    
+  /********Tentativa usando o webstorage******************
+  //localStorage.setItem("chosenColor", "black"); // selecionando a cor preta ao carregar a página
+  //localStorage.setItem("chosenColor", "black"); // selecionando a cor preta ao carregar a página
+  // colorPalette.addEventListener('click', function (event) {
+  //   localStorage.setItem("chosenColor", event.target.id);
+  //   let chosenColor = localStorage.getItem("chosenColor");
+  //   return chosenColor;
+  // });
+  /****************************************************** */
+
 }
