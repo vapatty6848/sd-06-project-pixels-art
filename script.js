d991e2ba1099f0b8bd021d72c84eb4e588f75906
Igor Leão <igorleao.eng@gmail.com>
2020-07-27 18:21:13 -0300

    teste evaluator

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
   const divPallet = document.querySelector('#color-palette');
   const clearButton = document.getElementById('clear-board');
+  let aux;
+  
+  let selectColor = document.getElementById('color-palette').childNodes;
+  let colorBlack = selectColor[1];
+  colorBlack.style.backgroundColor = 'black';
+  let getColorRGB = colorBlack.style.backgroundColor;
+  let colorGreen = selectColor[3];
+  colorGreen.style.backgroundColor = 'green';
+  let colorBlue = selectColor[5];
+  colorBlue.style.backgroundColor = 'blue';
+  let colorRed = selectColor[7];
+  colorRed.style.backgroundColor = 'red';
 
-  divPallet.addEventListener('click'), function(event) {
-    const firstSelectionBLack = document.querySelector('#black').style.backgroundColor;
-    firstSelectionBLack.classList.remove('selected');
-    event.target.classList.add('selected');
-    firstSelectionBLack = event.target;
-
-  }
- /* selectColor.addEventListener('click', function() {
-      optColor(selectColor)
-
-  });
-  function defineColor(pixel) {
-    pixel.addEventListener('click', function () {
-      pixel.style.backgroundColor = selectColor;
-    });
-  }*/
-
-  function optColor(div) {
-    const selectorDiv = document.getElementsByClassName('selected')[0];
+  
+  divPallet.addEventListener('click', function op(event) {
    
+    aux = event.target;
+    console.log(event.target);
+    const boardClickedPixel = document.querySelector('#pixel-board');
+  boardClickedPixel.addEventListener('click', function (event) {
+    const clickedPixel = event.target;
+    clickedPixel.style.backgroundColor = aux.style.backgroundColor;
+    
+  });
 
-  }
+  })
+  
+  
 
   clearButton.addEventListener('click', function () {
-    for (let i = 0; i < document.querySelectorAll('.pixel').length; i++) {
-      const currentPixel = document.querySelectorAll('.pixel')[i];
-      currentPixel.style.backgroundColor = 'white';
+    const clearPixel = document.querySelectorAll('.pixel');
+    for (let index = 0; index < clearPixel.length; index += 1) {
+      clearPixel[index].style.backgroundColor = 'white';
     }
   })
 
