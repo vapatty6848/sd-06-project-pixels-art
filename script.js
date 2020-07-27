761b2bf92f66e01fa32555609bf1d89829af584d
Julia Porto <juportob@gmail.com>
2020-07-27 12:33:56 -0300

    Add parte 2 do requisito 6

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 window.onload = function () {
 
-
-let color = document.querySelectorAll(".color")
-let pixel = document.querySelectorAll(".pixel")
-
-colorBlack = document.getElementById("black")
-
-colorBlack.addEventListener("click", function () {
-  const divSelect = document.getElementsByClassName('selected');
-  const div = document.getElementsByClassName('color');
-  divSelect.classList.remove('selected');
-  div.classList.add('selected');
-})
-
+let corBlack = document.getElementById("black")
+corBlack.classList.add("selected")
 
 }
