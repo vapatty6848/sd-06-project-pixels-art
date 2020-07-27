76bb078dca8bb6925a60c4e593c4b37f2c8b3f44
Bruna Franciele <brunafranciele1902@gmail.com>
2020-07-27 18:56:43 -0300

    alterações simples

---
+++ script.js
@@ -0,0 +1,19 @@
+let pintar = document.querySelectorAll(".pixel");
+let b = document.querySelectorAll(".td");
+
+        pintar[i].addEventListener("click", function () {
+
+            for (let i in pintar) {
+                pintar[i].style.backgroundColor = "yellow"; 
+            }
+        })
+
+    // let pixel = document.querySelector("#primeiro")
+    // let preto = document.querySelector(".selected")
+    // pixel.addEventListener("click", function () {
+    //     pixel.style.backgroundColor = preto;
+
+
+
+    // })
+
