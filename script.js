a14736d1503b9607f687fee218891ebd944062f8
Elcio Rodrigo <e.rhodrigo.a@gmail.com>
2020-07-27 20:33:13 -0300

    passo 3 4 5

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 window.onload = function() {
-    const cor0 = document.querySelectorAll('.color')[0];
-    cor0.className = 'selected';
-    const limpar = document.getElementById('clear-board');
-    limpar.addEventListener('click', function() {
-        const pixel = document.querySelectorAll('.pixel');
-        for (let i = 0; i < pixel.length; i += 1) {
-            pixel[i].style.backgroundColor = 'white';
-        }
-    })
+  const cor0 = document.querySelectorAll('.color')[0];
+  cor0.className = 'selected';
 }
