d8d200eb79c1fdba1fef342ca74a3ec89aa10cb2
Marcelo Pessini <marceloppessini@gmail.com>
2020-07-28 16:17:03 -0300

    Adicionando paleta de cores

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let color = ['black', 'blue', 'green', 'red'];
+let paletContainer = document.getElementById('color-palette');
+function createPalet(color){
+  for (let i = 0; i < color.length; i += 1){
+    let createDivPalet = document.createElement('div');
+    createDivPalet.style.backgroundColor = color[i];
+    createDivPalet.className = "color";
+    paletContainer.appendChild(createDivPalet);
+  }
+}
+onload = createPalet(color);
 
