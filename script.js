bff5485af66f9ae7cb3bc14a2264896784b51a3f
Pedro <pedroedu77@gmail.com>
2020-07-28 17:03:56 -0300

    Requisitos 3 e 4 corrigidos

--- script.js
+++ script.js
@@ -4,21 +4,20 @@ const colors = ['black', 'green', 'red', 'blue'];
 const pixelBoard = document.getElementById('pixel-board');
 // Funções e Eventos
 
-for (const i in colors) {
+for (let i = 0; i < 4; i += 1) {
   const createBoxColors = document.createElement('li');
   createBoxColors.className = 'color';
   createBoxColors.style.backgroundColor = colors[i];
   colorPalette.appendChild(createBoxColors);
 }
 
-  for (let i = 0; i < 5; i += 1) {
-    const linha = document.createElement('div');
-    linha.className = 'line';
-    pixelBoard.appendChild(linha);
-    for (let j = 0; j < 5; j += 1) {
-      const pixel = document.createElement('div');
-      pixel.className = 'colun';
-      pixel.id = 'pixel';
-      pixelBoard.appendChild(pixel);
-    }
+for (let i = 0; i < 5; i += 1) {
+  const linha = document.createElement('div');
+  linha.className = 'line';
+  pixelBoard.appendChild(linha);
+  for (let j = 0; j < 5; j += 1) {
+    const pixel = document.createElement('div');
+    pixel.className = 'colun pixel';
+    pixelBoard.appendChild(pixel);
   }
+}
