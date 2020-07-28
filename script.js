f0260541e3424ff8a4fd95fd1e298ef32eb686fe
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-07-28 16:23:38 -0300

    estrutra visual do painal de pixel finalizada

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 window.onload = function() {
     let color = document.querySelectorAll(".color");
+    let quadroPixel = document.querySelector("#pixel-board");
     let cores = ["black", "yellow", "blue", "gray", "Gold"];
     for (let nColor = 0; nColor < color.length; nColor += 1) {
         color[nColor].style.backgroundColor = cores[nColor];        
     }
-  
-
     
+    for (let n = 1; n <= 25; n += 1) {
+        let pixel = document.createElement("div");
+        pixel.className = "pixel";
+        quadroPixel.appendChild(pixel);
+        /* console.log(pixel.className) */
+    }      
 }
