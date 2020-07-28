0afe4169be77e07440f93e96dc37803a76ce8b91
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-28 19:05:32 -0300

    9: Limpando o quadro

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // Lidando com a Paleta de Cores
-function populatePaletteItem(colors) {
-  const paletteItemDiv = document.getElementById('color-palette');
-
-  for (let index in colors) {
-    const paletteItem = createPaletteItem(colors[index]);
-    paletteItemDiv.appendChild(paletteItem);
-  }
+function handleClick(event) {
+  const oldClassSelected = document.querySelector('.selected');
+  const currentSelected = event.target;
+  oldClassSelected.classList.remove('selected');
+  currentSelected.classList.add('selected');
 }
 
 function createPaletteItem(color) {
@@ -15,17 +13,17 @@ function createPaletteItem(color) {
   paletteItemColor.addEventListener('click', handleClick);
   if (color === 'black') {
     paletteItemColor.classList.add('selected');
-  };
+  }
   return paletteItemColor;
 }
 
-function handleClick(event) {
-  const oldClassSelected = document.querySelector('.selected');
-  const currentSelected = event.target;
-  oldClassSelected.classList.remove('selected');
-  currentSelected.classList.add('selected');
-  selectedBackground = currentSelected.style.background
-  console.log(selectedBackground);
+function populatePaletteItem(colors) {
+  const paletteItemDiv = document.getElementById('color-palette');
+
+  for (let index in colors) {
+    const paletteItem = createPaletteItem(colors[index]);
+    paletteItemDiv.appendChild(paletteItem);
+  }
 }
 
 function clickEvent(event) {
@@ -35,28 +33,40 @@ function clickEvent(event) {
 }
 
 // Lidando com o board
-function populandoBoard(nLine, nColums) {
-  for (let index = 0; index < nColums; index += 1) {
-    populandoUmLinha(nLine);
-  }
-}
-
-function populandoUmLinha(number) {
-  const pixelLine = document.querySelector('#pixel-board');
-  const br = document.createElement('br');
-  for (let index = 0; index < number; index += 1) {
-    let pixelItem = createPixelItem();
-    pixelLine.appendChild(pixelItem);
-  }
-  pixelLine.appendChild(br);
-}
-
 function createPixelItem() {
   const pixelDiv = document.createElement('div');
   pixelDiv.className = 'pixel';
   pixelDiv.addEventListener('click', clickEvent);
   return pixelDiv;
 }
+
+function populandoUmLinha(number) {
+  const pixelLine = document.querySelector('#pixel-board');
+  const br = document.createElement('br');
+  for (let index = 0; index < number; index += 1) {
+    const pixelItem = createPixelItem();
+    pixelLine.appendChild(pixelItem);
+  }
+  pixelLine.appendChild(br);
+}
+
+function populandoBoard(nLine, nColums) {
+  for (let index = 0; index < nColums; index += 1) {
+    populandoUmLinha(nLine);
+  }
+}
+
+// Limpando board
+function clearBoard() {
+  const pixelBoard = document.getElementsByClassName('pixel');
+  const cor = 'white';
+  for (let index in pixelBoard) {
+    pixelBoard[index].style.background = cor;
+  }
+}
+
+const btnClear = document.querySelector('#clear-board');
+btnClear.addEventListener('click', clearBoard);
 
 populatePaletteItem(['black', 'blue', 'yellow', 'green']);
 populandoBoard(5, 5);
