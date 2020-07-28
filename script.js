20f01ea05b18d33b46ee52006140fdd15437fa9a
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-07-28 20:17:04 -0300

    requisitos 4

--- script.js
+++ script.js
@@ -1,52 +1,42 @@
 window.onload = function () {
   createColorPallet(['black', 'red', 'blue', 'green'])
+  pixelsBoard(5);
 }
-  function createColorPallet(colors) {
-    let colorPalletContainer = document.getElementById("color-palette");
-    for (let index in colors) {
-      let palletItemDiv = createPalletItem(colors[index]);
-      colorPalletContainer.appendChild(palletItemDiv);
-    }
+let pixelBoardDiv = document.querySelector("#pixel-board");
+let btn = document.querySelector('#clear-board');
+let pixel = document.getElementsByClassName('pixel');
+
+function createColorPallet(colors) {
+  let colorPalletContainer = document.getElementById("color-palette");
+  for (let index in colors) {
+    let palletItemDiv = createPalletItem(colors[index]);
+    colorPalletContainer.appendChild(palletItemDiv);
   }
-  
-  function createPalletItem(color) {
-    let palletItemDiv = document.createElement('div');
-    palletItemDiv.style.backgroundColor = color;
-    palletItemDiv.className = "color";
-    palletItemDiv.addEventListener("click", handlePalletItemEvent);
-
-    if (color === "black") {
-      palletItemDiv.classList.add("selected");
-    }
-    return palletItemDiv;
+}
+function createPalletItem(color) {
+  let palletItemDiv = document.createElement('div');
+  palletItemDiv.style.backgroundColor = color;
+  palletItemDiv.className = "color";
+  palletItemDiv.addEventListener("click", handlePalletItemEvent);
+  if (color === "black") {
+    palletItemDiv.classList.add("selected");
   }
-
-//Quando clica, add o selected e retira da cor antiga
-  function handlePalletItemEvent(event) {
-    let oldSelectedDiv = document.querySelector(".selected");
-    let currentSelectedDiv = event.target;
-
-    oldSelectedDiv.classList.remove("selected");
-    currentSelectedDiv.classList.add("selected");
-
-    selectedColor = currentSelectedDiv.style.backgroundColor;
+  return palletItemDiv;
+}
+function handlePalletItemEvent(event) {
+  let oldSelectedDiv = document.querySelector(".selected");
+  let currentSelectedDiv = event.target;
+  oldSelectedDiv.classList.remove("selected");
+  currentSelectedDiv.classList.add("selected");
+  selectedColor = currentSelectedDiv.style.backgroundColor;
+}
+function handlePixelClick(event) {
+  let selectedPixelDiv = event.target;
+  selectedPixelDiv.style.backgroundColor = selectedColor;
+}
+pixelBoardDiv.addEventListener("click", handlePixelClick)
+btn.addEventListener("click", function () {
+  for (let i = 0; i < pixel.length; i += 1) {
+    pixel[i].style.backgroundColor = "white";
   }
-
-
-  function handlePixelClick(event) {
-    let selectedPixelDiv = event.target;
-    selectedPixelDiv.style.backgroundColor = selectedColor;
-  }
-
-  let pixelBoardDiv = document.querySelector("#pixel-board");
-  pixelBoardDiv.addEventListener("click", handlePixelClick)
-  
-
-  //Botão que limpa o quadro todo
-  let btn = document.querySelector('#clear-board');
-  let pixel = document.getElementsByClassName('pixel');
-  btn.addEventListener("click", function () {
-    for (let i = 0; i < pixel.length; i += 1) {
-      pixel[i].style.backgroundColor = "white";
-    }
-  })
+})
