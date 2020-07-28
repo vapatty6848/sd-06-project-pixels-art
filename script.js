4e421d930444e58f78d2212d7a6fbfb554db93ca
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-07-28 17:05:50 -0300

    site funcional nos requisitos obrigatorios

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onload = function() {
     let color = document.querySelectorAll(".color");
     let quadroPixel = document.querySelector("#pixel-board");
+    let containerColor = document.querySelector("#color-palette");
     let cores = ["black", "yellow", "blue", "gray", "Gold"];
     for (let nColor = 0; nColor < color.length; nColor += 1) {
         color[nColor].style.backgroundColor = cores[nColor];        
@@ -9,7 +10,17 @@ window.onload = function() {
     for (let n = 1; n <= 25; n += 1) {
         let pixel = document.createElement("div");
         pixel.className = "pixel";
-        quadroPixel.appendChild(pixel);
-        /* console.log(pixel.className) */
-    }      
+        quadroPixel.appendChild(pixel);        
+    } 
+    let colorSelecionada = cores[0];
+    console.log(colorSelecionada);
+    containerColor.addEventListener("click", function(event) {
+        colorSelecionada = event.target.style.backgroundColor;        
+    });
+
+    quadroPixel.addEventListener("click", function(event) {
+        event.target.style.backgroundColor = colorSelecionada;               
+    });
+
+
 }
