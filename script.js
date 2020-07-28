cbd22feb171053d9c91f5bfe30c7819a872ae86d
Matheus Dias <matheuscara2000@gmail.com>
2020-07-28 19:11:53 -0300

    Adicionando pixeis corretor para os elementos

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//Criando os Blocos da Tabela
 function desenhandoBlocos(){
     let bloco = document.getElementById("pixel-board")
     for (let i=0;i<5;i++){
@@ -7,15 +7,12 @@ function desenhandoBlocos(){
         for (let o=0;o<5;o++){
             let blocoUnico=document.createElement("div")
             blocoUnico.className="pixel"
-            blocoUnico.style.background="white"
-            blocoUnico.style.width="50px"
-            blocoUnico.style.height="50px"
+            blocoUnico.style.background="red"
+            blocoUnico.style.width="40px"
+            blocoUnico.style.height="40px"
+            blocoUnico.style.border="1px solid black"
+            blocoUnico.style.display="inline-block"
             linha.appendChild(blocoUnico)
         }
     }
 }
-
-function selectColor(){
-
-}
-window.onload = desenhandoBlocos();
