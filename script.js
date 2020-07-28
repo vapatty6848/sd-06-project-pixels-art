ea1c45c4410319ff42f10d6478a46a67be254ca7
Leandro Parisi <lepcbelisario@gmail.com>
2020-07-28 18:40:03 -0300

    Req 3

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let colorPalette = document.querySelector("#color-palette");
+
 // create palette colors
 createPaletteColors("black", "red", "blue", "green", "purple");
-
 
 function createPaletteColors (color1, color2, color3, color4, color5){
     let colorArray = [color1, color2, color3, color4, color5];
@@ -10,6 +10,23 @@ function createPaletteColors (color1, color2, color3, color4, color5){
         let newDiv = document.createElement("div");
         colorPalette.appendChild(newDiv).className = "color";
         colorPalette.lastChild.style.backgroundColor = colorArray[index];
+
+    }
+}
+
+// create pixel board
+let pixelBoard = document.querySelector("#pixel-board");
+
+createPixelBoard();
+
+function createPixelBoard(){
+    for (index = 1; index <= 5; index += 1){
+        let newLine = document.createElement("div");
+        pixelBoard.appendChild(newLine).className = "board-line";
+        for (internIndex = 1; internIndex <= 5; internIndex += 1){
+            let newPixel = document.createElement("div");
+            pixelBoard.lastChild.appendChild(newPixel).className = "pixel";
+        }
 
     }
 }
