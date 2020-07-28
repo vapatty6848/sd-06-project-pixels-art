d5c4cd1f1629457a1821ae835d1e6ddbd044c63e
LeandroFeitozaGnu <leandro.feitoza8018@gmail.com>
2020-07-28 17:23:27 -0300

    Criado 4 divse estilizado cada uma com um background color

---
+++ script.js
@@ -0,0 +1,17 @@
+const blackColor = document.getElementsByClassName('color')[0]
+blackColor.style.backgroundColor = 'black'
+
+const blueColor = document.getElementsByClassName('color')[1]
+blueColor.style.backgroundColor = 'blue'
+
+const orangeColor = document.getElementsByClassName('color')[2]
+orangeColor.style.backgroundColor = 'orange'
+
+const greenColor = document.getElementsByClassName('color')[3]
+greenColor.style.backgroundColor = 'green'
+
+ 
+
+console.log(
+    blackColor
+)
