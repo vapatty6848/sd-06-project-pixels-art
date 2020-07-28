b1e371eaed00fadfae3b57dc49349101f09ac8ce
Leandro Parisi <lepcbelisario@gmail.com>
2020-07-28 18:41:16 -0300

    Req 2: corrigindo quantidade de cores

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let colorPalette = document.querySelector("#color-palette");
 
 // create palette colors
-createPaletteColors("black", "red", "blue", "green", "purple");
+createPaletteColors("black", "red", "blue", "green");
 
-function createPaletteColors (color1, color2, color3, color4, color5){
-    let colorArray = [color1, color2, color3, color4, color5];
+function createPaletteColors (color1, color2, color3, color4){
+    let colorArray = [color1, color2, color3, color4];
 
     for (index = 0; index < colorArray.length; index += 1){
         let newDiv = document.createElement("div");
