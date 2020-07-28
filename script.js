1ff69030ab900ce4c8f8c2cf73b458b9f0e3d15b
Pedro <pedroedu77@gmail.com>
2020-07-28 16:46:08 -0300

    Requisitos 3 e 4 concluidos

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 // Variaveis //
 const colorPalette = document.getElementById('color-palette');
-const colors = ['blue', 'green', 'red', 'yellow'];
-
+const colors = ['black', 'green', 'red', 'blue'];
+const pixelBoard = document.getElementById('pixel-board');
 // Funções e Eventos
 
 for (const i in colors) {
-  let createBoxColors = document.createElement('li');
+  const createBoxColors = document.createElement('li');
   createBoxColors.className = 'color';
   createBoxColors.style.backgroundColor = colors[i];
   colorPalette.appendChild(createBoxColors);
 }
+
+  for (let i = 0; i < 5; i += 1) {
+    const linha = document.createElement('div');
+    linha.className = 'line';
+    pixelBoard.appendChild(linha);
+    for (let j = 0; j < 5; j += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'colun';
+      pixel.id = 'pixel';
+      pixelBoard.appendChild(pixel);
+    }
+  }
