044c985933464c99a975e386ee870647065cbc56
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-07-28 19:57:50 -0300

    Mudança no código para que os elementos contidos na div color-palette fossem feitos através do javascript

---
+++ script.js
@@ -0,0 +1,32 @@
+window.onload = function () {
+
+    palletColor(['black', 'red', 'blue', 'green']);
+
+    function palletColor (colors) {
+        let colorContainer = document.getElementById('color-palette');
+        for (let i in colors) {
+            let colorDiv = createPallet(colors[i]);
+            colorContainer.appendChild(colorDiv);
+        }
+    }
+    function createPallet (color) {
+        let palletDiv = document.createElement('div');
+        palletDiv.style.backgroundColor = color;
+        palletDiv.className = 'pallet-item';
+        palletDiv.addEventListener('click', handlePallet);
+        if (color === 'black') {
+            palletDiv.classList.add('selected');
+        }
+        return palletDiv;
+    }
+
+    function handlePallet (event){
+        let currentDiv = document.querySelector('.selected');
+        let newDiv = event.target;
+        currentDiv.classList.remove('selected');
+        newDiv.classList.add('selected');
+        let bkgrdColor = div.style.backgroundColor;
+    }
+
+
+}
