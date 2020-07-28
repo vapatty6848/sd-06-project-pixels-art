e73c8f0311f1857eac3a9d948d714c6a139cfe0b
Arthur de Sa <arthurdesa94@gmail.com>
2020-07-28 17:44:21 -0300

    tentativa1

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//Hello world
+let selectedColor = 'black';
+
+window.onload=function(){
+
+    let colorList = ['black', 'red', 'green', 'blue'];
+    let pixels = document.querySelectorAll('.pixel');
+  
+    createColorPalette(colorList);
+ }
+    function colorSelector (color){
+        let selectorDivElement = document.createElement('div');
+        selectorDivElement.className = 'color';
+        selectorDivElement.style.backgroundColor = color;
+        selectorDivElement.style.display = "table-cell";
+        return selectorDivElement;
+      }
+    }
+
+
+
