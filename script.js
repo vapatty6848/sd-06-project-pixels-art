1b11196568963a07ef93b6c00917697dace0f984
Paulo Afonso Volpin <paulo.volpin@gmail.com>
2020-07-28 17:46:44 -0300

    Tasks 10 and 11: user defines board size

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ window.onload = function () {
   let colorPalette = document.querySelector('#color-palette');
   let pixelsBoardTable = document.querySelector('#pixel-board');
   let clearButton = document.querySelector('#clear-board');
+  let buildBoardButton = document.querySelector('#generate-board');
   let numberOfColumns = 5;
   let numberOfRows = 5;
   let numberOfColors = 4;
@@ -40,35 +41,54 @@ window.onload = function () {
       let color = 'rgb(' + [redChannel , greenChannel , blueChannel].toString() + ')';
       return color;
     },
-  }
-
-  // Building colors palette
-  for (let i = 0; i < numberOfColors; i += 1) {
-    let colorItem = document.createElement('li');
-    if (i === 0) {
-        colorItem.className = 'color selected'; // Default color black
-        colorItem.style.background = 'black';
-    } else {
-        colorItem.className = 'color';
-        colorItem.style.background = functionalities.generateColorPalette(); // Random color
-    }
-    colorPalette.appendChild(colorItem);
-    colorItem.addEventListener('click', functionalities.changeClass); // Event to change class
+    buildBoard: function () {
+      for (let row = 0; row < numberOfRows; row += 1) { // Walkind on rows
+        let tableRow = document.createElement('tr');
+        tableRow.className = 'table-row';
+        pixelsBoardTable.appendChild(tableRow);
+        for (let column  = 0; column < numberOfColumns; column += 1) { // Walking on columns
+          let tablePixel = document.createElement('td');
+          tablePixel.className = 'pixel';
+          tableRow.appendChild(tablePixel);
+          tablePixel.addEventListener('click', functionalities.putColor); // Event to put color on pixel
+        }
+      }
+    },
+    buildColorsPalette: function () {
+      for (let i = 0; i < numberOfColors; i += 1) {
+        let colorItem = document.createElement('li');
+        if (i === 0) {
+            colorItem.className = 'color selected'; // Default color black
+            colorItem.style.background = 'black';
+        } else {
+            colorItem.className = 'color';
+            colorItem.style.background = functionalities.generateColorPalette(); // Random color
+        }
+        colorPalette.appendChild(colorItem);
+        colorItem.addEventListener('click', functionalities.changeClass); // Event to change class
+      }
+    },
+    rebuildBoard: function () {
+      let userInputDimension = document.querySelector('#board-size').value;
+      numberOfRows = (userInputDimension >= 5 && userInputDimension <= 50) ? userInputDimension : 5;
+      numberOfColumns = numberOfRows;
+      let previousTableRows = document.querySelectorAll('tr');
+      for (let i = 0; i < previousTableRows.length; i += 1) {
+        pixelsBoardTable.removeChild(previousTableRows[i]);
+      }
+      functionalities.buildBoard();
+    },
   }
 
   // Building pixels board
-  for (let row = 0; row < numberOfRows; row += 1) {
-    let tableRow = document.createElement('tr');
-    tableRow.className = 'table-row';
-    pixelsBoardTable.appendChild(tableRow);
-    for (let column  = 0; column < numberOfColumns; column += 1) {
-    let tablePixel = document.createElement('td');
-    tablePixel.className = 'pixel';
-    tableRow.appendChild(tablePixel);
-    tablePixel.addEventListener('click', functionalities.putColor); // Event to put color on pixel
-    }
-  }
+  functionalities.buildBoard();
+
+  // Building colors palette
+  functionalities.buildColorsPalette();
 
   // Clear all pixels
   clearButton.addEventListener('click', functionalities.clearPixels);
+
+  // Rebuild the board as user set
+  buildBoardButton.addEventListener('click', functionalities.rebuildBoard);
 }
