6efabe6e88af02a0e5e2a5982a6c459b42f47e64
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-07-28 17:04:05 -0300

    Corrigindo quantidade de cores e escopo da variável colorChange.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-document.addEventListener('click', function (pointedPixel, changeColor) {
+let changeColor = document.querySelector('.selected').id;
+
+document.addEventListener('click', function (pointedPixel) {
   pointedPixel = event.target;
 
   if (pointedPixel.classList.contains('color') === true) {
     document.querySelector('.selected').classList.remove('selected');
     pointedPixel.classList.add('selected');
+    changeColor = document.querySelector('.selected').id;
   } else if (pointedPixel.classList.contains('pixel') === true) {
-    let changeColor = document.querySelector('.selected').id;
     pointedPixel.style.backgroundColor = changeColor;
   }
 });
