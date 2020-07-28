32b21eedae7a02a30f789c4314c02f86b81e6fc5
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-07-28 17:03:45 -0300

    Changed the palette creation to the script, black is first color

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 window.onload = function () {
-  alert('Draw something!');
+  const colorList = ['black', 'red', 'green', 'blue'];
+  const pixels = document.querySelectorAll('.pixel');
+
+  createColorPalette(colorList);
+};
+
+function createColorSelector(color) {
+  let selectorDivElement = document.createElement('div');
+  selectorDivElement.className = 'color';
+  selectorDivElement.style.backgroundColor = color;
+  selectorDivElement.style.display = "table-cell";
+  return selectorDivElement;
+};
+
+function createColorPalette(colors) {
+  const paletteContainer = document.getElementById('color-palette');
+  
+  for (let i in colors) {
+    let colorSelector = createColorSelector(colors[i]);
+    paletteContainer.appendChild(colorSelector);
+  }
 };
