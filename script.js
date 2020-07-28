f1ffe9ddcfc10fde57951ac8def4fbf3e0a54cff
LughWalle <civalfran1@gmail.com>
2020-07-28 17:26:23 -0500

    tabela corrigidas

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-//'greenyellow', 'orange', 'violet', 'cyan', 'blueviolet', 'chartreuse', 'chocolate', 'coral', 'gold', 'goldenrod', 'grey', 'black'
+// 'greenyellow', 'orange', 'violet', 'cyan', 'blueviolet', 'chartreuse', 'chocolate', 'coral', 'gold', 'goldenrod', 'grey', 'black'
 // variaveis
-const colorPalette = document.querySelector("#color-palette");
-const tableColor = document.getElementById("pixel-board");
+const colorPalette = document.querySelector('#color-palette');
+const tableColor = document.getElementById('pixel-board');
 
-let colors = ["black", "blue", "yellow", "red"];
+const colors = ['black', 'blue', 'yellow', 'red'];
 
 // functions and events
 for (let i = 0; i < colors.length; i++) {
-  let createBoxColor = document.createElement("li");
-  createBoxColor.className = "color";
+  let createBoxColor = document.createElement('li');
+  createBoxColor.className = 'color';
   createBoxColor.style.backgroundColor = colors[i];
   colorPalette.appendChild(createBoxColor);
 }
 
 for (let i = 0; i < 5; i++) {
-  let createTr = document.createElement("tr");
-  createTr.className = "tabela";
+  let createTr = document.createElement('tr');
+  createTr.className = 'tabela';
   tableColor.appendChild(createTr);
   for (let i = 0; i < 5; i++) {
     let createTd = document.createElement('td');
     createTd.className = 'pixel';
     createTr.appendChild(createTd);
-
-    
   }
 }
