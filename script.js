8f67d5f6dce5f12da5fb85a3039f450ba8c6c546
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2020-07-28 13:24:42 -0300

    script linkado no html e tantativas de estilizar corretamente classes e ids

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 //Ao carregar a página, a cor preta da paleta já deve estar selecionada para pintar os pixels.///
 
+
+
 window.onload
 
-function() {
+let pintar = "black";
+
+function(event) {
+
 
     let black = document.getElementById("black");
 
     black.classList.add("selected")
 
+    black.addEventListener("click", colorBlack)
+}
+
+function addText(event) {
+    var conteudo = event.target.value;
+    var getDiv = document.getElementById("meme-text");
+    getDiv.innerHTML = conteudo;
 
 
 }
