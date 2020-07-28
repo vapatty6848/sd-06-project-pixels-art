663b60242310987148bd7b90164f6b046338cd7e
Kesley Lahoz <namielahoz@gmail.com>
2020-07-28 17:25:27 -0300

    incluido grid em branco e cores da paleta

---
+++ script.js
@@ -0,0 +1,17 @@
+
+window.onload = function () {
+    
+    backColorPalette = (["black", "blue", "brown", "red"]);
+    //pega o array e adiciona as cores nos boxes
+    function addColorInBoxes (colors) {
+        let colorContainer = document.querySelector(".color_palette");
+        
+        for (let index in colors) {
+          let divColors = colorContainer(colors[i]);
+          colorContainer.appendChild(divColors);  
+        }
+    }
+};
+    function makeABoard () {
+        for(var i=0; i>=)
+    }
