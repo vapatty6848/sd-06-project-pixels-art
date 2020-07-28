66e81d0abe5a5df8bd0c680b8675faf60515a71a
Marxneves <marxkhristie@gmail.com>
2020-07-28 12:49:01 -0300

    Botao limpar criado e funcionando.

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-window.onload = function () {
-  const pixels = document.querySelectorAll('.pixel');
-  const colors = document.querySelectorAll('.color');
-  let = corSelecionada = 'black';
-  for(let pixel = 0; pixel < pixels.length; pixel += 1){
-    pixels[pixel].addEventListener('click', function () {
-      pixels[pixel].style.backgroundColor = corSelecionada;
-    });
-  }
-  for(let color = 0; color < colors.length; color += 1){
-    let cor = colors[color]; 
-    cor.addEventListener('click', function () {
-      if(!cor.classList.contains('selected')){
-        for (let i = 0; i < colors.length; i++) {
-          colors[i].classList.remove('selected');
-        }
-        cor.className += ' selected';
-        corSelecionada = colors[color].classList[1];
-      }
-    }) 
-  }
+const pixels = document.querySelectorAll('.pixel');
+const colors = document.querySelectorAll('.color');
+const btnLimpar = document.querySelector('#clear-board');
+
+let corSelecionada = 'black';
+for (let pixel = 0; pixel < pixels.length; pixel += 1) {
+  pixels[pixel].addEventListener('click', function () {
+    pixels[pixel].style.backgroundColor = corSelecionada;
+  });
 }
+
+for (let color = 0; color < colors.length; color += 1) {
+  const cor = colors[color];
+  cor.addEventListener('click', function () {
+    if(!cor.classList.contains('selected')){
+      for (let i = 0; i < colors.length; i += 1) {
+        colors[i].classList.remove('selected');
+      }
+      cor.className += ' selected';
+      corSelecionada = colors[color].classList[1];
+    }
+  });
+}
+
+btnLimpar.addEventListener('click', () => {
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = 'white';
+  }
+});
