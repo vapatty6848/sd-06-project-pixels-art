93334b3fcef70a011871fd2b8fa6c391de4098b6
Paulo Afonso Volpin <paulo.volpin@gmail.com>
2020-07-28 12:33:20 -0300

    Refactoring the code

--- script.js
+++ script.js
@@ -1,86 +1,62 @@
-//Colors Palette
-let colorPalette = document.querySelector('#color-palette');
-let colorsArray = ['black', 'blue', 'red' , 'green'];
-for (let i = 0; i < 4; i += 1) {
+
+window.onload = function () {
+  // Elements
+  let colorPalette = document.querySelector('#color-palette');
+  let pixelsBoardTable = document.querySelector('#pixels-board');
+  let availableColors = ['black', 'blue', 'red' , 'green'];
+  let clearButton = document.querySelector('button');
+  let numberOfColumns = 5;
+  let numberOfRows = 5;
+
+  // Functionalities
+  let functionalities = {
+    putColor: function () { 
+      let activeColorOfPalette = document.querySelector('.selected'); // Selected color
+      event.target.style.background = activeColorOfPalette.style.backgroundColor;
+    },
+    changeClass: function () {
+      let colorsOnPalette = document.querySelectorAll('li');
+      for (let i = 0; i < colorsOnPalette.length; i += 1) {
+        colorsOnPalette[i].className = 'color';  // Set default class to all colors icon - not selected
+      }
+      // Set selected class just to clicked color icon
+      // IF to avoid wrong class concatenation in case of click the same color 2 or more times in a row
+      if (event.target.className.includes('selected')) { 
+        event.target.className = event.target.className.concat(' selected'); 
+      } else {
+        event.target.className = event.target.className;
+      }
+    },
+    clearPixels: function () {
+      allPixels = document.querySelectorAll('td');
+      for (let i = 0; i < allPixels.length; i += 1) {
+        allPixels[i].style.background = 'white';
+      }
+    },
+  }
+
+  // Building colors palette
+  for (let i = 0; i < 4; i += 1) {
     let colorItem = document.createElement('li');
-    colorItem.className = 'color';
+    colorItem.className = (i === 0) ? 'color selected' : 'color'; // Default color 
+    colorItem.style.background = availableColors[i];
     colorPalette.appendChild(colorItem);
-    colorItem.style.background = colorsArray[i];
-    colorItem.style.border = 'solid black 1px';
-    colorItem.style.borderRadius = '100%';
-    colorItem.style.display = 'inline-block';
-    colorItem.style.padding = '2%';
-    colorItem.style.margin = '1%';
-    colorItem.style.width = '1px';
-}
+    colorItem.addEventListener('click', functionalities.changeClass); // Event to change class
+  }
 
-//Pixel Board
-let pixelBoard = document.createElement('table');
-pixelBoard.id = 'pixel-board';
-document.querySelector('section').appendChild(pixelBoard);
-pixelBoard.style.display = 'block';
-pixelBoard.style.lineHeight = 0;
-
-for (let row = 0; row < 5; row += 1) {
+  // Building pixels board
+  for (let row = 0; row < numberOfRows; row += 1) {
     let tableRow = document.createElement('tr');
-    tableRow.style.display = 'block';
-    pixelBoard.appendChild(tableRow);
-   for (let column  = 0; column < 5; column += 1) {
-        let tablePixel = document.createElement('td');
-        tablePixel.className = 'pixel';
-        tablePixel.style.display = 'inline-block';
-        tablePixel.style.border = 'solid black 1px';
-        tablePixel.style.background = 'white';
-        tableRow.appendChild(tablePixel);
-        tablePixel.style.width = '40px';
-        tablePixel.style.height = '40px';
-        //Adding event for Task 8
-        tablePixel.addEventListener('click', (event) => putColor(tablePixel));
+    tableRow.className = 'table-row';
+    pixelsBoardTable.appendChild(tableRow);
+    for (let column  = 0; column < numberOfColumns; column += 1) {
+    let tablePixel = document.createElement('td');
+    tablePixel.className = 'pixel';
+    tableRow.appendChild(tablePixel);
+    tablePixel.addEventListener('click', functionalities.putColor); // Event to put color on pixel
     }
-}
+  }
 
-//Selected Color
-let colorsOnPalette = document.querySelectorAll('li');
-colorsOnPalette[0].className = colorsOnPalette[0].className.concat(' selected');
-
-for (let i = 0; i < colorsOnPalette.length; i += 1) {
-    colorsOnPalette[i].addEventListener('click', (event) => changeClass(i));
-}
-
-function changeClass(indiceOfElement){
-    for (let i = 0; i < colorsOnPalette.length; i += 1) {
-        if (i == indiceOfElement) {
-            colorsOnPalette[i].className = colorsOnPalette[i].className.concat(' selected');
-        } else {
-            colorsOnPalette[i].className = 'color';
-        }
-    }
-}
-
-
-//Putting color on pixels
-function putColor(clickedPixel) {
-    let activeColorOfPalette = document.querySelector('.selected').style.background;
-    clickedPixel.style.background = activeColorOfPalette;
-}
-
-//Clear all pixels
-let clearButton = document.createElement('button');
-clearButton.id = 'clear-board';
-clearButton.innerText = 'Limpar';
-clearButton.style.background = 'red';
-clearButton.style.display = 'inline-block';
-clearButton.style.padding = '2%';
-clearButton.style.margin = '2%';
-clearButton.style.border = 'solid white 3px';
-clearButton.style.borderRadius = '20%';
-clearButton.addEventListener('click', clearAllPixels);
-document.querySelector('section').appendChild(clearButton);
-//console.log(document.querySelector('section'));
-
-function clearAllPixels() {
-    arrayOfPixels = document.querySelectorAll('td');
-    for (let i = 0; i < arrayOfPixels.length; i += 1) {
-        arrayOfPixels[i].style.background = 'white';
-    }
+  // Clear all pixels
+  clearButton.addEventListener('click', functionalities.clearPixels);
 }
