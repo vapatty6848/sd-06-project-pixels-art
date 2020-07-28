b00e8c46fe48aac767f7a30fa8ce8f6978538a76
rafaelgelatti <rafael.gelatti@gmail.com>
2020-07-28 16:50:28 -0300

    testing pixel-board

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+window.onload = createPixelBoard();
 
+// function createPixelBoard () {
+//   for (let y = 0; y <= 5; y += 1) {
+//     const pixelBoard = document.getElementById('pixel-board');
+//     for (let x = 0; x < 5; x += 1) {
+//       const pixelSquare = document.createElement('div');
+//       pixelSquare.style.border = '1px solid black';
+//       pixelSquare.style.height = '40px';
+//       pixelSquare.style.width = '40px';
+//       pixelSquare.style.display = 'inline-block';
+//       pixelSquare.className = 'pixel';
+//       if (x !== 5) {
+//         pixelBoard.appendChild(pixelSquare);
+//       }
+//     }
+//   }
+// }
