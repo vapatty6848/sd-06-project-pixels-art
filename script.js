272a982526244079bf5779a8517e21923d580049
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-07-28 19:10:34 -0300

    Add javacript with selected black color

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  const blackColor = document.getElementById("black");
+}
+
+const btnClear = document.querySelector("#clear-board");
+btnClear.addEventListener('click', function () {
+  alert("funcionando!");
+})
