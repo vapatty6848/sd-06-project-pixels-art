8e31adeaea76ef952e2f01e22b451143110390b6
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-28 18:39:32 -0300

    8: Clicando nas cores

--- script.js
+++ script.js
@@ -1,81 +1,62 @@
-window.onload = function () {
+// Lidando com a Paleta de Cores
+function populatePaletteItem(colors) {
+  const paletteItemDiv = document.getElementById('color-palette');
 
-  populatePaletteItem(['black', 'blue', 'yellow', 'green']);
-  populandoBoard(5, 5);
-
-  //Lidando com a Paleta de Cores
-  function populatePaletteItem(colors) {
-    let paletteItemDiv = document.getElementById('color-palette');
-
-    for (let index in colors) {
-      let paletteItem = createPaletteItem(colors[index]);
-      paletteItemDiv.appendChild(paletteItem);
-    }
+  for (let index in colors) {
+    const paletteItem = createPaletteItem(colors[index]);
+    paletteItemDiv.appendChild(paletteItem);
   }
+}
 
-  function createPaletteItem(color) {
-    let paletteItemColor = document.createElement('div');
-    paletteItemColor.className = "color";
-    paletteItemColor.style.background = color;
-    paletteItemColor.addEventListener('click', handleClick);
+function createPaletteItem(color) {
+  const paletteItemColor = document.createElement('div');
+  paletteItemColor.className = 'color';
+  paletteItemColor.style.background = color;
+  paletteItemColor.addEventListener('click', handleClick);
+  if (color === 'black') {
+    paletteItemColor.classList.add('selected');
+  };
+  return paletteItemColor;
+}
 
-    if(color === 'black') {
-      paletteItemColor.classList.add('selected');
-    }
-    return paletteItemColor;
-  }
+function handleClick(event) {
+  const oldClassSelected = document.querySelector('.selected');
+  const currentSelected = event.target;
+  oldClassSelected.classList.remove('selected');
+  currentSelected.classList.add('selected');
+  selectedBackground = currentSelected.style.background
+  console.log(selectedBackground);
+}
 
-  function handleClick(event) {
-    let oldClassSelected = document.querySelector('.selected');
-    let currentSelected = event.target;
+function clickEvent(event) {
+  const classSelected = document.querySelector('.selected');
+  const colorSelected = event.target;
+  colorSelected.style.background = classSelected.style.background;
+}
 
-    oldClassSelected.classList.remove('selected');
-    currentSelected.classList.add('selected');
-  }
-
-  //Lidando com o board
-function populandoBoard (nLine, nColums){
+// Lidando com o board
+function populandoBoard(nLine, nColums) {
   for (let index = 0; index < nColums; index += 1) {
     populandoUmLinha(nLine);
   }
 }
 
-  function populandoUmLinha (number) {
-    let pixelLine = document.querySelector('#pixel-board');
-    let br = document.createElement('br');
-    for (let index = 0; index < number; index +=  1) {
-      let pixelItem = createPixelItem();
-      pixelLine.appendChild(pixelItem);
-    }
-    pixelLine.appendChild(br);
+function populandoUmLinha(number) {
+  const pixelLine = document.querySelector('#pixel-board');
+  const br = document.createElement('br');
+  for (let index = 0; index < number; index += 1) {
+    let pixelItem = createPixelItem();
+    pixelLine.appendChild(pixelItem);
   }
-
-  function createPixelItem() {
-    let pixelDiv = document.createElement('div');
-    pixelDiv.className = 'pixel';
-    return pixelDiv;
-  }
-
+  pixelLine.appendChild(br);
 }
 
+function createPixelItem() {
+  const pixelDiv = document.createElement('div');
+  pixelDiv.className = 'pixel';
+  pixelDiv.addEventListener('click', clickEvent);
+  return pixelDiv;
+}
 
-
-
-// const black = document.getElementById('color-palette').firstElementChild;
-// black.className = "color selected";
-// // // const green = document.getElementById('color-palette').firstElementChild.nextElementSibling;
-// // const yellow = document.getElementById('color-palette').lastElementChild;
-// // const corClicada = document.getElementsByClassName('color');
-
-// const colorPalette = document.querySelector('#color-palette');
-// colorPalette.addEventListener('click', function (event) {
-//   let selectedColor = document.querySelector('.color');
-//   selectedColor.className = 'color selected'
-// })
-
-
-// // for (index = black; index = yellow; index = nextElementSibling) {
-// //   black.index.addEventListener('click', function () {
-// //     corClicada.index.className = "color selected";
-// //   })
-// // }
+populatePaletteItem(['black', 'blue', 'yellow', 'green']);
+populandoBoard(5, 5);
