cd6fe8a68cf6ef2a98083d6998fbe51c82c1348b
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-07-28 18:16:02 -0300

    Implementação do requisito 8

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-let selectedColor;
-
-function selectColor () {
-  document.querySelector('.selected').classList.remove('selected');
-  event.target.classList.add('selected');
-  console.log(document.querySelectorAll('.color'));
-}
+let selectedColor = 'rgb(0 , 0 , 0)'; // COMEÇANDO COMO UNDEFINED! VER ISSO DEPOIS!
+console.log(selectedColor);
 
 document.addEventListener('click', function (event) {
   if (event.target.classList.contains('color')) {
-    selectColor();
+    // Swap selected color
+    document.querySelector('.selected').classList.remove('selected');
+    event.target.classList.add('selected');
+    console.log(document.querySelectorAll('.color'));
+
+    // Set selected color;
+    selectedColor = window.getComputedStyle(event.target, null).getPropertyValue('background-color');
+    console.log(event.target);
+    console.log(selectedColor);
+  }
+
+  if(event.target.classList.contains('pixel')) {
+    event.target.style.backgroundColor = selectedColor;
   }
 });
 
