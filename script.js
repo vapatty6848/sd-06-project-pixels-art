2124bc77764a97d9336fa5559b8330d44b9c3f19
RenatoCostaDev <renatotrybe@gmail.com>
2020-07-28 17:45:35 -0300

    03- customizando  JS

--- script.js
+++ script.js
@@ -1,45 +1,52 @@
 window.onload = function () {
   let selectedColor = 'black';
 
-  //Passando array de cores como parâmetro
+  // Passando array de cores como parâmetro
   createColorPallet(['black', 'red', 'blue', 'green']);
 
-  function createColorPallet(colors){
-    let colorPalletContainer = document.getElementById("color-palette");
+  function createColorPallet(colors) {
+    const colorPalletContainer = document.getElementById('color-palette');
 
     for (let index in colors) {
-      let palletItemDiv = createPalletItem(colors[index]);
+      const palletItemDiv = createPalletItem(colors[index]);
       colorPalletContainer.appendChild(palletItemDiv);
     }
   }
-  //Criando divs da paleta de cores
+  // Criando divs da paleta de cores
   function createPalletItem(color) {
     let palletItemDiv = document.createElement('div');
     palletItemDiv.style.backgroundColor = color;
-    palletItemDiv.className = "color";
-    palletItemDiv.addEventListener("click", handlePalletItemEvent);
+    palletItemDiv.className = 'color';
+    palletItemDiv.addEventListener('click', handlePalletItemEvent);
 
-    if (color === "black") {
-      palletItemDiv.classList.add("selected");
+    if (color === 'black'){
+      palletItemDiv.classList.add('selected');
     }
     return palletItemDiv;
   }
 
-    //Mudando o selected da paleta de cores
-    function handlePalletItemEvent(event) {
-      let oldSelectedDiv = document.querySelector(".selected");
-      let currentSelectedDiv = event.target;
-      oldSelectedDiv.classList.remove("selected");
-      currentSelectedDiv.classList.add("selected");
-  
+    // Mudando o selected da paleta de cores
+    function handlePalletItemEvent(event){
+      const oldSelectedDiv = document.querySelector('.selected');
+      const currentSelectedDiv = event.target;
+      oldSelectedDiv.classList.remove('selected');
+      currentSelectedDiv.classList.add('selected');  
       selectedColor = currentSelectedDiv.style.backgroundColor;
     }
+
+    function handlePixelClick(event) {
+      let selectedPixelDiv = event.target;
+      selectedPixelDiv.style.backgroundColor = selectedColor;
+    }
   
-    //Limpar tela
+    const pixelBoardDiv = document.querySelector('#pixel-board');
+    pixelBoardDiv.addEventListener('click', handlePixelClick)
+  
+    // Limpar tela
     let btn = document.querySelector('#clear-board');
     let pixel = document.getElementsByClassName('pixel');
     btn.addEventListener('click', function () {
-      for (let index = 0; index < pixel.length; index++) {
+      for (let index = 0; index < pixel.length; index += 1) {
         pixel[index].style.backgroundColor = 'white';
       }
     })
