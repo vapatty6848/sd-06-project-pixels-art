b3cf8fc064b963a7ffdd165d6d83ce733e86988e
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-07-28 18:05:37 -0300

    Verificando o requisito 8

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function () {
-  let colors = document.querySelectorAll('.color');
-  
+  const colors = document.querySelectorAll('.color');
+  const pixelTable = document.querySelector('#pixel-board');
   for (let i = 0; i < colors.length; i += 1) {
     colors[i].addEventListener('click', function () {
       for (let j = 0; j < colors.length; j += 1) {
@@ -9,6 +9,15 @@ window.onload = function () {
         }
       }
       colors[i].classList.add('selected');
-  });
+    });
   }
-}
+  pixelTable.addEventListener('click', function (event) {
+    let color
+    for (let i = 0; i < colors.length; i += 1) {
+      if (colors[i].classList.contains('selected')) {
+        color = colors[i].style.backgroundColor;
+      }
+    }
+    event.target.style.backgroundColor = color;
+  });
+};
