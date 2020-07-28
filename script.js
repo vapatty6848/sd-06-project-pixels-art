e18327a073dd5c78e6d0ae4c2b529b6b8b898d63
Leandro Parisi <lepcbelisario@gmail.com>
2020-07-28 17:24:02 -0300

    req 7: ajuste

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 let palette = document.querySelector("#color-palette");
+
 palette.addEventListener("click", selectPaletteColor);
 
 
@@ -8,9 +9,11 @@ let oldSelectedColor = document.querySelector(".selected");
 let currentSelectedColor = "";    
 
 function selectPaletteColor (event){
-    oldSelectedColor = document.querySelector(".selected");
-    currentSelectedColor = event.target;
+    if(event.target.className !== "color-palette"){
+        oldSelectedColor = document.querySelector(".selected");
+        currentSelectedColor = event.target;
 
-    currentSelectedColor.classList.add("selected");
-    oldSelectedColor.classList.remove("selected");
+        currentSelectedColor.classList.add("selected");
+        oldSelectedColor.classList.remove("selected");
+    }
 }
