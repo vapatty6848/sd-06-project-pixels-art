b33014dee1f64e20bb59c0b769722614d59c8df1
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-07-28 20:50:04 -0300

    Adicionado tamanho mínimo e máximo do board conforme solicitado pela etapa 11 do projeto.

--- script.js
+++ script.js
@@ -5,6 +5,44 @@ function generateBoard () {
 
   if (boardSize === "") {
     alert('Board inválido!');
+  } else if (boardSize > 50) {
+    boardSize = 50;
+    let whereCreate = document.querySelector('#pixel-board');
+
+    while (whereCreate.firstChild) {
+      whereCreate.removeChild(whereCreate.firstChild);
+    }
+
+    for (let x = 1; x <= boardSize; x += 1) {
+      let newLine = document.createElement('div');
+      newLine.className = 'pixel-line';
+
+      for (let y = 1; y <= boardSize; y += 1) {
+        let newPixel = document.createElement('div');
+        newPixel.className = 'pixel';
+        newLine.appendChild(newPixel);
+      }
+      whereCreate.appendChild(newLine);
+    }
+  } else if (boardSize < 5) {
+    boardSize = 5;
+    let whereCreate = document.querySelector('#pixel-board');
+
+    while (whereCreate.firstChild) {
+      whereCreate.removeChild(whereCreate.firstChild);
+    }
+
+    for (let x = 1; x <= boardSize; x += 1) {
+      let newLine = document.createElement('div');
+      newLine.className = 'pixel-line';
+
+      for (let y = 1; y <= boardSize; y += 1) {
+        let newPixel = document.createElement('div');
+        newPixel.className = 'pixel';
+        newLine.appendChild(newPixel);
+      }
+      whereCreate.appendChild(newLine);
+    }
   } else {
     let whereCreate = document.querySelector('#pixel-board');
 
@@ -23,7 +61,6 @@ function generateBoard () {
       }
       whereCreate.appendChild(newLine);
     }
-    console.log(whereCreate);
   }
 }
 
