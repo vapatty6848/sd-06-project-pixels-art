2a399f2962d40d8696291eeb46091f86acb8d7ed
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-07-28 18:06:29 -0300

    Resolucao do Requisito 05

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 window.onload = function () {
 	// Altera a cor selecionada
   let myColor01 = document.getElementById("color01");
-  myColor01.style.backgroundColor = "rgb(0 , 0 , 0)";
+  // myColor01.style.backgroundColor = "rgb(0 , 0 , 0)";
 	myColor01.addEventListener("click", function () {
 		chageSelected(myColor01);
 	});	
 
   let myColor02 = document.getElementById("color02");
-  myColor02.style.backgroundColor = "rgb(155 , 50 , 8)";  
+  // myColor02.style.backgroundColor = "rgb(155 , 50 , 8)";  
 	myColor02.addEventListener("click", function () {
 		chageSelected(myColor02);
-	});	
+	});
 
   let myColor03 = document.getElementById("color03");
-  myColor03.style.backgroundColor = "rgb(198 , 168 , 108)";  
+  // myColor03.style.backgroundColor = "rgb(198 , 168 , 108)";  
 	myColor03.addEventListener("click", function () {
 		chageSelected(myColor03);
 	});	
 
   let myColor04 = document.getElementById("color04");
-  myColor04.style.backgroundColor = "rgb(45 , 75 , 139)";
+  // myColor04.style.backgroundColor = "rgb(45 , 75 , 139)";
 	myColor04.addEventListener("click", function () {
 		chageSelected(myColor04);
   });	
@@ -28,8 +28,9 @@ window.onload = function () {
   
     function chageSelected(newSelected){
       let oldSelected = document.querySelector(".selected");
+      oldSelected.classList.remove("selected");
+      newSelected.classList.add("selected");
 
-      // colorSelected = window.getComputedStyle(newSelected, null).getPropertyValue("background-color");
       colorSelected = newSelected.style.backgroundColor;
       console.log(newSelected);
       console.log(newSelected.style.backgroundColor);
@@ -38,11 +39,10 @@ window.onload = function () {
       // newSelected.style.backgroundColor = "rgb(0 , 55, 100)";
       // console.log(newSelected.style.backgroundColor);
 
-	   	let myPixel = document.getElementById("pixel11");
-	   	myPixel.style.backgroundColor = colorSelected;
 
-      oldSelected.classList.remove("selected");
-      newSelected.classList.add("selected");
     }
 
 }
+
+// let myPixel = document.getElementById("pixel11");
+// myPixel.style.backgroundColor = colorSelected;
