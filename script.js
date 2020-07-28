191397c19adac16eb698f9640ae5deaa29e6c2d1
Bruna Franciele <brunafranciele1902@gmail.com>
2020-07-27 23:35:05 -0300

    requisitos 6 e 7

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-// let pintar = document.querySelectorAll(".pixel");
-// let b = document.querySelectorAll(".td");
+window.onload = function () {
+    // passamos as cores do array
+    createColorPalette(["black", "rgb(240, 49, 122)", "yellow", "purple"])
 
-//         pintar[i].addEventListener("click", function () {
+// função que cria um array de cores, para adicionar ao container color-palette
+// criando uma div com as cores que passaram no for
+function createColorPalette(colors){
+    let  colorPaletteContainer = document.getElementById("color-palette")
+    for(let index in colors) {
+    let paletteItemDiv = createPaletteItem(colors[index]);
+   colorPaletteContainer.appendChild(paletteItemDiv)
+}
+}
+// criar o item de cada cor
+function createPaletteItem(color) {
+let paletteItemDiv = document.createElement("div");
+paletteItemDiv.style.backgroundColor = color;
+paletteItemDiv.classList.add("palette-item")
+paletteItemDiv.classList.add("color")
+paletteItemDiv.addEventListener("click", handlePalletItemEvent);
 
-//             for (let i in pintar) {
-//                 pintar[i].style.backgroundColor = "yellow"; 
-//             }
-//         })
+if (color === "black") {
+    paletteItemDiv.classList.add("selected")
+}
+return paletteItemDiv
+}
+// função para disparar o evento e trocar a classe selected para a que for clicada
+function handlePalletItemEvent() {
+let oldSelectedDiv = document.querySelector(".selected");
+let currentSelectedDiv = event.target
 
-    // let pixel = document.querySelector("#primeiro")
-    // let preto = document.querySelector(".selected")
-    // pixel.addEventListener("click", function () {
-    //     pixel.style.backgroundColor = preto;
-
-
-
-    // })
-
+oldSelectedDiv.classList.remove("selected");
+currentSelectedDiv.classList.add("selected")
+}
+}
