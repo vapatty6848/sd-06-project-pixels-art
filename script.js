03d389ca5e0ece60273dda491f67152ba51a5c9a
Tiago Berwanger <berwangertiago@gmail.com>
2020-07-28 16:17:36 -0300

    Requisitos 8 e 9

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const colors = document.querySelectorAll('.color');
+const pixel = document.querySelectorAll('.pixel');
+const selected = document.querySelector('.selected');
 
-for (let index = 0; index < colors.length; index += 1) {
-   colors[index].addEventListener ('click', function (){
-       const style = window.getComputedStyle(colors[i]).backgroundColor
-   })
+let color = window.getComputedStyle(selected).backgroundColor;
+
+for (let i = 0; i < colors.length; i += 1) {
+   colors[i].addEventListener('click', function() {
+       const style = window.getComputedStyle(colors[i]).backgroundColor;
+       color = style;
+   });
 }
+
+for (let i = 0; i < pixel.length; i += 1) {
+    pixel[i].addEventListener('click', function() {
+        pixel[i].style.backgroundColor = color;
+    });  
+}
+
+function clear() {
+    for (let i = 0; i < pixel.length; i += 1) {
+        pixel[i].style.backgroundColor = 'white';   
+    }
+}
+
+const clearBoard = document.getElementById('clear-board');
+clearBoard.addEventListener('click', clear);
