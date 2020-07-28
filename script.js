590010a1f2134e36d9c38954190c6334eaebf9b6
Marthus Sera <marthus2001@gmail.com>
2020-07-28 20:12:52 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const color2 = document.getElementById('color2');
 const color3 = document.getElementById('color3');
 const color4 = document.getElementById('color4');
 const pixels = document.getElementsByClassName('pixel');
-let color;
+const clear = document.getElementById('clear-board');
+let color = 'black';
 
 color1.addEventListener('click', function() {selectColor(color1)});
 color2.addEventListener('click', function() {selectColor(color2)});
 color3.addEventListener('click', function() {selectColor(color3)});
 color4.addEventListener('click', function() {selectColor(color4)});
+clear.addEventListener('click', clearBoard);
 
 function changeClass(div) {
   const oldDiv = document.getElementsByClassName('selected')[0];
@@ -26,6 +28,12 @@ function changeColor(pixel) {
   pixel.addEventListener('click', function() {
     pixel.style.backgroundColor = color;
   });
+}
+
+function clearBoard() {
+  for(let i = 0; i < pixels.length; i++) {
+    pixels[i].style.backgroundColor = 'white';
+  }
 }
 
 for(let i = 0; i < pixels.length; i++) {
