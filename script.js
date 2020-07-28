4babf1f83fa6fbbbc77784270d462c3546bf20b1
LeonardoCavachini <cavachini240@gmail.com>
2020-07-28 17:29:02 -0300

    item 6

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+// Carregando a cor preta.
+document.getElementById('black').classList.remove('color');
+document.getElementById('black').classList.add('selected');
+};
