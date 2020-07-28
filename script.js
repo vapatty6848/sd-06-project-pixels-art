8fd674342c3a5b70a019242a2bb09907ca4cbdfe
Lais Gurgel <laisgurgelf@gmail.com>
2020-07-28 17:10:13 -0300

    algoritmo limpar quadro

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = function {
+    const clearButton = document.getElementById('clear-board');
+
+    function clearBoard() {
+        const pixels = document.querySelectorAll('.pixel');
+        for (let i = 0; i < pixels.length; i += 1) {
+          pixels[i].style.backgroundColor = '';
+        }
+      }
+    clearButton.addEventListener('click', clearBoard);
+    
+}
