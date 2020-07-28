cd467394cd251d4e9e1bf84440eecb71d94e9dbd
MCoutinho <matheuscavalcante88@hotmail.com>
2020-07-28 16:34:12 -0300

    add click event listener to color palettes

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const colorsPalette = document.querySelectorAll('.not-black');
+const colorsPaletteNotBlack = document.querySelectorAll('.not-black');
+const colorsPalette = document.querySelectorAll('.color');
 const pixelBoard = document.querySelector('#pixel-board');
+const firstColor = document.querySelector('.color');
 
 const colors = ['red', 'green', 'blue'];
 
 const generatePaletteColors = () => {
-  colorsPalette.forEach((item, index) => {
+  colorsPaletteNotBlack.forEach((item, index) => {
     item.style.backgroundColor = colors[index];
   });
 };
 
-const generateBoard = () => {
-  for (let i = 0; i < 5; i += 1) {
+const generateBoard = (number = 5) => {
+  for (let i = 0; i < number; i += 1) {
     const lineDiv = document.createElement('div');
     lineDiv.className = 'line';
     pixelBoard.appendChild(lineDiv);
-    for (let j = 0; j < 5; j += 1) {
+    for (let j = 0; j < number; j += 1) {
       const pixel = document.createElement('div');
       pixel.className = 'pixel';
       lineDiv.appendChild(pixel);
@@ -22,7 +24,27 @@ const generateBoard = () => {
   }
 };
 
+const removeClassSelected = () => {
+  colorsPalette.forEach((item) => item.classList.remove('selected'));
+};
+
+const getSelectedColor = () => {
+  colorsPalette.forEach((item) => {
+    item.addEventListener('click', () => {
+      removeClassSelected();
+      if (!item.classList.contains('selected')) {
+        item.classList.add('selected');
+      } else {
+        item.classList.remove('selected');
+      }
+    });
+  });
+};
+
 window.onload = () => {
   generatePaletteColors();
+  firstColor.style.backgroundColor = 'black';
+  firstColor.classList.add('selected');
   generateBoard();
+  getSelectedColor();
 };
