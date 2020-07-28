ae431a106836b3e9620a6bcbdacead155e41d9f2
us19861229c <rcks.santos@gmail.com>
2020-07-28 18:31:52 -0300

    Acrescenta a paleta, com suas respectivas cores como cor de fundo.

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+  const primeiroItemPaleta = document.querySelectorAll('li')[0];
+  primeiroItemPaleta.style.backgroundColor = 'black';
+
+  const segundoItemPaleta = document.querySelectorAll('li')[1];
+  segundoItemPaleta.style.backgroundColor = 'red';
+
+  const terceiroItemPaleta = document.querySelectorAll('li')[2];
+  terceiroItemPaleta.style.backgroundColor = 'green';
+
+  const quartoItemPaleta = document.querySelectorAll('li')[3];
+  quartoItemPaleta.style.backgroundColor = 'blue';
+}
