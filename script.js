0b961079d27b87f227bcbaf9bd41fa3222bbee9a
Matheus Dias <matheuscara2000@gmail.com>
2020-07-28 18:52:32 -0300

    Adicionando Parametro 2

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 
 function desenhandoBlocos(){
-    let bloco = document.getElementById("blocos-Pixel")
+    let bloco = document.getElementById("pixel-board")
     for (let i=0;i<5;i++){
+        let linha = document.createElement("div")
+        bloco.appendChild(linha)
         for (let o=0;o<5;o++){
             let blocoUnico=document.createElement("div")
             blocoUnico.className="pixel"
             blocoUnico.style.background="white"
             blocoUnico.style.width="50px"
             blocoUnico.style.height="50px"
-            bloco.appendChild(blocoUnico)
+            linha.appendChild(blocoUnico)
         }
     }
+}
+
+function selectColor(){
+
 }
 window.onload = desenhandoBlocos();
