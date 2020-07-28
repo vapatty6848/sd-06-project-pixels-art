a0c4354388323b2c027b853f334893812f75f4c5
LughWalle <civalfran1@gmail.com>
2020-07-27 20:59:15 -0500

    cores a retiradas

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+  //'greenyellow', 'orange', 'violet', 'cyan', 'blueviolet', 'chartreuse', 'chocolate', 'coral', 'gold', 'goldenrod', 'grey', 'black'
 // variaveis
 const colorPalette = document.querySelector('#color-palette');
-let colors = ['blue', 'green', 'greenyellow', 'yellow', 'red', 'orange', 'violet', 'cyan', 'blueviolet', 'chartreuse', 'chocolate', 'coral', 'gold', 'goldenrod', 'grey', 'black'];
+let colors = ['blue', 'green', , 'yellow', 'red'];
 
 // functions and events
 for (const i in colors) {
