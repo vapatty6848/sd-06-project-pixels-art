44661979c422e67d3d480d038977bf3c82269660
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-07-28 10:35:47 -0300

    fix setBoardSize func

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 
 function clickPixel(event) {
-  console.log(sessionStorage.getItem('class'));
   event.target.classList.remove('selected', 'two', 'three', 'four');
   event.target.classList.add(sessionStorage.getItem('class'));
 }
-
 function selectColor(event) {
-  console.log(event.target.classList[1]);
   switch (event.target.classList[1]) {
     case 'two':
       sessionStorage.setItem('class', 'two');
@@ -21,23 +18,33 @@ function selectColor(event) {
       sessionStorage.setItem('class', 'selected');
   }
 }
-
 function clearBoard() {
-  for (let element of document.getElementsByClassName('pixel')) {
+  for (const element of document.getElementsByClassName('pixel')) {
     element.classList.remove('selected', 'two', 'three', 'four');
   }
 }
-
 function setBlackColor() {
   sessionStorage.setItem('class', 'selected');
 }
-
 function setBoardSize() {
-  const size = parseInt(document.getElementById('board-size').value);
+  let size = parseInt(document.getElementById('board-size').value);
   if (document.getElementById('board-size').value === '') {
-    alert("Board inválido!")
-  }
-  if (size >= 5 && size <= 60) {
+    alert('Board inválido!')
+  } else if (size < 5) {
+    size = 5;
+    for (let element of document.getElementsByClassName('pixel')) {
+      element.style.height = size + 'px';
+      element.style.width = size + 'px';
+      element.classList.remove('selected', 'two', 'three', 'four');
+    }
+  } else if (size > 60) {
+    size = 60;
+    for (let element of document.getElementsByClassName('pixel')) {
+      element.style.height = size + 'px';
+      element.style.width = size + 'px';
+      element.classList.remove('selected', 'two', 'three', 'four');
+    }
+  } else {
     for (let element of document.getElementsByClassName('pixel')) {
       element.style.height = size + 'px';
       element.style.width = size + 'px';
@@ -46,17 +53,10 @@ function setBoardSize() {
   }
   setBlackColor();
 }
-
 setBlackColor();
-
 window.onload = function () {
   document.getElementById('pixel-board').addEventListener('click', clickPixel);
   document.getElementById('color-palette').addEventListener('click', selectColor);
   document.getElementById('clear-board').addEventListener('click', clearBoard);
   document.getElementById('generate-board').addEventListener('click', setBoardSize);
 }
-
-
-
-
-
