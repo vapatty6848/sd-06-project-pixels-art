8207f2df2831f12b3bad409b13ab6c8a1b8c8c9a
LeonardoCavachini <cavachini240@gmail.com>
2020-07-28 18:40:23 -0300

    item 6 bloqueia item 2

--- script.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-// Carregando a cor preta.
-document.getElementById('black').classList.remove('color');
-document.getElementById('black').classList.add('selected');
-};
