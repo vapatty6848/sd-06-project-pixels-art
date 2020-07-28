a4454d147d40080df7a1ebef063316afb0055c50
Macarena Rodriguez <macarenar697@gmail.com>
2020-07-28 16:18:52 -0300

    vai trocar a class selected a o novo item selecionado

--- script.js
+++ script.js
@@ -7,10 +7,24 @@ window.onload = function () {
   colorsArray[2].style.backgroundColor = 'orange';
   colorsArray[3].style.backgroundColor = 'green';
 
-  //  Criacao de 
+  //  Criacao de quadrado
   for (let i = 0; i < 25; i += 1) {
     let div = document.createElement('div');
     div.className = 'pixel';
     document.querySelector('#pixel-board').appendChild(div);
+  }
+
+  //  Seleccionar cor
+  let selCor = document.querySelector('#color-palette');
+  selCor.addEventListener('click', function (event) {
+    mudarClass(event);
+  })
+
+  //  Cambiar o elemento que contem a class selected
+  function mudarClass (event) {
+    let corSelecionado = document.querySelector('.selected');
+    let corNovoSelecionado = event.target;
+    corSelecionado.classList.remove('selected');
+    corNovoSelecionado.classList.add('selected');
   }
 }
