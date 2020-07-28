65a6c93509890032f4a6215c38d5ff2225110f2f
RenatoCostaDev <renatotrybe@gmail.com>
2020-07-28 17:17:27 -0300

    01- commitando HTML/CSS/JS

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-alert('oi mundo');
+window.onload = function () {
+  let selectedColor = 'black';
+
+  //Passando array de cores como parâmetro
+  createColorPallet(['black', 'red', 'blue', 'green']);
+
+  function createColorPallet(colors){
+    let colorPalletContainer = document.getElementById("color-palette");
+
+    for (let index in colors) {
+      let palletItemDiv = createPalletItem(colors[index]);
+      colorPalletContainer.appendChild(palletItemDiv);
+    }
+  }
+  //Criando divs da paleta de cores
+  function createPalletItem(color) {
+    let palletItemDiv = document.createElement('div');
+    palletItemDiv.style.backgroundColor = color;
+    palletItemDiv.className = "color";
+    palletItemDiv.addEventListener("click", handlePalletItemEvent);
+
+    if (color === "black") {
+      palletItemDiv.classList.add("selected");
+    }
+    return palletItemDiv;
+  }
+}
