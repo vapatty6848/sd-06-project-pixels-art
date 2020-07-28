71f775239d83a59cc121ed6d7dedd334ff563724
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-07-28 18:56:06 -0300

    Implementação do requisito 9

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-let selectedColor = 'rgb(0 , 0 , 0)'; // COMEÇANDO COMO UNDEFINED! VER ISSO DEPOIS!
-console.log(selectedColor);
+let selectedColor = 'rgb(0 , 0 , 0)';
+
+function clearBoard () {
+  const allPixels = document.getElementsByClassName('pixel');
+  for (i = 0; i < allPixels.length; i += 1) {
+    const pixel = allPixels[i];
+    pixel.style.backgroundColor = 'white';
+  }
+}
 
 document.addEventListener('click', function (event) {
+  // Click on color palette
   if (event.target.classList.contains('color')) {
     // Swap selected color
     document.querySelector('.selected').classList.remove('selected');
     event.target.classList.add('selected');
-    console.log(document.querySelectorAll('.color'));
 
     // Set selected color;
     selectedColor = window.getComputedStyle(event.target, null).getPropertyValue('background-color');
-    console.log(event.target);
-    console.log(selectedColor);
   }
-
+  // Click on board cell
   if(event.target.classList.contains('pixel')) {
     event.target.style.backgroundColor = selectedColor;
+  }
+
+  // Click on Clear button;
+  if(event.srcElement.id === 'clear-board') {
+    clearBoard();
   }
 });
 
