e4b0ad473eef88ffa871d4871f7fefb5a17f5a9c
DehSartoreto <fixsartoreto@gmail.com>
2020-07-28 16:11:41 -0300

    add/rm classe .selected

--- script.js
+++ script.js
@@ -4,21 +4,37 @@ window.onload = function() {
     let colorBlack = document.querySelector(".preto");
     colorBlack.addEventListener("click", function(){
         selectedColor = "black";
+        colorBlack.classList.add("selected");
+        colorBlue.classList.remove("selected");
+        colorYellow.classList.remove("selected");
+        colorGreen.classList.remove("selected");
     })
 
     let colorBlue = document.querySelector(".azul");
     colorBlue.addEventListener("click", function(){
         selectedColor = "blue";
+        colorBlack.classList.remove("selected");
+        colorBlue.classList.add("selected");
+        colorYellow.classList.remove("selected");
+        colorGreen.classList.remove("selected");
     })
 
     let colorYellow = document.querySelector(".amarelo");
     colorYellow.addEventListener("click", function(){
         selectedColor = "yellow";
+        colorBlack.classList.remove("selected");
+        colorBlue.classList.remove("selected");
+        colorYellow.classList.add("selected");
+        colorGreen.classList.remove("selected");
     })
 
     let colorGreen = document.querySelector(".verde");
     colorGreen.addEventListener("click", function(){
         selectedColor = "green";
+        colorBlack.classList.remove("selected");
+        colorBlue.classList.remove("selected");
+        colorYellow.classList.remove("selected");
+        colorGreen.classList.add("selected");
     })
 
     let clickPixel = document.querySelectorAll(".pixel");
