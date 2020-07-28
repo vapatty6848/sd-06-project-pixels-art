39c64aa7d01fcae5ca5f47e8a29b2c456101fcbf
Elias Carvalho <eliascarvalho01@hotmail.com>
2020-07-28 19:31:00 -0300

    Code refactoring

--- script.js
+++ script.js
@@ -1,96 +1,92 @@
-const METHODS = {
-  0: 'querySelector',
-  1: 'querySelectorAll',
-  3: 'createElement',
+const METHODS = { 0: 'querySelector', 1: 'querySelectorAll', 2: 'createElement', };
+
+const DIVS = {
+  0: '.board', 1: '#color-palette', 2: '.btn-reset', 3: '#generate-board',
+  4: '.input-text', 5: '#black', 6: '.pixel', 7: '.line', 8: '.random', 9: 'div',
 };
 
-const lazyCoder = function (method, tagName) {
-  return document[`${METHODS[method]}`](tagName);
-};
+function lazyCoder(keyMethod, keyDiv) {
+  return document[`${METHODS[keyMethod]}`](DIVS[keyDiv]);
+}
 
-const COLORS = ['deeppink', 'magenta', 'deepskyblue', 'aqua', 'turquoise',
-  'azure', 'goldenrod', 'lime', 'gold', 'darkorange', 'crimson'];
-
-const divBoard = lazyCoder(0, '.board');
-const divPalette = lazyCoder(0, '#color-palette');
-const btnReset = lazyCoder(0, '.btn-reset');
-const btnGenerate = lazyCoder(0, '#generate-board');
-const generateBoard = lazyCoder(0, '.input-text');
+let divBoard = lazyCoder(0, 0);
+let pixel = lazyCoder(1, 6);
+let divWithSelection = lazyCoder(0, 5);
 const divColumns = [];
-let pixel = lazyCoder(1, '.pixel');
-let divWithSelection = lazyCoder(0, '#black');
 
 function generateElements(elements) {
+
   if (elements < 5) {
     elements = 5;
   } else if (elements > 50) {
     elements = 50;
   }
 
-  for (let i = 0; i < elements; i += 1) {
-    divColumns.push(document.createElement('div'));
-    divColumns[i].className = 'line';
-    for (let j = 0; j < elements; j += 1) {
-      const pixels = document.createElement('div');
-      pixels.className = 'pixel';
-      divColumns[i].appendChild(pixels);
-    }
-    divBoard.appendChild(divColumns[i]);
-  }
-
-  pixel = lazyCoder(1, '.pixel');
-  divColumns.forEach((g) => {
-    g.style.gridTemplateColumns = `repeat(${elements}, 40px)`;
+  let divColumns = new Array(elements).fill(0);  
+  console.log(`${divColumns.length}`);
+  
+  divColumns.forEach((e, index, array) => {  
+    e = lazyCoder(2, 9);
+    e.className = 'line';
+    array.forEach((i) => {   
+      i = lazyCoder(2, 9);
+      i.className = 'pixel';
+      i.id = 'pixel';
+      e.appendChild(i);
+    });    
+    e.style.gridTemplateColumns = `repeat(${elements}, 40px)`;
+    divBoard.appendChild(e);
   });
+  console.log(`${divColumns.length}`);
+  pixel = lazyCoder(1, 6);
 }
 
-btnGenerate.onclick = () => {
-  if (generateBoard.value === '') {
-    window.alert('Board inválido!');
-  } else {
-    lazyCoder(1, '.pixel').forEach((e) => e.remove());
-    lazyCoder(1, '.line').forEach((e) => e.remove());
-    generateElements(generateBoard.value);
-  }
-};
+function clearBoard() {
+  lazyCoder(1, 7).forEach((e) => e.remove());
+}
 
 function getCurrentColor() {
   return divWithSelection.style.backgroundColor;
 }
 
-divPalette.addEventListener('click', (e) => {
+lazyCoder(0, 3).onclick = () => {
+  if (lazyCoder(0, 4).value === '') {
+    window.alert('Board inválido!');
+  } else {
+    clearBoard();
+    generateElements(parseInt(lazyCoder(0, 4).value));
+  }
+};
+
+lazyCoder(0, 1).addEventListener('click', (e) => {
   divWithSelection.classList.remove('selected');
   e.target.classList.add('selected');
   divWithSelection = e.target;
 });
 
-divBoard.addEventListener('click', (e) => {
+lazyCoder(0, 0).addEventListener('click', (e) => {
   e.target.style.backgroundColor = getCurrentColor();
 });
 
-btnReset.addEventListener('click', () => {
+lazyCoder(0, 2).addEventListener('click', () => {
   pixel.forEach((p) => {
     p.style.backgroundColor = 'white';
   });
 });
 
 function getRandomColor() {
-  return COLORS[Math.round(Math.random() * 10)];
+  let colors = [];
+
+  for (let i = 0; i < 3; i += 1) {
+    colors.push(Math.floor(Math.random() * 255));
+  }
+
+  return `rgb(${colors[0]}, ${colors[1]}, ${colors[0]})`;
 }
 
 function changeColorToRandom() {
-  const divColor = document.querySelectorAll('.random');
-  const repeated = [getRandomColor(), getRandomColor(), getRandomColor()];
-
-  if (repeated[0] === repeated[1]) {
-    repeated[1] = getRandomColor();
-  }
-  if (repeated[1] === repeated[2]) {
-    repeated[2] = getRandomColor();
-  }
-
-  divColor.forEach((item, index) => {
-    item.style.backgroundColor = repeated[index];
+  lazyCoder(1, 8).forEach((item) => {
+    item.style.backgroundColor = getRandomColor();
   });
 }
 
