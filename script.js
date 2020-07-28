2b5784c163b1171062f4626a0c9c3e1dc0e7fea2
isabellavjs <isabellavjs@gmail.com>
2020-07-28 15:55:04 -0300

    Adding requirements 7

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-//Select the black color from the palette first
+//Add select class to the black color from the palette first
 let corPreta = document.getElementById('color-1');
 corPreta.classList.add('selected');
+
+//Add select class when clicking on the square from the palette
+let selectedColor = document.querySelectorAll('.color');
+document.addEventListener('click', function(event) {
+    if (event.target.classList = 'color') {
+        selectedColor.classList.add('selected');
+    }
+});
