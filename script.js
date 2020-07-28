51f3fa72ec85b09034dd53e6d3fe4fcd04335eea
Tiago Berwanger <berwangertiago@gmail.com>
2020-07-28 13:15:14 -0300

    alteração de table para div

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//Iniciando o JS
+const colors = document.querySelectorAll('.color');
+
+for (let index = 0; index < colors.length; index += 1) {
+   colors[index].addEventListener ('click', function (){
+       const style = window.getComputedStyle(colors[i]).backgroundColor
+   })
+}
