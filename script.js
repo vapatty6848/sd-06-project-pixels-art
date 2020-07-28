cd5540a37245d9e3121e3e80813f08ac59d9bfcc
Renan Cunha <renan2610@gmail.com>
2020-07-28 19:41:41 -0300

    adicionando classe selected nas cores, e função para pintar o quadro

--- script.js
+++ script.js
@@ -1,39 +1,57 @@
-window.onload = function () {
-  const pixelRows = document.getElementsByClassName('pixel-row');
+const pixelRows = document.getElementsByClassName('pixel-row');
+const divPallete = document.getElementById('color-palette');
+const palleteColors = ['black', 'red', 'blue', 'green'];
+let selectedColor = 'black';
 
-  function fillBoardColumn(divPixelBoard) {
-    for (let column = 0; column < pixelRows.length; column += 1) {
-      const pixelColumn = document.createElement('div');
-      pixelColumn.className = 'pixel';
-      pixelColumn.style.backgroundColor = 'rgb(255, 255, 255)';
-      pixelRows[column].appendChild(pixelColumn);
-    }
+window.onload = function() {
+  createPalleteDiv(palleteColors);
+  fillBoardRows();
+}
+
+function fillBoardColumn() {
+  for (let column = 0; column < pixelRows.length; column += 1) {
+    const pixelColumn = document.createElement('div');
+    pixelColumn.className = 'pixel';
+    pixelColumn.style.backgroundColor = 'rgb(255, 255, 255)';
+    pixelRows[column].appendChild(pixelColumn);
   }
+}
 
-  function fillBoardRows(pixelRows) {
-    for (let row = 0; row < pixelRows.length; row += 1) {
-      fillBoardColumn();
-    }
+function fillBoardRows() {
+  for (let row = 0; row < pixelRows.length; row += 1) {
+    fillBoardColumn();
   }
+}
 
-  let coolors = ['black', 'red', 'blue', 'green'];
-
-  function createPalleteDiv(coolors) {
-    let divPallete = document.getElementById('color-palette');
-
-    for (let index in coolors) {
-      let divColor = createPalleteColor(coolors[index]);
-      divPallete.appendChild(divColor);
-    }
+function createPalleteColor(color) {
+  const divColor = document.createElement('div');
+  divColor.style.backgroundColor = color;
+  divColor.className = 'color';
+  if (color === 'black') {
+    divColor.classList.add('selected');
   }
+  return divColor;
+}
 
-  function createPalleteColor(color) {
-    let divColor = document.createElement('div');
-    divColor.style.backgroundColor = color;
-    divColor.className = 'color';
-    return divColor;
+function createPalleteDiv(colors) {
+  for (let index = 0; index < colors.length; index += 1) {
+    const divColor = createPalleteColor(colors[index]);
+    divPallete.appendChild(divColor);
   }
+}
 
-  createPalleteDiv(coolors);
-  fillBoardRows(pixelRows);
-};
+divPallete.addEventListener('click', selectColorItem);
+
+function selectColorItem(event) {
+  let oldSelectedColor = document.querySelector('.selected');
+  oldSelectedColor.classList.remove('selected')
+  selectedColor = event.target.style.backgroundColor;
+  event.target.classList.add('selected');
+  console.log(selectedColor);
+}
+
+document.getElementById('pixel-board').addEventListener('click', paintingTheBoard)
+
+function paintingTheBoard(event) {
+  event.target.style.backgroundColor = selectedColor;
+}
