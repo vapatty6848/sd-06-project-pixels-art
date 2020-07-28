7d4d03bf2f6862ee9cc552cf4544d476bb5f5c60
rodrigoderkian <derkianpromo@gmail.com>
2020-07-28 19:33:27 -0300

    Adicionando cor preta ao carregar a página

--- script.js
+++
@@ -1,8 +0,0 @@
-const Clear = document.getElementById('clear-board');
-const pixel = document.querySelectorAll('.pixel');
-
-Clear.addEventListener('click', function () {
-    for (let i = 0; i < pixel.length; i += 1) {
-      pixel[i].style.backgroundColor = 'white';
-    }
-}
