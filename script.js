813b8b4cac2b060204949fb30b30cf0f72790c30
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-07-28 19:04:07 -0300

    Pequenas melhorias na formatação do código

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let selectedColor = 'rgb(0 , 0 , 0)';
 
-function clearBoard () {
+function clearBoard() {
   const allPixels = document.getElementsByClassName('pixel');
-  for (i = 0; i < allPixels.length; i += 1) {
+  for (let i = 0; i < allPixels.length; i += 1) {
     const pixel = allPixels[i];
     pixel.style.backgroundColor = 'white';
   }
@@ -19,15 +19,12 @@ document.addEventListener('click', function (event) {
     selectedColor = window.getComputedStyle(event.target, null).getPropertyValue('background-color');
   }
   // Click on board cell
-  if(event.target.classList.contains('pixel')) {
+  if (event.target.classList.contains('pixel')) {
     event.target.style.backgroundColor = selectedColor;
   }
 
   // Click on Clear button;
-  if(event.srcElement.id === 'clear-board') {
+  if (event.srcElement.id === 'clear-board') {
     clearBoard();
   }
 });
-
-
-
