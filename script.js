3861b481dbb9efa9cbc622a9883dd0dfb9ea67d5
Isadora Koga <isadorakoga710@gmail.com>
2020-07-28 16:17:09 -0300

    Começando js

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 window.onload = function() {
+
+  // Deixar o preto selecionado
+  let colorBlack = document.querySelector('#black');
+  colorBlack.classList.add('selected');
+  console.log(colorBlack);
+
   // Como funciona a seleção de cores:
   // Classe selected = Deixa a cor selecionada para pintar a div (Add class="selected" style.backgroundColor = ".color[x]")
   // Quando clica em outra cor remove classe selected da antiga e adiciona na nova (onclick -> remove selected da antiga e poe na clicada)
   // Preto já inicia com a classe selected (WebStorage infinito)
   // Apertando no pixel em branco transforma na cor seleceionada -> onclick chama funcao changeColor muda pra cor selecionada
 }
+
+const palleteColor = document.querySelector('#color-pallete');
+palleteColor.addEventListener('click', function() {
+  let oldSelectedDiv = document.querySelector('.selected');
+  let currentSelectedDiv = event.target;
+
+  oldSelectedDiv.classList.remove('selected');
+  currentSelectedDiv.classList.add('selected');
+  console.log(currentSelectedDiv);
+});
 
 function resetPixelBoard() {
   const pixels = document.querySelectorAll('.pixel');
