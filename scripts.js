9e2a88bc4fde73aa59eeb2677cf5e7790a4c5af7
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-07-27 23:53:16 -0300

    Seventh Task

--- scripts.js
+++ scripts.js
@@ -1,3 +1,44 @@
+const colorBlack = document.querySelector('.black');
+const colorRed = document.querySelector('.red');
+const colorBlue = document.querySelector('.blue');
+const colorPurple = document.querySelector('.purple');
+
 window.onload = function () {
   document.querySelector('.black').className += ' selected';
 };
+
+colorBlack.addEventListener('click', function () {
+  colorBlack.className = 'color black';
+  colorRed.className = 'color red';
+  colorBlue.className = 'color blue';
+  colorPurple.className = 'color purple';
+  colorBlack.className += ' selected';
+  document.querySelector('.selected').style.color = 'black';
+});
+
+colorRed.addEventListener('click', function () {
+  colorBlack.className = 'color black';
+  colorRed.className = 'color red';
+  colorBlue.className = 'color blue';
+  colorPurple.className = 'color purple';
+  colorRed.className += ' selected';
+  document.querySelector('.selected').style.color = 'red';
+});
+
+colorBlue.addEventListener('click', function () {
+  colorBlack.className = 'color black';
+  colorRed.className = 'color red';
+  colorBlue.className = 'color blue';
+  colorPurple.className = 'color purple';
+  colorBlue.className += ' selected';
+  document.querySelector('.selected').style.color = 'blue';
+});
+
+colorPurple.addEventListener('click', function () {
+  colorBlack.className = 'color black';
+  colorRed.className = 'color red';
+  colorBlue.className = 'color blue';
+  colorPurple.className = 'color purple';
+  colorPurple.className += ' selected';
+  document.querySelector('.selected').style.color = 'purple';
+});
