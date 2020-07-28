3b1e1557122a91889e0f892acebc8bca2e41d9c6
Arthur de Sa <arthurdesa94@gmail.com>
2020-07-28 19:03:53 -0300

    reformulando as cores

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+
 let selectedColor = 'black';
 
 window.onload=function(){
 
-    let colorList = ['black', 'red', 'green', 'blue'];
-    let pixels = document.querySelectorAll('.pixel');
-  
-    createColorPalette(colorList);
- }
+    let colorPalette = document.querySelector('#color-palette');
+    let colors = ['black', 'red', 'yellow', 'blue'];
 
+  function createColorPallet(colors) {
+    for (index in colors) {
+      let colorDivCreated = createColor(colors[index]); // funcao createColor sera declarada abaixo
+      colorPalette.appendChild(colorDivCreated);
+    }
+  };
+
+  createColorPallet(colors);
+
+  function createColor(color) { //chamando a funcao declarada acima
+    let colorDivCreated = document.createElement('div');//Criando uma div para cada color
+    colorDivCreated.className = 'color';//cirando class para todas as divs de color
+    colorDivCreated.style.backgroundColor = color;//utilizando a mesma cor para o background
+    colorDivCreated.style.border = "black 3px solid";
+    
+    return colorDivCreated;
+  };
+
+  }
 
 
