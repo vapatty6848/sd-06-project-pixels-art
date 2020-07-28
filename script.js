e3c0b8c5333e8d4c010f588d6edb8c6c032759f2
Fabio Camargos <fabiocmmcm@gmail.com>
2020-07-28 17:01:04 -0300

    Js, foi?

--- script.js
+++ script.js
@@ -10,12 +10,39 @@ window.onload = function(){
         }
     }
 
-function handlePalleteItemEvent (event){
+    function createPaletteItem (color){
+        let palleteItemDiv = document.createElement('div');
+        palleteItemDiv.style.backgroundColor = color;
+        palleteItemDiv.className = "collor";
+        palleteItemDiv.addEventListener('click', handlePalleteItemEvent);
+
+        if(color === 'black'){
+            palleteItemDiv.classList.add('selected');
+        }
+        return palleteItemDiv;
+    }
+
+    function handlePalleteItemEvent (event){
     let oldSeletecedDiv = document.querySelector(".selected");
     let currentSelectedDiv = event.target
 
     oldSeletecedDiv.classList.remove("selected");
     currentSelectedDiv.classList.add("selected");
-}
+
+    }
+    
+    document.getElementById('pixel').addEventListener('click', function(){
+      pixelColorSelected = document.querySelector('.selected');
+      let pixelBackgroundColor = pixelColorSelected.style.backgroundColor;
+      event.target.style.backgroundColor = pixelBackgroundColor;  
+    })
+
+    let btn = document.querySelector('#clear-board');
+    let pixel = document.getElementsByClassName('pixel');
+    btn.addEventListener('click', function(){
+        for (let index = 0; index < pixel.length; index += 1){
+            pixel[index].style.backgroundColor = 'white';
+        }
+    })
 
 }
