7a1f5c9259898440f7bde9d4fa31f611172ca753
Marcelo Pessini <marceloppessini@gmail.com>
2020-07-28 16:50:24 -0300

    Adicionando pixel-board

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ function createPalet(color){
 }
 onload = createPalet(color);
 
+let N = 5
+let pixelBoard = document.getElementById('pixel-board');
+function createPixelBoard(N){
+  for(let i = 1; i <= N; i += 1){
+    let createDivTr = document.createElement('div');
+    createDivTr.className = "tr";
+    pixelBoard.appendChild(createDivTr);
+    for (let i = 1; i <= N; i += 1){
+    let createDivPixel = document.createElement('div');
+    createDivPixel.style.backgroundColor = "white";
+    createDivPixel.className = "pixel";
+    createDivTr.appendChild(createDivPixel);
+    }
+  }  
+}
+onload= createPixelBoard(N);
