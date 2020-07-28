09fd4a9c686fbeb71f8a5108c4948f1e61b2a67a
Ana Flávia <anaflvia@gmail.com>
2020-07-28 11:20:32 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,34 +1,12 @@
-
-
-function createColorPallet(colors) {
-  let colorPaletContainer = document.getElementById('color-palette');
-  for (let index in colors) {
-    let palletItemDiv = createPalletItem(colors[index]);
-    colorPaletContainer.appendChild(palletItemDiv);
-  }
-}
-
-function createPalletItem(color) {
-  let palletItemDiv = document.createElement('div');
-  palletItemDiv.style.backgroundColor = color;
-  palletItemDiv.classList.add('color');
-  palletItemDiv.addEventListener('click', handlePalletItemEvent);
-
-  if (color === 'black') {
-    palletItemDiv.classList.add('selected');
-  }
-
-  return palletItemDiv;
-}
-
-function handlePalletItemEvent(event) {
-  let oldSelectedDiv = document.querySelector('.selected');
-  let currentSelectedDiv = event.target;
-
-  oldSelectedDiv.classList.remove('selected');
-  currentSelectedDiv.classList.add('selected');
-}
-
-window.onload = function () {
-  createColorPallet(['black', 'red', 'blue', 'green']);
-}
+let selectedElement = document.getElementById('color-palette')
+selectedElement.addEventListener('click', function() {
+  let oldSelectedElement = document.querySelector('.selected');
+  oldSelectedElement.classList.remove('selected');
+  let currentSelectedElement = event.target;
+  currentSelectedElement.classList.add('selected');
+})
+let selectedPixel = document.getElementById('pixel-board')
+selectedPixel.addEventListener('click', function() {
+  let pixelColorSelected = document.querySelector('.selected').id;
+  event.target.style.backgroundColor = pixelColorSelected;
+})
