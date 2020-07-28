cf8789774b37717fb0d9a0f18365686f5c26c418
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-07-28 17:58:34 -0300

    Added function for pixel board creation

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
 window.onload = function () {
   const colorList = ['black', 'red', 'green', 'blue'];
-  const pixels = document.querySelectorAll('.pixel');
+  // const pixels = document.querySelectorAll('.pixel');
 
   createColorPalette(colorList);
+
+  createPixelBoard(5);
 };
 
 function createColorSelector(color) {
-  let selectorDivElement = document.createElement('div');
+  const selectorDivElement = document.createElement('div');
   selectorDivElement.className = 'color';
   selectorDivElement.style.backgroundColor = color;
-  selectorDivElement.style.display = "table-cell";
+  selectorDivElement.style.display = 'table-cell';
   return selectorDivElement;
-};
+}
 
 function createColorPalette(colors) {
   const paletteContainer = document.getElementById('color-palette');
-  
-  for (let i in colors) {
-    let colorSelector = createColorSelector(colors[i]);
+  for (let i = 0; i < colors.length; i += 1) {
+    const colorSelector = createColorSelector(colors[i]);
     paletteContainer.appendChild(colorSelector);
   }
-};
+}
+
+function createPixel() {
+  const pixel = document.createElement('div');
+  pixel.className = 'pixel';
+  return pixel;
+}
+
+function createRow(num) {
+  const pixelRow = document.createElement('div');
+  for (let i = 0; i < num; i += 1) {
+    pixelRow.appendChild(createPixel());
+  }
+  return pixelRow;
+}
+
+function createPixelBoard(n) {
+  const pixelBoardContainer = document.querySelector('#pixel-board');
+  for (let i = 0; i < n; i += 1) {
+    pixelBoardContainer.appendChild(createRow(n));
+  }
+}
