7095e325a7a43f3e0c57313b3d8df22be8d2e89e
Macarena Rodriguez <macarenar697@gmail.com>
2020-07-27 21:30:58 -0300

    Comencando fazer quadrado

--- script.js
+++ script.js
@@ -5,5 +5,18 @@ window.onload = function () {
   colorsArray[1].style.backgroundColor = 'red';
   colorsArray[2].style.backgroundColor = 'orange';
   colorsArray[3].style.backgroundColor = 'green';
-  
+
+  let quadrado = document.querySelector('.pixel');
+  let quadradoTotal = document.querySelector('#pixel-board');
+  for (let i = 0; i < 4; i += 1) {
+    
+    for (let i = 0; i < 4; i += 1) {
+      
+      let div = document.createElement('div');
+      div.className = 'pixel';
+      document.querySelector('#pixel-board').appendChild(div);
+      
+    }  
+    div.style.width = '50px'
+  }
 }
