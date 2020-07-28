8571293ab736376407e7fa298162af8a87b23c8e
isabellavjs <isabellavjs@gmail.com>
2020-07-28 20:15:33 -0300

    Corrections pallet container - creating colorItensDiv

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function createPaletteItens(color) {
     let colorItensDiv = document.createElement('div');
     colorItensDiv.style.backgroundColor = color;
     colorItensDiv.className = 'palette-itens';
+    colorItensDiv.classList.add(color);
     colorItensDiv.addEventListener('click', handlePaletteItenEvent);
     return colorItensDiv;
 }
