56b4cf1304efaf70e8a0f1c3ba27a1f4d17ce45a
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-07-28 19:59:08 -0300

    bugs fixed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const colors = ['black', 'red', 'orange', 'yellow']
+const colors = ['black', 'red', 'orange', 'yellow'];
 const squarePaletteSize = '50px';
 const squareBoardSize = '40px';
 
@@ -12,16 +12,44 @@ for (let i = 0; i < 4; i += 1) {
   createColorClassDiv.style.border = '1px solid black';
 
   document.getElementById('color-palette').appendChild(createColorClassDiv);
-};
-
-for (let i = 0; i < 25; i += 1) {
-  let createColorClassDiv = document.createElement('div');
-  createColorClassDiv.className = 'pixel';
-  createColorClassDiv.style.width = squareBoardSize;
-  createColorClassDiv.style.height = squareBoardSize;
-  createColorClassDiv.style.backgroundColor = 'white';
-  createColorClassDiv.style.float = 'left';
-  createColorClassDiv.style.border = '1px solid black';
-
-  document.getElementById('pixel-board').appendChild(createColorClassDiv);
 }
+
+for (let i = 0; i < 5; i += 1) {
+  const lineDiv = document.createElement('div');
+  lineDiv.className = 'pixel-wrapp';
+  lineDiv.width = (colors.length * squareBoardSize) + (colors.length * 2);
+  document.getElementById('pixel-board').appendChild(lineDiv);
+
+  for (let j = 0; j < 5; j += 1) {
+    let createColorClassDiv = document.createElement('div');
+    createColorClassDiv.className = 'pixel';
+    createColorClassDiv.style.width = squareBoardSize;
+    createColorClassDiv.style.height = squareBoardSize;
+    createColorClassDiv.style.backgroundColor = 'white';
+    createColorClassDiv.style.float = 'left';
+    createColorClassDiv.style.border = '1px solid black';
+
+
+    document.getElementsByClassName('pixel-wrapp')[i].appendChild(createColorClassDiv);
+  }
+}
+
+
+// for (let i = 0; i < 5; i += 1) {
+//   // const lineDiv = document.createElement('div');
+//   // lineDiv.id = 'pixel-wrapp';
+//   document.getElementById('pixel-board').appendChild(lineDiv);
+//   for (let i = 0; i < 5; i += 1) {
+//     let createColorClassDiv = document.createElement('div');
+//     createColorClassDiv.className = 'pixel';
+//     createColorClassDiv.style.width = squareBoardSize;
+//     createColorClassDiv.style.height = squareBoardSize;
+//     createColorClassDiv.style.backgroundColor = 'white';
+//     createColorClassDiv.style.float = 'left';
+//     createColorClassDiv.style.border = '1px solid black';
+
+
+//     document.getElementById('pixel-wrapp').appendChild(createColorClassDiv);
+//   }
+// }
+// document.getElementById('')
