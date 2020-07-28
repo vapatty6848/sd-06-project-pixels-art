349251d08b22af025784cb379f9c227c06203a10
FlavioSugano <flavio.sugano@gmail.com>
2020-07-28 19:26:00 -0300

    Adicionada função p/ inserir a cor selecionada no pixel do pixel board

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 let selectedColor = 'Black';
+let palette = document.getElementsByClassName('color');
+let pixelBoard = document.getElementsByClassName('pixel');
 
 window.onload = function () {
   
   // captura a cor após o click na paleta de cores
-  let palette = document.getElementsByClassName('color')
   for (let i = 0; i < palette.length; i += 1) {
     palette[i].addEventListener('click', function () {
       selectedColor = palette[i].style.backgroundColor;
       console.log(selectedColor)
     })
   }
+
+  for (let i = 0; i < pixelBoard.length; i += 1) {
+    pixelBoard[i].addEventListener('click', function () {
+      pixelBoard[i].style.backgroundColor = selectedColor;
+    })
+  }
+
+
+  
 
 
 
