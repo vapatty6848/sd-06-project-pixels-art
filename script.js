55ec4e41a6cac68845911d2f39881ca9030e3481
Macarena Rodriguez <macarenar697@gmail.com>
2020-07-28 16:37:45 -0300

    Se coloco funcion para selecionar cor e pintar quadrado e comenzar a pintar o quadrado de preto antes de selecionar

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 window.onload = function () {
   //  Criacao da paleta com os correspondentes cores
   let color_Palette = document.querySelector('#color-palette');
@@ -7,6 +8,8 @@ window.onload = function () {
   colorsArray[2].style.backgroundColor = 'orange';
   colorsArray[3].style.backgroundColor = 'green';
 
+  
+  
   //  Criacao de quadrado
   for (let i = 0; i < 25; i += 1) {
     let div = document.createElement('div');
@@ -15,10 +18,13 @@ window.onload = function () {
   }
 
   //  Seleccionar cor
-  let selCor = document.querySelector('#color-palette');
-  selCor.addEventListener('click', function (event) {
+  let palette = document.querySelector('#color-palette');
+  palette.addEventListener('click', function (event) {
     mudarClass(event);
+    color = event.target.style.backgroundColor;
+    colocarCor(color);
   })
+  
 
   //  Cambiar o elemento que contem a class selected
   function mudarClass (event) {
@@ -26,5 +32,15 @@ window.onload = function () {
     let corNovoSelecionado = event.target;
     corSelecionado.classList.remove('selected');
     corNovoSelecionado.classList.add('selected');
+  }
+
+  //  colocar cor na palete
+  let color = 'black';
+  colocarCor(color);
+  function colocarCor (color) {
+    let quadrado = document.querySelector('#pixel-board');
+    quadrado.addEventListener('click', function (event) {
+      event.target.style.backgroundColor = color;
+    });
   }
 }
