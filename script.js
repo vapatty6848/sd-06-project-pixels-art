3abfd0db13bab761733f741575808653ca076fee
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-07-29 00:30:21 -0300

    pixel art

--- script.js
+++ script.js
@@ -4,12 +4,46 @@ window.onload=function(){
 
     localStorage.clear();
     localStorage.setItem("selected-color", "black");
+
 }
+
+//add class selected ao elemento que recebeu clique e remover dos outros elementos
+
+//se o botao com id x receber onclick, adiciona selected nele e remove selected dos outros elementos.
 
 //select color-buttons
 
 function selectColor(color){
+
     localStorage.setItem("selected-color", color);
+
+    if(localStorage.getItem("selected-color")=="rgb(113, 222, 226)"){
+        document.getElementById("blue").classList.add("selected")
+        document.getElementById("black").classList.remove("selected")
+        document.getElementById("yellow").classList.remove("selected")
+        document.getElementById("red").classList.remove("selected")
+    }
+
+    else if (localStorage.getItem("selected-color")=="rgb(245, 131, 116)"){
+        document.getElementById("red").classList.add("selected")
+        document.getElementById("black").classList.remove("selected")
+        document.getElementById("yellow").classList.remove("selected")
+        document.getElementById("blue").classList.remove("selected")
+    }
+
+    else if (localStorage.getItem("selected-color")=="rgb(245, 210, 116)"){
+        document.getElementById("yellow").classList.add("selected")
+        document.getElementById("black").classList.remove("selected")
+        document.getElementById("blue").classList.remove("selected")
+        document.getElementById("red").classList.remove("selected")
+    }
+
+    else if (localStorage.getItem("selected-color")=="black"){
+        document.getElementById("black").classList.add("selected")
+        document.getElementById("blue").classList.remove("selected")
+        document.getElementById("yellow").classList.remove("selected")
+        document.getElementById("red").classList.remove("selected")
+    }
 }
 
 //
@@ -51,6 +85,8 @@ for (let i=0; i < pixels.length; i++) {
         pasteColor(pixels[i], localStorage.getItem("selected-color"));
     }
 }
+
+
 
 //clear-button
 
