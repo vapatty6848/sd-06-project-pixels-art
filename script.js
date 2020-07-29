37c3eb0f880a34e577fe55b704ac968e8a56555d
us19861229c <rcks.santos@gmail.com>
2020-07-29 16:16:46 -0300

    Reposiciona o botão Limpar. Cria a interação de colorir os quadros brancos com a cor que possua a classe 'selected'.

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 window.onload = function () {
   const listaDeItensPaleta = document.querySelectorAll('li');
+  const apagaTudo = document.querySelector('#clear-board');
+  let corSelecionada = document.querySelector('.selected');
+  const todosQuadros = document.getElementsByTagName('td');
 
   for (let indice = 0; indice < listaDeItensPaleta.length; indice += 1) {
     listaDeItensPaleta[indice].addEventListener('click', function () {
       for (let ordem = 0; ordem < listaDeItensPaleta.length; ordem += 1) {
-        // console.log(listaDeItensPaleta[ordem].classList[1]);
         listaDeItensPaleta[ordem].classList.remove('selected');
       }
       this.classList.add('selected');
+      corSelecionada = document.querySelector('.selected');
     });
   }
 
-  const apagaTudo = document.querySelector('#clear-board');
-  const todosQuadros = document.getElementsByTagName('td');
+  for (let indice = 0; indice < todosQuadros.length; indice += 1) {
+    todosQuadros[indice].addEventListener('click', function () {
+      todosQuadros[indice].className = `pixel ${corSelecionada.classList[1]}`;
+    });
+  }
+
   apagaTudo.addEventListener('click', function () {
     for (let indice = 0; indice < todosQuadros.length; indice += 1) {
       todosQuadros[indice].className = 'pixel white';
