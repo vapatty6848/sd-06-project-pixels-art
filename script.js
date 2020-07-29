7e7d69b57cf2760bd58217a4ca6d1940afc8a1f6
us19861229c <rcks.santos@gmail.com>
2020-07-28 22:16:55 -0300

    Adiciona id a tag h1, modifica a atribuição de cores para o .css, cria o painel com os quadrados a serem pintados, cria as classes de cores e pixel.

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 window.onload = function () {
-  const primeiroItemPaleta = document.querySelectorAll('li')[0];
-  primeiroItemPaleta.style.backgroundColor = 'black';
 
-  const segundoItemPaleta = document.querySelectorAll('li')[1];
-  segundoItemPaleta.style.backgroundColor = 'red';
-
-  const terceiroItemPaleta = document.querySelectorAll('li')[2];
-  terceiroItemPaleta.style.backgroundColor = 'green';
-
-  const quartoItemPaleta = document.querySelectorAll('li')[3];
-  quartoItemPaleta.style.backgroundColor = 'blue';
-}
+};
