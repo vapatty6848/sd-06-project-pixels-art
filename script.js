fe6e38ccf3777ea5a48fb3abea0e10ee97254c0e
us19861229c <rcks.santos@gmail.com>
2020-07-29 16:36:47 -0300

    Corrige alguns erros apontados pelo codeclimate referentes a função dentro de loop.

--- script.js
+++ script.js
@@ -4,20 +4,24 @@ window.onload = function () {
   let corSelecionada = document.querySelector('.selected');
   const todosQuadros = document.getElementsByTagName('td');
 
+  function toggleSelect() {
+    for (let ordem = 0; ordem < listaDeItensPaleta.length; ordem += 1) {
+      listaDeItensPaleta[ordem].classList.remove('selected');
+    }
+    this.classList.add('selected');
+    corSelecionada = document.querySelector('.selected');
+  }
+
+  function paintPixel() {
+    this.className = `pixel ${corSelecionada.classList[1]}`;
+  }
+
   for (let indice = 0; indice < listaDeItensPaleta.length; indice += 1) {
-    listaDeItensPaleta[indice].addEventListener('click', function () {
-      for (let ordem = 0; ordem < listaDeItensPaleta.length; ordem += 1) {
-        listaDeItensPaleta[ordem].classList.remove('selected');
-      }
-      this.classList.add('selected');
-      corSelecionada = document.querySelector('.selected');
-    });
+    listaDeItensPaleta[indice].addEventListener('click', toggleSelect);
   }
 
   for (let indice = 0; indice < todosQuadros.length; indice += 1) {
-    todosQuadros[indice].addEventListener('click', function () {
-      todosQuadros[indice].className = `pixel ${corSelecionada.classList[1]}`;
-    });
+    todosQuadros[indice].addEventListener('click', paintPixel);
   }
 
   apagaTudo.addEventListener('click', function () {
