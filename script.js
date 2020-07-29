9a5c053f7a60c46fa4294e3f41e371ae72541911
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-07-28 23:53:06 -0300

    Step 10 - Bonus - Done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const colors = ['black', 'red', 'orange', 'yellow'];
 const squarePaletteSize = 50;
 const squareBoardSize = 40;
 const palettesSize = 4;
-const boardSize = 5;
+var boardSize = 5;
 
 for (let i = 0; i < 4; i += 1) {
   const createColorClassDiv = document.createElement('div');
@@ -16,39 +16,8 @@ for (let i = 0; i < 4; i += 1) {
   document.getElementById('color-palette').appendChild(createColorClassDiv);
 }
 
-for (let i = 0; i < boardSize; i += 1) {
-  const lineDiv = document.createElement('div');
-  lineDiv.className = 'pixel-wrapp';
-  lineDiv.width = (colors.length * squareBoardSize) + (colors.length * 2);
-  document.getElementById('pixel-board').appendChild(lineDiv);
-
-  for (let j = 0; j < boardSize; j += 1) {
-    const createColorClassDiv = document.createElement('div');
-    createColorClassDiv.className = 'pixel';
-    createColorClassDiv.style.width = `${squareBoardSize}px`;
-    createColorClassDiv.style.height = `${squareBoardSize}px`;
-    createColorClassDiv.style.backgroundColor = 'white';
-    createColorClassDiv.style.float = 'left';
-    createColorClassDiv.style.border = '1px solid black';
-
-    document.getElementsByClassName('pixel-wrapp')[i].appendChild(createColorClassDiv);
-  }
-}
-
-const sidePaletteSize = ((palettesSize * squarePaletteSize) + ((palettesSize) * 2));
-const divIdColorPalette = document.getElementById('color-palette');
-divIdColorPalette.style.border = '1px solid black';
-divIdColorPalette.style.width = `${sidePaletteSize}px`;
-divIdColorPalette.style.height = `${squarePaletteSize + 2}px`;
-
-const sideBoardSize = ((boardSize * squareBoardSize) + ((boardSize) * 2));
-const divIdPixelBoard = document.getElementById('pixel-board');
-divIdPixelBoard.style.border = '1px solid black';
-divIdPixelBoard.style.width = `${sideBoardSize}px`;
-divIdPixelBoard.style.height = `${sideBoardSize}px`;
-
 function deleteAllSelected() {
-  document.querySelectorAll('.color').forEach(item => {
+  document.querySelectorAll('.color').forEach((item) => {
     for (let i = 0; i < item.classList.length; i += 1) {
       if (item.classList[i] === 'selected') {
         item.classList.remove(item.classList[i]);
@@ -57,21 +26,55 @@ function deleteAllSelected() {
   });
 }
 
-document.querySelectorAll('.color').forEach(item => {
-  item.addEventListener('click', event => {
-    deleteAllSelected();
-    event.target.className += ' selected';
-  });
-});
+createNewBoard();
 
-document.querySelectorAll('.color')[0].className += ' selected';
+function createNewBoard() {
+  for (let i = 0; i < boardSize; i += 1) {
+    const lineDiv = document.createElement('div');
+    lineDiv.className = 'pixel-wrapp';
+    lineDiv.width = (colors.length * squareBoardSize) + (colors.length * 2);
+    document.getElementById('pixel-board').appendChild(lineDiv);
 
-document.querySelectorAll('.pixel').forEach(box => {
-  box.addEventListener('click', function () {
-    const color = document.querySelector('.selected').style.backgroundColor;
-    box.style.backgroundColor = color;
+    for (let j = 0; j < boardSize; j += 1) {
+      const createColorClassDiv = document.createElement('div');
+      createColorClassDiv.className = 'pixel';
+      createColorClassDiv.style.width = `${squareBoardSize}px`;
+      createColorClassDiv.style.height = `${squareBoardSize}px`;
+      createColorClassDiv.style.backgroundColor = 'white';
+      createColorClassDiv.style.float = 'left';
+      createColorClassDiv.style.border = '1px solid black';
+
+      document.getElementsByClassName('pixel-wrapp')[i].appendChild(createColorClassDiv);
+    }
+  }
+  const sidePaletteSize = ((palettesSize * squarePaletteSize) + ((palettesSize) * 2));
+  const divIdColorPalette = document.getElementById('color-palette');
+  divIdColorPalette.style.border = '1px solid black';
+  divIdColorPalette.style.width = `${sidePaletteSize}px`;
+  divIdColorPalette.style.height = `${squarePaletteSize + 2}px`;
+
+  const sideBoardSize = ((boardSize * squareBoardSize) + ((boardSize) * 2));
+  const divIdPixelBoard = document.getElementById('pixel-board');
+  divIdPixelBoard.style.border = '1px solid black';
+  divIdPixelBoard.style.width = `${sideBoardSize}px`;
+  divIdPixelBoard.style.height = `${sideBoardSize}px`;
+
+  document.querySelectorAll('.color').forEach((item) => {
+    item.addEventListener('click', (event) => {
+      deleteAllSelected();
+      event.target.className += ' selected';
+    });
   });
-});
+
+  document.querySelectorAll('.color')[0].className += ' selected';
+
+  document.querySelectorAll('.pixel').forEach((box) => {
+    box.addEventListener('click', function () {
+      const color = document.querySelector('.selected').style.backgroundColor;
+      box.style.backgroundColor = color;
+    });
+  });
+}
 
 const btClean = document.getElementById('clear-board');
 btClean.addEventListener('click', function () {
@@ -79,4 +82,18 @@ btClean.addEventListener('click', function () {
   for (let i = 0; i < allBoxes.length; i += 1) {
     allBoxes[i].style.backgroundColor = 'white';
   }
+});
+
+const btGenerateBoard = document.getElementById('generate-board');
+btGenerateBoard.addEventListener('click', function () {
+  boardSize = document.getElementById('board-size').value;
+  console.log(boardSize);
+  if (boardSize.length !== 0) {
+    console.log(boardSize.length);
+    document.getElementById('pixel-board').innerHTML = '';
+    createNewBoard();
+  } else {
+    alert('Board inválido!');
+  }
+
 });
