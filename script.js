458a0ebb5fef49aa2d66865ad9c689ea8468371b
luiz <phellipe501@gmail.com>
2020-07-29 15:40:29 -0300

    random colors

--- script.js
+++ script.js
@@ -7,7 +7,14 @@ var blue = document.querySelector(".blue");
 var colors = document.querySelectorAll(".color")
 var clear = document.querySelector("#clear-board")
 var quadrados = document.querySelectorAll(".pixel")
+var randonC1 = Math.floor(Math.random()*16777215).toString(16);
+var randonC2 = Math.floor(Math.random()*16777215).toString(16);
+var randonC3 = Math.floor(Math.random()*16777215).toString(16);
 
+
+colors[1].style.backgroundColor = "#" +randonC1;
+colors[2].style.backgroundColor = "#" +randonC2;
+colors[3].style.backgroundColor = "#" +randonC3;
 black.addEventListener("click", function(){
     for(i = 0; i < colors.length; i += 1){
         if(colors[i].classList[2] == "selected"){
@@ -60,11 +67,11 @@ pixelBoard.addEventListener("click", function(event){
     if(black.classList[2] == "selected"){
     event.target.style.backgroundColor = "black"
     }else if(red.classList[2] == "selected"){
-    event.target.style.backgroundColor = "red";
+    event.target.style.backgroundColor = "#" +randonC1;
     }else if(green.classList[2] == "selected"){
-        event.target.style.backgroundColor = "green";
+        event.target.style.backgroundColor = "#" +randonC2;
     }else if(blue.classList[2] == "selected"){
-            event.target.style.backgroundColor = "blue";
+            event.target.style.backgroundColor = "#" +randonC3;
     }
 
 })
