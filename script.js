5d82f82f7e1279d3354efe41e2a3a353dedcbf5a
ricknois <rickssti@outlook.com>
2020-07-29 03:32:23 -0300

    Code Climate

--- script.js
+++ script.js
@@ -3,24 +3,24 @@ const colorPalet = document.querySelector('#color-palette');
 const btnClear = document.querySelector('#clear-board');
 const btnShow = document.querySelector('#generate-board');
 const inputNum = document.querySelector('#board-size');
-const sectionTable = document.querySelector('.sectionPB')
+const sectionTable = document.querySelector('.sectionPB');
 let classSel = 'preto';
 let sizeBox = 5;
 
 function createPixelBoard() {
-  let createFather = document.createElement('section')
+  const createFather = document.createElement('section');
   pixelBoard.appendChild(createFather);
-  createFather.setAttribute('class', 'father')
+  createFather.setAttribute('class', 'father');
   for (let j = 0; j < sizeBox; j += 1) {
-    let createSection = document.createElement('section');
+    const createSection = document.createElement('section');
     createSection.setAttribute('class', 'sectionPB');
     createFather.appendChild(createSection);
   }
 
   const sect = document.getElementsByClassName('sectionPB');
-  for (let l = 0; l < sizeBox; l++) {
+  for (let l = 0; l < sizeBox; l += 1) {
     for (let i = 0; i < sizeBox; i += 1) {
-      let createDiv = document.createElement('div')
+      const createDiv = document.createElement('div');
       createDiv.setAttribute('class', 'pixel bgbranco')
       sect[l].appendChild(createDiv);
     }
@@ -28,23 +28,23 @@ function createPixelBoard() {
 }
 
 colorPalet.addEventListener('click', function (event) {
-  classSel = event.target.classList[1]
+  classSel = event.target.classList[1];
   for (const j of document.querySelectorAll('.color')) {
     if (j.classList.contains('selected')) {
-      j.classList.remove('selected')
+      j.classList.remove('selected');
     }
   }
-  event.target.classList.add('selected')
-})
+  event.target.classList.add('selected');
+});
 
 pixelBoard.addEventListener('click', function (event) {
   if (event.target.classList.contains('pixel')) {
     event.target.className = '';
-    event.target.classList.add('pixel')
-    event.target.classList.add(classSel)
-    event.target.classList.remove('bgbranco')
+    event.target.classList.add('pixel');
+    event.target.classList.add(classSel);
+    event.target.classList.remove('bgbranco');
   }
-})
+});
 
 btnClear.addEventListener('click', function () {
   for (const j of document.querySelectorAll('.pixel')) {
@@ -52,22 +52,22 @@ btnClear.addEventListener('click', function () {
       j.className = 'pixel bgbranco'
     }
   }
-})
+});
 
 btnShow.addEventListener('click', function () {
   const inputVal = inputNum.value;
   if (inputVal === '') {
     alert('Board inválido!')
-  } else if(inputVal < 5) {
-    sizeBox = 5; 
-  } else if(inputVal > 50){
+  } else if (inputVal < 5) {
+    sizeBox = 5;
+  } else if (inputVal > 50) {
     sizeBox = 50;
   } else {
     sizeBox = inputVal;
   }
-  const no = document.querySelector('.father')
+  const no = document.querySelector('.father');
   if (no.parentNode) {
     no.parentNode.removeChild(no)
     createPixelBoard();
   }
-})
+});
