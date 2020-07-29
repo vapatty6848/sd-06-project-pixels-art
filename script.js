b5ffeec3656665fd801c66ddc95fb53f0525bbb6
CleberFontinele <cleberfontinele2387@gmail.com>
2020-07-29 09:11:44 -0300

    Crie um botão-O botão deve estar posicionado entre a paleta de cores e o quadro de pixels

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.onclick = function () {
   const selection = document.querySelector('.selected');
-  	if (event.target.className === 'color') {
+  if (event.target.className === 'color') {
     event.target.classList.add('selected');
     selection.classList.remove('selected');
   }
   if (event.target.className === 'pixel') {
-  	event.target.style.backgroundColor = selection.id;
-}
+    event.target.style.backgroundColor = selection.id;
+  }
 };
