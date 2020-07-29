b6866e7ae2bfe27fdba6cc18f8f044579cbab400
Micaeli Teixeira <luime6@gmail.com>
2020-07-28 21:38:00 -0300

    fazendo alterações do codeclimate

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+let selectedColor = 'black';
 window.onload = function () {
- 
-/* add cor */
+ /* add cor */
   document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
   document.getElementsByClassName('color')[1].style.backgroundColor = 'red';
   document.getElementsByClassName('color')[2].style.backgroundColor = 'blue';
@@ -12,20 +12,20 @@ window.onload = function () {
 
 /* função para disparar o evento e trocar a classe selected para a que for clicada */
   function handlePalletItemEvent() {
-  const oldSelectedDiv = document.querySelector('.selected');
-  const currentSelectedDiv = event.target;
+    const oldSelectedDiv = document.querySelector('.selected');
+    const currentSelectedDiv = event.target;
 
-  oldSelectedDiv.classList.remove('selected');
-  currentSelectedDiv.classList.add('selected');
+    oldSelectedDiv.classList.remove('selected');
+    currentSelectedDiv.classList.add('selected');
 
-  selectedColor = currentSelectedDiv.style.backgroundColor;
-}
-
+    selectedColor = currentSelectedDiv.style.backgroundColor;
+  }
+  
 /* colorindo os pixels */
   function handlePixel(event) {
-  const selectedPixelDiv = event.target;
-  selectedPixelDiv.style.backgroundColor = selectedColor;
-}
+    const selectedPixelDiv = event.target;
+    selectedPixelDiv.style.backgroundColor = selectedColor;
+  }
 
 /* individualizando os pixels para colorir */
   const pixelBoardDiv = document.getElementById('pixel-board');
@@ -35,9 +35,9 @@ window.onload = function () {
 /* Button reset color */
   const buttonReset = document.querySelector('#clear-board');
   buttonReset.addEventListener('click', function () {
-  const colorPixels = document.querySelectorAll('.pixel');
-  for (let index = 0; index < colorPixels.length; index += 1) {
+    const colorPixels = document.querySelectorAll('.pixel');
+    for (let index = 0; index < colorPixels.length; index += 1) {
     colorPixels[index].style.backgroundColor = 'white';
   }
-});
+  });
 };
