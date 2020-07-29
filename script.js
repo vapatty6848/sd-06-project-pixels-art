2a97aa26829b4ac3cbae4aeb1199d5e9718f5c84
Alessandra <itsalematos@hotmail.com>
2020-07-29 08:34:11 -0400

    Finalizando requisito 7 e 8, refatorando o código

--- script.js
+++ script.js
@@ -1,19 +1,69 @@
-let pixelBoard = document.querySelector('#pixel-board');
-let preto = document.querySelector('#black')
+let selectedColor = 'black';
 
+window.onload = function(){
 
-for (let i = 0; i < 5; i++){
-    let linha = document.createElement('div');
-    linha.className = 'row';
-    pixelBoard.appendChild(linha);
-    for (let j = 0; j < 5; j++){
-        let coluna = document.createElement('div');
-        coluna.className = 'pixel';
-        coluna.id= 'column'
-        linha.appendChild(coluna);
+   
+    createColorPallet(['black', 'red', 'blue', 'green']); //aqui adiciono quantas cores quiser
+
+            // aqui ele vai criar a paleta de cores, vai chamar o array e criar uma paleta para cada cor do array, para criar cada pelate ele chama a função createPalletItem, que é específica só para a criação. Então ele cria dentro do container maior, os quadrados menores
+
+    function createColorPallet(colors){
+        let colorPaletContainer = document.querySelector('#color-palette');
+
+        for (let index in colors){
+        let palletItemDiv = createPalletItem(colors[index]); 
+        colorPaletContainer.appendChild(palletItemDiv)
+        }  
     }
+            // nessa função ele cria cada quadradinho com sua cor específica
+
+    function createPalletItem(color){
+        let palletItemDiv = document.createElement('div');
+        palletItemDiv.style.backgroundColor = color;
+        palletItemDiv.classList.add("color");
+        palletItemDiv.addEventListener('click', handlePalletItemEvent);
+
+        if (color === 'black'){
+            palletItemDiv.classList.add('selected');
+        }
+
+                // palletItemDiv.addEventListener('click', function(event){
+                //     console.log("funcionou");
+                // }); Podia fazer usando um função anônima também, ao invés de chamar a handle. Mas dessa forma é mais organizado
+
+        return palletItemDiv;
+    }
+
+    function handlePalletItemEvent(event){
+        let oldSelectedDiv = document.querySelector('.selected');
+        let currentSelectedDiv = event.target;
+        let backgroundColor = currentSelectedDiv.style.backgroundColor;
+        console.log(backgroundColor);
+
+        oldSelectedDiv.classList.remove('selected');
+        currentSelectedDiv.classList.add('selected');
+        
+        selectedColor = backgroundColor;
+    }
+
+    for (let i = 0; i < 5; i++){
+        let pixelBoard = document.querySelector('#pixel-board');
+        let linha = document.createElement('div');
+        linha.className = 'row';
+        pixelBoard.appendChild(linha);
+        for (let j = 0; j < 5; j++){
+            let coluna = document.createElement('div');
+            coluna.className = 'pixel';
+            coluna.id= 'column'
+            linha.appendChild(coluna);
+            coluna.addEventListener('click', function(event){
+                coluna.style.backgroundColor = selectedColor;
+            })
+        }
+    }
+    
+   
 }
 
-// window.onload = function(){
-//     preto.style = "black"
-// }
+
+
