7e9ac10a934c5fdbfdd80dcd3946ba852793881e
Sttefanie Maciel <macielsttefanie@gmail.com>
2020-07-29 17:12:20 -0300

    Fazendo os ultimos ajustes

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
-window.onclick = function() {
-
-
+// window = janela atual da pagina
+// onclick = evento da pagina
+window.onclick = function(event) {
+  // declarada constante da cor selecionada palletColorSelector
   const palletColorSelector = document.querySelector('.selected');
+  // Se onde eu cliquei tem classe color
+  // event.target = Onde eu cliquei na janela
   if (event.target.className === 'color') {
+    // Eu irei add a class Selected
     event.target.classList.add('selected');
+    // Removi a class selected do elemento palletColorSelector
     palletColorSelector.classList.remove('selected');
   }
+  // Se onde eu cliquei tem classe pixel
   if (event.target.className === 'pixel') {
+    // Irei copiar o estilo de backgroundColor do palletColorSelector
+    // event.target.style.backgroundColor = #id==black,red,green
     event.target.style.backgroundColor = palletColorSelector.id;
   }
   if (event.target.id === 'clear-board') {
-    const pixelSelector = document.querySelectorAll('.pixel');
-    for (let i = 0; i < pixelSelector.length; i += 1) {
-      pixelSelector[i].style.backgroundColor = 'white';
+    // Cria um array de pixel
+    const pixelsArray = document.querySelectorAll('.pixel');
+    // Pecorre todo o array e define backgroundColor = white
+    for (let i = 0; i < pixelsArray.length; i += 1) {
+      pixelsArray[i].style.backgroundColor = 'white';
     }
   }
+};
+
+function setColor(colorButtonPallet) {
+  const palletButton = document.querySelector(colorButtonPallet);
+  palletButton.addEventListener('click', function() {
+    const selector = document.querySelector('.selected');
+    selector.classList.remove('selected');
+    palletButton.classList.add('selected');
+  });
 }
-const buttonBlack = document.querySelector('#black');
-buttonBlack.addEventListener('click', function() {
-  const selector = document.querySelector('.selected');
-  selector.classList.remove('selected');
-  buttonBlack.classList.add('selected');
-});
 
-const buttonRed = document.querySelector('#red');
-buttonRed.addEventListener('click', function() {
-  const selector = document.querySelector('.selected');
-  selector.classList.remove('selected');
-  buttonRed.classList.add('selected');
-});
+const collorsPallet = ['#black', '#red', '#blue', '#green'];
 
-const buttonBlue = document.querySelector('#blue');
-buttonBlue.addEventListener('click', function() {
-  const selector = document.querySelector('.selected');
-  selector.classList.remove('selected');
-  buttonBlue.classList.add('selected');
-});
-
-const buttonGreen = document.querySelector('#green');
-buttonGreen.addEventListener('click', function() {
-  const selector = document.querySelector('.selected');
-  selector.classList.remove('selected');
-  buttonGreen.classList.add('selected');
+collorsPallet.forEach(colorName => {
+  setColor(colorName);
 });
