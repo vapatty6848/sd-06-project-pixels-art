5d07a6039fd563f36f3d9b0094bbaa97fff18c32
lucasvr39 <lucasveras1994@gmail.com>
2020-07-29 15:43:16 -0300

    fix code climate issues and clear button

--- script.js
+++ script.js
@@ -1,63 +1,47 @@
-// creates color palette
+window.onload = function () {
+  // handles the event to get selected pixel and change div's class
 
-function createColorPalette(colorArray) {
-  const colorPaletteContainer = document.getElementById('color-palette');
-  
-  for (let index in colorArray) {
-    const pixelDiv = createPaletteColorDiv(colorArray[index]);
-    colorPaletteContainer.appendChild(pixelDiv);
+  function handlePalettePixelColor(event) {
+    const lastSelectedDiv = document.querySelector('.selected');
+    const currentPixelDiv = event.target;
+    lastSelectedDiv.classList.remove('selected');
+    currentPixelDiv.classList.add('selected');
   }
-}
 
-// creates the colored div in the color palette
+  // creates the colored div in the color palette
 
-createColorPalette(['black', 'cyan', 'red', 'yellow']);
-
-function createPaletteColorDiv(color) {
-  const addPixelDiv = document.createElement('div');
-  addPixelDiv.style.backgroundColor = color;
-  addPixelDiv.className = 'color';
-  addPixelDiv.addEventListener('click', handlePalettePixelColor);
-
-  if (color === 'black') {
-    addPixelDiv.classList.add('selected');
+  function createPaletteColorDiv(color) {
+    const addPixelDiv = document.createElement('div');
+    addPixelDiv.style.backgroundColor = color;
+    addPixelDiv.className = 'color';
+    addPixelDiv.addEventListener('click', handlePalettePixelColor);
+    if (color === 'black') {
+      addPixelDiv.classList.add('selected');
+    }
+    return addPixelDiv;
   }
-  return addPixelDiv;
-}
 
-// handles the event to get selected pixel and change div's class
+  // creates color palette
 
-function handlePalettePixelColor(event) {
-  const lastSelectedDiv = document.querySelector('.selected');
-  const currentPixelDiv = event.target;
-  lastSelectedDiv.classList.remove('selected');
-  currentPixelDiv.classList.add('selected');
-}
+  function createColorPalette(colorArray) {
+    const colorPaletteContainer = document.getElementById('color-palette');
 
+    for (let index = 0; index < colorArray.length; index += 1) {
+      const pixelDiv = createPaletteColorDiv(colorArray[index]);
+      colorPaletteContainer.appendChild(pixelDiv);
+    }
+  }
+
+  createColorPalette(['black', 'cyan', 'red', 'yellow']);
+};
 // handle event to change div's background color and make the pixel art
 
-// function addDivBgColor () {
-  // const pixelDivs = document.querySelectorAll('.pixel')
-  
-  // for (let i in pixelDivs) {
-  //   let currentDiv = pixelDivs[i];
-  //   currentDiv.addEventListener('click', function (event) {
-  //     const currentSelectedColor = document.querySelector('.selected').style.backgroundColor;
-  //     currentDiv = event.target;
-  //   currentDiv.style.backgroundColor = currentSelectedColor;
-  //   });
-  // }
-//   return currentDiv;
-// }
-
-const pixelDiv = document.querySelector('.pixel')
-pixelDiv.addEventListener('click', function (event) {
-  const allPixelDivs = document.querySelectorAll('.pixel')
-
-  for (let i in pixelDivs) {
-    let currentDiv = allPixelDivs[i];
-    currentDiv.addEventListener('click', function(event) {
-      const currentSelectedColor = document.querySelector('.selected').style.backgroundColor;
+document.querySelector('#pixel-board').addEventListener('click', function () {
+  const pixelDiv = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixelDiv.length; i += 1) {
+    let currentDiv = pixelDiv[i];
+    const currentSelectedColor = document.querySelector('.selected').style.backgroundColor;
+    currentDiv.addEventListener('click', function (event) {
       currentDiv = event.target;
       currentDiv.style.backgroundColor = currentSelectedColor;
     });
@@ -68,6 +52,7 @@ pixelDiv.addEventListener('click', function (event) {
 
 const btnClear = document.querySelector('#clear-board');
 btnClear.addEventListener('click', function () {
-  for (let i = 0; i < document.getElementsByClassName('pixel').length; i += 1)
-    document.querySelectorAll('.pixel')[i].style.backgroundColor = 'white';
-})
+  for (let i = 0; i < document.getElementsByClassName('pixel').length; i += 1) {
+    document.querySelectorAll('.pixel')[i].style.removeProperty('background-color');
+  }
+});
