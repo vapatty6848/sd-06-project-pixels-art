489efce11d9876c085183de74bb0a405cb5e11f2
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-07-29 11:13:31 -0300

    12. Cores aleatórias + preta como primeira

--- script.js
+++ script.js
@@ -95,11 +95,29 @@ function gerarBoard() {
   }
 }
 
+//Gerando cor aleatória
+function aleatoria(inf, sup) {
+  let possibilitiesNumber = sup - inf;
+  let aleat = Math.random() * possibilitiesNumber;
+  aleat = Math.floor(aleat);
+  return parseInt(inf) + aleat;
+}
+
+function corAleatoria() {
+  let hexaArray = ['0' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , 'A' , 'B' , 'C' , 'D' , 'E' , 'F'];
+  let aleatCor = '#';
+  for (let index = 0; index < 6; index += 1) {
+    let posArray = aleatoria(0, hexaArray.length);
+    aleatCor += hexaArray[posArray];
+  }
+  return aleatCor;
+}
+
 const btnBoard = document.querySelector('#generate-board');
 btnBoard.addEventListener('click', gerarBoard);
 
 const btnClear = document.querySelector('#clear-board');
 btnClear.addEventListener('click', clearBoard);
 
-populatePaletteItem(['black', 'blue', 'yellow', 'green']);
+populatePaletteItem(['black', corAleatoria(), corAleatoria(), corAleatoria()]);
 populandoBoard(5, 5);
