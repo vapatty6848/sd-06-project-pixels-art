67b6be14e0d37cfa24cf95686d03f2f4170bfc08
us19861229c <rcks.santos@gmail.com>
2020-07-29 13:55:57 -0300

    Cria botão limpar e funcionalidade, para tornar todos os quadros brancos novamente.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = function () {
   const listaDeItensPaleta = document.querySelectorAll('li');
+
   for (let indice = 0; indice < listaDeItensPaleta.length; indice += 1) {
     listaDeItensPaleta[indice].addEventListener('click', function () {
       for (let ordem = 0; ordem < listaDeItensPaleta.length; ordem += 1) {
@@ -9,4 +10,12 @@ window.onload = function () {
       this.classList.add('selected');
     });
   }
+
+  const apagaTudo = document.querySelector('#clear-board');
+  const todosQuadros = document.getElementsByTagName('td');
+  apagaTudo.addEventListener('click', function () {
+    for (let indice = 0; indice < todosQuadros.length; indice += 1) {
+      todosQuadros[indice].className = 'pixel white';
+    }
+  });
 };
