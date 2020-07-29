9d22612b2b563360d75713c65118a1f7b314676c
Paulo Frazão <paulo4187@gmail.com>
2020-07-29 20:11:37 -0300

    pixel e paletas já com selection

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
 const pixels = document.querySelector('.pixel');
-const sectionPaleta = document.querySelector('#pixel-board');
 
-window.onload = function() {
-  const palletColor = document.querySelector('#color-palette');
-  const paletaCores = ['black', 'red', 'blue', 'green'];
-  createDivAndColor(paletaCores);
-  function createDivAndColor(color) {
-    for (let i = 0; i < color.length; i += 1) {
-      let palletDiv = document.createElement('div');
-      palletDiv.style.backgroundColor = color[i];
-      palletDiv.className = 'color';
-      palletColor.appendChild(palletDiv);
+
+const sectionPaleta = document.querySelector('#pixel-board');
+const palletColor = document.querySelector('#color-palette');
+const paletaCores = ['black', 'red', 'blue', 'green'];
+const elementoUl = document.createElement('ul');
+sectionPaleta.appendChild(elementoUl);
+
+function createDivAndColor(color) {
+  for (let i = 0; i < color.length; i += 1) {
+    const palletDiv = document.createElement('div');
+    palletDiv.style.backgroundColor = color[i];
+    palletDiv.className = 'color';
+    palletColor.appendChild(palletDiv);
+    if (i === 0) {
+      palletDiv.className = 'color selected';
+    }
+    palletDiv.addEventListener('click', changeSelected, PixelColor(palletDiv.backgroundColor));
+  }
+}
+createDivAndColor(paletaCores);
+
+function createLiAndDivPixels() {
+  for (let j = 0; j < 5; j += 1) {
+    const elementoLi = document.createElement('li');
+    elementoUl.appendChild(elementoLi);
+    for (let i = 0; i < 5; i += 1) {
+      const pixelDiv = document.createElement('div');
+      pixelDiv.className = 'pixel';
+      elementoLi.appendChild(pixelDiv);
+      pixelDiv.addEventListener('click', changePixelColor);
     }
   }
+}
+createLiAndDivPixels();
+
+function changeSelected() {
+  const divSelectedBefore = document.querySelector('.selected');
+  const divSelectedNow = event.target;
+  divSelectedBefore.classList.remove('selected');
+  divSelectedNow.classList.add('selected');
+}
+
+function PixelColor(color) {
+  const colorNow = color;
+  console.log(colorNow);
+}
+
+function changePixelColor() {
+  const divPixelColor = event.target;
+  divPixelColor.style.backgroundColor = 'blue';
 }
