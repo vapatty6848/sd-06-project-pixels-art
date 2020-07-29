05413b01c903cc013cf3aea778698e7cd9ddb303
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-07-28 22:42:36 -0300

    10° no meio do exerciciofinalizar por hoje

--- script.js
+++ script.js
@@ -5,15 +5,26 @@ window.onload = function() {
     let botaoLimpar = document.querySelector("#clear-board")
     let cores = ["black", "yellow", "blue", "gray", "Gold"];
 
+    function limparQuadro() {
+        let listaPixel = document.querySelectorAll("div.pixel");
+        for (let nPixel = 0; nPixel < listaPixel.length; nPixel += 1) {
+            listaPixel[nPixel].style.backgroundColor = "white";
+        }        
+    }
+
+    function criarPixels(nDivs) {
+        for (let n = 1; n <= nDivs; n += 1) {
+            let pixel = document.createElement("div");
+            pixel.className = "pixel";
+            quadroPixel.appendChild(pixel);        
+        }
+    }
+    criarPixels(25);
+
     for (let nColor = 0; nColor < color.length; nColor += 1) {
         color[nColor].style.backgroundColor = cores[nColor];        
     }
-    
-    for (let n = 1; n <= 25; n += 1) {
-        let pixel = document.createElement("div");
-        pixel.className = "pixel";
-        quadroPixel.appendChild(pixel);        
-    } 
+     
     let elementoSelecionado = color[0];
     let click1 = 0;    
     containerColor.addEventListener("click", function(event) {
@@ -30,10 +41,19 @@ window.onload = function() {
     });
 
     botaoLimpar.addEventListener("click", function(event) {
-        let listaPixel = document.querySelectorAll("div.pixel");
-        for (let nPixel = 0; nPixel < listaPixel.length; nPixel += 1) {
-            listaPixel[nPixel].style.backgroundColor = "white";
-        }             
+        limparQuadro();             
     });
 
+    let botaoTamanho = document.querySelector("#generate-board");
+    // ver se tamanho é uma string ou um numero
+    let tamanho = document.querySelector("#board-size").value;
+    botaoTamanho.addEventListener("click", function(event) {
+        if (tamanho <= 50 && tamanho >= 5) {
+            quadroPixel.style.width = tamanho * 42;
+
+        } else {
+            alert("Board inválido!");
+        }
+        limparQuadro();             
+    });         
 }
