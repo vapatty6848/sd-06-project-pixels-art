181b46cb1eb884f71b3d5d780e3ae54ddf9f7ca1
Stefano Branz <stefano.branz@gmail.com>
2020-07-29 16:20:52 -0300

    Plantão

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 window.onload = function() {
-  
+  let selectedColor = document.getElementsByClassName('selected');
+  let color = document.getElementsByClassName('color');
+  let pixel = document.getElementsByClassName('pixel')
+
+  color.addEventListener('onclick', function(event) {
+    event.target.classList.add('selected');
+    selectedColor.classList.remove('selected');
+  });
+
+  pixel.addEventListener('onclick', function(event) {
+    event.target.style.backgroundColor = selectedColor.id;
+  });
 }
