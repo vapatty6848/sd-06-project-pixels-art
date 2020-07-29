e25b6480cb88e51510ac0679d1a0a029e1ca6cb3
ilanribeiro <ilanribeiro@gmail.com>
2020-07-29 15:33:28 -0300

    created javascript function to build pixel board

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+
+let colors = ['black', 'green', 'blue', 'orange', 'yellow'];
+let colorPalletItem = document.getElementsByClassName('color-palette')[0];
+let pixelBoard = document.querySelector('.pixel-board');
+
 window.onload = function() {
-  let colors = ['black', 'green', 'blue', 'orange', 'yellow', 'gray'];
-  let colorPalletItem = document.getElementsByClassName('color-palette')[0];
-  let pixelBoard = document.getElementsByClassName('pixel-board');
 
   // build color pallet based in quantity colors specified in array colors;
   function buildPallet(colors) {
@@ -16,3 +18,21 @@ window.onload = function() {
   buildPallet(colors);
 
 }
+
+// build pixel board
+let n = 5
+
+function buildPixelBoard(n) {
+  for (let index1 = 1; index1 <= n; index1 += 1) {
+    let buildPix;
+
+    for (index2 = 1; index2 <= n; index2 += 1) {
+      buildPix = document.createElement('div');
+      buildPix.className = 'pixel';
+      buildPix.style.backgroundColor = 'white';
+      pixelBoard.appendChild(buildPix);
+    }
+  }
+}
+
+buildPixelBoard(n);
