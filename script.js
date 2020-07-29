55619f8879be03b54dd5f1fc0f0688ff6360626d
Micaeli Teixeira <luime6@gmail.com>
2020-07-28 21:16:30 -0300

    fazendo alterações do codeclimate

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-let selectedColor = "black"
 window.onload = function () {
-  //add cor
-document.getElementsByClassName('color')[0].style.backgroundColor="black";
-document.getElementsByClassName('color')[1].style.backgroundColor="red";
-document.getElementsByClassName('color')[2].style.backgroundColor="blue";
-document.getElementsByClassName('color')[3].style.backgroundColor="green";
+ 
+/* add cor */
+  document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
+  document.getElementsByClassName('color')[1].style.backgroundColor = 'red';
+  document.getElementsByClassName('color')[2].style.backgroundColor = 'blue';
+  document.getElementsByClassName('color')[3].style.backgroundColor = 'green';
 
-//clicando
-let listen = document.querySelector('#color-palette');
-listen.addEventListener("click", handlePalletItemEvent);
+/* clicando */
+  const listen = document.querySelector('#color-palette');
+  listen.addEventListener('click', handlePalletItemEvent);
 
-// função para disparar o evento e trocar a classe selected para a que for clicada
-function handlePalletItemEvent() {
-let oldSelectedDiv = document.querySelector(".selected");
-let currentSelectedDiv = event.target
+/* função para disparar o evento e trocar a classe selected para a que for clicada */
+  function handlePalletItemEvent() {
+  const oldSelectedDiv = document.querySelector('.selected');
+  const currentSelectedDiv = event.target;
 
-oldSelectedDiv.classList.remove("selected");
-currentSelectedDiv.classList.add("selected")
+  oldSelectedDiv.classList.remove('selected');
+  currentSelectedDiv.classList.add('selected');
 
-selectedColor = currentSelectedDiv.style.backgroundColor;
+  selectedColor = currentSelectedDiv.style.backgroundColor;
 }
 
-// colorindo os pixels
-function handlePixel(event) {
-    let selectedPixelDiv = event.target;
-    selectedPixelDiv.style.backgroundColor = selectedColor;
+/* colorindo os pixels */
+  function handlePixel(event) {
+  const selectedPixelDiv = event.target;
+  selectedPixelDiv.style.backgroundColor = selectedColor;
 }
 
-// individualizando os pixels para colorir
-let pixelBoardDiv= document.getElementById("pixel-board");
-pixelBoardDiv.addEventListener("click", handlePixel)   
+/* individualizando os pixels para colorir */
+  const pixelBoardDiv = document.getElementById('pixel-board');
+  pixelBoardDiv.addEventListener('click', handlePixel);
 
 
-// Button reset color
-let buttonReset = document.querySelector('#clear-board');
-buttonReset.addEventListener('click', function() {
-  let colorPixels = document.querySelectorAll('.pixel');
+/* Button reset color */
+  const buttonReset = document.querySelector('#clear-board');
+  buttonReset.addEventListener('click', function () {
+  const colorPixels = document.querySelectorAll('.pixel');
   for (let index = 0; index < colorPixels.length; index += 1) {
     colorPixels[index].style.backgroundColor = 'white';
   }
-})
-}
+});
+};
