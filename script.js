781cfa27fd8482a82751f346d97f473e095ce212
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-07-28 22:16:06 -0300

    fixed bugs

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const colors = ['black', 'red', 'orange', 'yellow'];
-const squarePaletteSize = '50px';
-const squareBoardSize = '40px';
+const squarePaletteSize = 50;
+const squareBoardSize = 40;
+const palettesSize = 4;
+const boardSize = 5;
 
 for (let i = 0; i < 4; i += 1) {
   const createColorClassDiv = document.createElement('div');
   createColorClassDiv.className = 'color';
-  createColorClassDiv.style.width = squarePaletteSize;
-  createColorClassDiv.style.height = squarePaletteSize;
+  createColorClassDiv.style.width = `${squarePaletteSize}px`;
+  createColorClassDiv.style.height = `${squarePaletteSize}px`;
   createColorClassDiv.style.backgroundColor = colors[i];
   createColorClassDiv.style.float = 'left';
   createColorClassDiv.style.border = '1px solid black';
@@ -14,17 +16,17 @@ for (let i = 0; i < 4; i += 1) {
   document.getElementById('color-palette').appendChild(createColorClassDiv);
 }
 
-for (let i = 0; i < 5; i += 1) {
+for (let i = 0; i < boardSize; i += 1) {
   const lineDiv = document.createElement('div');
   lineDiv.className = 'pixel-wrapp';
   lineDiv.width = (colors.length * squareBoardSize) + (colors.length * 2);
   document.getElementById('pixel-board').appendChild(lineDiv);
 
-  for (let j = 0; j < 5; j += 1) {
+  for (let j = 0; j < boardSize; j += 1) {
     const createColorClassDiv = document.createElement('div');
     createColorClassDiv.className = 'pixel';
-    createColorClassDiv.style.width = squareBoardSize;
-    createColorClassDiv.style.height = squareBoardSize;
+    createColorClassDiv.style.width = `${squareBoardSize}px`;
+    createColorClassDiv.style.height = `${squareBoardSize}px`;
     createColorClassDiv.style.backgroundColor = 'white';
     createColorClassDiv.style.float = 'left';
     createColorClassDiv.style.border = '1px solid black';
@@ -32,8 +34,14 @@ for (let i = 0; i < 5; i += 1) {
     document.getElementsByClassName('pixel-wrapp')[i].appendChild(createColorClassDiv);
   }
 }
+const sideSize = ((palettesSize * squarePaletteSize) + ((palettesSize) * 2));
+const divIdColorPalette = document.getElementById('color-palette');
+divIdColorPalette.style.border = '1px solid black';
+divIdColorPalette.style.width = `${sideSize}px`;
+divIdColorPalette.style.height = `${squarePaletteSize + 2}px`;
+console.log(divIdColorPalette.style.border);
+console.log(sideSize);
 
-document.getElementById('color-palette').height = 'auto';
 
 function deleteAllSelected() {
   document.querySelectorAll('.color').forEach(item => {
@@ -49,8 +57,8 @@ document.querySelectorAll('.color').forEach(item => {
   item.addEventListener('click', event => {
     deleteAllSelected();
     event.target.className += ' selected';
-  })
-})
+  });
+});
 
 document.querySelectorAll('.color')[0].className += ' selected';
 
@@ -58,7 +66,7 @@ document.querySelectorAll('.pixel').forEach(box => {
   box.addEventListener('click', function () {
     const color = document.querySelector('.selected').style.backgroundColor;
     box.style.backgroundColor = color;
-  })
+  });
 });
 
 const btClean = document.getElementById('clear-board');
@@ -66,6 +74,5 @@ btClean.addEventListener('click', function () {
   const allBoxes = document.querySelectorAll('.pixel');
   for (let i = 0; i < allBoxes.length; i += 1) {
     allBoxes[i].style.backgroundColor = 'white';
-
   }
 })
