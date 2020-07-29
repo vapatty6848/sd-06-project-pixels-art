3828b0dc0757aea68c05a79d7b399bbb8fb71ae2
William Gomes <williamjog@hotmail.com>
2020-07-29 20:58:36 -0300

    2nd bonus requirement - Adding Board Size limitations

--- script.js
+++ script.js
@@ -25,48 +25,6 @@ function generatePaletteColors() {
   }
 }
 
-function generateInput() {
-  const input = document.createElement('input');
-  input.id = 'board-size';
-  input.classList.add('board-size');
-  input.type = 'number';
-  input.min = 1;
-  input.max = 50;
-  document.querySelector('.btn').appendChild(input);
-}
-
-function generateCustomizedPixelBoard() {
-  const size = document.querySelector('.board-size').value;
-  if (size === '') {
-    alert('Board inválido!')
-    return
-  }
-  generatePixels(size);
-}
-
-function generateBoardSizeButton() {
-  const boardSizeButton = document.createElement('button');
-  boardSizeButton.id = 'generate-board';
-  boardSizeButton.classList.add('generate-board');
-  boardSizeButton.innerText = 'VQV';
-  document.querySelector('.btn').appendChild(boardSizeButton);
-  boardSizeButton.addEventListener('click', generateCustomizedPixelBoard);
-}
-
-function generateCleanButton() {
-  const button = document.createElement('button');
-  button.id = 'clear-board';
-  button.classList.add('clean');
-  button.innerText = 'Limpar';
-  document.querySelector('.btn').appendChild(button);
-  button.addEventListener('click', () => {
-    const allPixelsFromPixelBoard = document.querySelectorAll('.pixel');
-    allPixelsFromPixelBoard.forEach((item) => {
-      item.style.backgroundColor = 'white';
-    });
-  });
-}
-
 function generatePixels(size) {
   pixelBoard.innerHTML = '';
   for (let i = 0; i < size; i += 1) {
@@ -83,6 +41,56 @@ function generatePixels(size) {
       });
     }
   }
+}
+
+function generateInput() {
+  const input = document.createElement('input');
+  input.id = 'board-size';
+  input.classList.add('board-size');
+  input.type = 'number';
+  input.min = 1;
+  input.max = 50;
+  document.querySelector('.btn').appendChild(input);
+}
+
+function generateCustomSizedPixelBoard() {
+  let size = document.querySelector('.board-size').value;
+  if (size === '' || size < 1) {
+    alert('Board inválido!');
+    return;
+  }
+  if (size < 5) {
+    size = 5;
+    generatePixels(size);
+  }
+  if (size > 50) {
+    size = 50;
+    generatePixels(size);
+  }
+  generatePixels(size);
+}
+
+function generateBoardSizeButton() {
+  const boardSizeButton = document.createElement('button');
+  boardSizeButton.id = 'generate-board';
+  boardSizeButton.classList.add('generate-board');
+  boardSizeButton.innerText = 'VQV';
+  document.querySelector('.btn').appendChild(boardSizeButton);
+  boardSizeButton.addEventListener('click', generateCustomSizedPixelBoard);
+}
+
+function generateCleanButton() {
+  const button = document.createElement('button');
+  button.id = 'clear-board';
+  button.classList.add('clean');
+  button.innerText = 'Limpar';
+  document.querySelector('.btn').appendChild(button);
+  button.addEventListener('click', () => {
+    const allPixelsFromPixelBoard = document.querySelectorAll('.pixel');
+    allPixelsFromPixelBoard.forEach((item) => {
+      item.style.backgroundColor = 'white';
+    });
+  });
 }
 
 window.onload = function () {
