1baf308069111d9931252441521b481e09d76c8a
CristianoNunes <cristianonunes99@gmail.com>
2020-07-28 22:23:59 -0300

    Setando cores e limpando

--- script.js
+++ script.js
@@ -1,42 +1,63 @@
+let selectColor = 'black';
 
 window.onload = function() {
-let selectColor = 'black';
-let colors = ['black', 'red', 'blue', 'green'];
-createColorPallet(colors);
+
+  let colors = ['black', 'red', 'blue', 'green'];
+  createColorPallet(colors);
+
+  let pixelBoardDiv = document.querySelector('#pixel-board');
+  pixelBoardDiv.addEventListener('click', handlePixelClick)
+
+}
 
 //Quando clica, add o select e retira da cor antiga
 function handlePalletItemEvent(event) {
-    let oldSelectedDiv = document.querySelector('.selected');
-    let currentSelectedDiv = event.target;
+  let oldSelectedDiv = document.querySelector('.selected');
+  let currentSelectedDiv = event.target;
 
-    oldSelectedDiv.classList.remove('selected');
-    currentSelectedDiv.classList.add('selected');
+  oldSelectedDiv.classList.remove('selected');
+  currentSelectedDiv.classList.add('selected');
 
-    selectColor = currentSelectedDiv.style.backgroundColor;
-    console.log(selectColor);
+  selectColor = currentSelectedDiv.style.backgroundColor;
+  console.log(selectColor);
 }
 
 //Criação dos itens da paleta (divs)
 function createPalletItem(color) {
-    let palletItemDiv = document.createElement('div');
-    palletItemDiv.style.background = color;
-    palletItemDiv.className = 'color';
-    palletItemDiv.addEventListener('click', handlePalletItemEvent);
+  let palletItemDiv = document.createElement('div');
+  palletItemDiv.style.background = color;
+  palletItemDiv.className = 'color';
+  palletItemDiv.addEventListener('click', handlePalletItemEvent);
 
-    if (color === 'black') {
-        palletItemDiv.classList.add('selected');
-    }
-    return palletItemDiv;
+  if (color === 'black') {
+    palletItemDiv.classList.add('selected');
+  }
+  return palletItemDiv;
 }
 
 //Pega a paleta de cores e add os itens a paleta
 function createColorPallet(colors) {
-    let colorPalletContainer = document.getElementById('color-palette');
+  let colorPalletContainer = document.getElementById('color-palette');
 
-    for (let index in colors) {
-        let palletItemDiv = createPalletItem(colors[index]);
-        colorPalletContainer.appendChild(palletItemDiv);
-    }
+  for (let index in colors) {
+    let palletItemDiv = createPalletItem(colors[index]);
+    colorPalletContainer.appendChild(palletItemDiv);
+  }
 }
 
+//Setar a cor na div selecionada
+function handlePixelClick(event) {
+  let selectPixelDiv = event.target;
+  selectPixelDiv.style.backgroundColor = selectColor;
+}
+
+//Limpar o pixelBoard
+function clearBoard() {
+  let btn = document.querySelector('#clear-board');
+  let pixel = document.getElementsByClassName('pixel');
+  btn.addEventListener('click', function() {
+    for (let i = 0; i < pixel.length; i+= 1) {
+      pixel[i].style.backgroundColor = 'white';
+    }
+  })
 }
