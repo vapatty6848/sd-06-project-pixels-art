e595965b10a6a3509da2d395161df70fa5bd3d26
LughWalle <civalfran1@gmail.com>
2020-07-28 21:53:49 -0500

    amaciando o ego do CodeClimate

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
 // 'greenyellow', 'orange', 'violet', 'cyan', 'blueviolet', 'chartreuse', 'chocolate', 'coral', 'gold', 'goldenrod', 'grey', 'black'
 // variaveis
-const colorPalette = document.querySelector("#color-palette");
-const tableColor = document.getElementById("pixel-board");
-const colorSelected = document.getElementsByClassName("color");
+const colorPalette = document.querySelector('#color-palette');
+const tableColor = document.getElementById('pixel-board');
+const colorSelected = document.getElementsByClassName('color');
 // const currentColor = document.querySelector('.selected').style.backgroundColor;
 
-const colors = ["black", "blue", "yellow", "red"];
+const colors = ['black', 'blue', 'yellow', 'red'];
 
 // functions and events
 for (let i = 0; i < colors.length; i += 1) {
-  const createBoxColor = document.createElement("li");
-  createBoxColor.className = "color";
+  const createBoxColor = document.createElement('li');
+  createBoxColor.className = 'color';
   createBoxColor.style.backgroundColor = colors[i];
   colorPalette.appendChild(createBoxColor);
 }
 window.onload = function () {
-  colorSelected[0].className += " selected";
+  colorSelected[0].className += ' selected';
 };
 
 for (let i = 0; i < 5; i += 1) {
-  const createTr = document.createElement("tr");
-  createTr.className = "tabela";
+  const createTr = document.createElement('tr');
+  createTr.className = 'tabela';
   tableColor.appendChild(createTr);
   for (let index = 0; index < 5; index += 1) {
-    const createTd = document.createElement("td");
-    createTd.className = "pixel";
-    createTd.backgroundColor = "white";
+    const createTd = document.createElement('td');
+    createTd.className = 'pixel';
+    createTd.backgroundColor = 'white';
     createTr.appendChild(createTd);
   }
 }
 
 function changeSelected(select) {
-  const selected = document.querySelector(".selected");
-  selected.classList.remove("selected");
-  select.className += " selected";
+  const selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  select.className += ' selected';
 }
 
-colorPalette.addEventListener("click", function (e) {
+colorPalette.addEventListener('click', function (e) {
   changeSelected(e.target);
   console.log(e.target);
 });
 
-tableColor.addEventListener("click", function (e) {
+tableColor.addEventListener('click', function (e) {
   e.target.style.backgroundColor = document.querySelector(
-    ".selected"
+    '.selected'
   ).style.backgroundColor;
   
 });
