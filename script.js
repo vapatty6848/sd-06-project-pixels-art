67fab715f4627db5cc311a6076efb8d0d86e852e
CleberFontinele <cleberfontinele2387@gmail.com>
2020-07-29 01:36:07 -0300

    exercicio 7 correção

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 window.onclick = function () {
-    const selection = document.querySelector('.selected');
-    if (event.target.className === 'color') {
+  if (event.target.className === 'color') {
     event.target.classList.add('selected');
+    const selection = document.querySelector('.selected');
     selection.classList.remove('selected');
-    selection = document.querySelector('.selected');
   }
-  if (event.target.className === '.pixel') {
-    event.target.style.backgroundColor = selection.id;  
- }
 };
-  
