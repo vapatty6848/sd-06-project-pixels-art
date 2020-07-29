fbb3c05dfc18663e821aeeedbe490d3764dedf2f
us19861229c <rcks.santos@gmail.com>
2020-07-29 12:40:51 -0300

    Adiciona a classe white para conferir a cor branca aos quadros de pixels. Cria a atribuição e remoção da classe 'selected'.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 window.onload = function () {
-
+  const listaDeItensPaleta = document.querySelectorAll('li');
+  for (let indice = 0; indice < listaDeItensPaleta.length; indice += 1) {
+    listaDeItensPaleta[indice].addEventListener('click', function () {
+      for (let ordem = 0; ordem < listaDeItensPaleta.length; ordem += 1) {
+        // console.log(listaDeItensPaleta[ordem].classList[1]);
+        listaDeItensPaleta[ordem].classList.remove('selected');
+      }
+      this.classList.add('selected');
+    });
+  }
 };
