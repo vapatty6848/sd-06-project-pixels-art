93f7eb137f73611aff55582310730d0e420bc7dd
ricknois <rickssti@outlook.com>
2020-07-29 02:09:11 -0300

    Board Generator

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 const pixelBoard = document.querySelector('#pixel-board');
 const colorPalet = document.querySelector('#color-palette');
 const btnClear = document.querySelector('#clear-board');
+const btnShow = document.querySelector('#generate-board');
+const inputNum = document.querySelector('#board-size');
+const sectionTable = document.querySelector('.sectionPB')
 let classSel = 'preto';
+let sizeBox = 5;
 
 function createPixelBoard() {
-
-  for (let j = 0; j < 5; j += 1) {
+  let createFather = document.createElement('section')
+  pixelBoard.appendChild(createFather);
+  createFather.setAttribute('class', 'father')
+  for (let j = 0; j < sizeBox; j += 1) {
     let createSection = document.createElement('section');
     createSection.setAttribute('class', 'sectionPB');
-    pixelBoard.appendChild(createSection);
+    createFather.appendChild(createSection);
   }
 
   const sect = document.getElementsByClassName('sectionPB');
-  for (let l = 0; l < 5; l++) {
-    for (let i = 0; i < 5; i += 1) {
+  for (let l = 0; l < sizeBox; l++) {
+    for (let i = 0; i < sizeBox; i += 1) {
       let createDiv = document.createElement('div')
       createDiv.setAttribute('class', 'pixel bgbranco')
       sect[l].appendChild(createDiv);
@@ -44,6 +50,19 @@ btnClear.addEventListener('click', function () {
   for (const j of document.querySelectorAll('.pixel')) {
     if (j.classList.contains('pixel')) {
       j.className = 'pixel bgbranco'
+    }
+  }
+})
+
+btnShow.addEventListener('click', function () {
+  if (inputNum.value === "") {
+    alert('Board inválido!')
+  }else{
+    sizeBox = inputNum.value;    
+    let no = document.querySelector('.father')
+    if (no.parentNode) {
+      no.parentNode.removeChild(no)
+      createPixelBoard();
     }
   }
 })
