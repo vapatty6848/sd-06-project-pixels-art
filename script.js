c6691be188c42ba24c27e6a3af9bb71c772928b8
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-07-29 12:08:10 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -8,11 +8,26 @@ window.onclick = function() {
     if(event.target.className === "pixel") { 
         event.target.style.backgroundColor = selection.id;
     }
-    if(event.target.id === "clear-board") {
-        const all = document.querySelectorAll(".pixel");
-        for (let i = 0; i < all.length; i++){
-            all[i].style.backgroundColor = "white";
-        }
-        
+}
+const all = document.querySelectorAll(".pixel");
+function limpar(){
+    for (let i = 0; i < all.length; i += 1) {
+        all[i].style.backgroundColor = "white";
     }
 }
+const botaoLimpar =  document.querySelector("#clear-board");
+botaoLimpar.addEventListener("click", limpar);
+const botaoBorda = document.querySelector("#generate-board");
+botaoBorda.addEventListener("click", function() {
+    const caixaTamanho = document.querySelector("#board-size");
+    if(caixaTamanho.value === ""){
+        alert("Board inválido!");
+    }
+    for (let i = 0; i < all.length; i += 1) {
+        all[i].style.width = caixaTamanho.value + "px";
+        all[i].style.height = caixaTamanho.value + "px";
+    }
+    limpar();
+})
+
+
