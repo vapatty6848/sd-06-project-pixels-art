56b4f2f90248a7d244ecb63f8871f1dfbd9abfce
Sttefanie Maciel <macielsttefanie@gmail.com>
2020-07-29 12:27:31 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const bottonLimpar = document.querySelector('#clear-board');
+const inputBoard = document.querySelector('#board-size');
+const bottonGenerat = document.querySelector('#generat-board');
+const pixelBoard = document.querySelector('#pixel-board');
