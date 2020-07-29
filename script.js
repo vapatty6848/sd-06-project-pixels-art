7287cf7d60b8dfa62104b87041df063cfde71522
Ana Flávia <anaflvia@gmail.com>
2020-07-29 18:24:42 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 let selectedElement = document.getElementById('color-palette')
+
 selectedElement.addEventListener('click', function() {
   let oldSelectedElement = document.querySelector('.selected');
   oldSelectedElement.classList.remove('selected');
   let currentSelectedElement = event.target;
   currentSelectedElement.classList.add('selected');
 })
+
 let selectedPixel = document.getElementById('pixel-board')
+
 selectedPixel.addEventListener('click', function() {
   let pixelColorSelected = document.querySelector('.selected').id;
   event.target.style.backgroundColor = pixelColorSelected;
 })
+
+let botaoLimpar = document.querySelector('#clear-board');
+
+botaoLimpar.addEventListener('click', clearBoard);
+
+let pixelColorido = document.getElementsByClassName('pixel');
+
+function clearBoard () {
+  selectedPixel.style.backgroundColor = 'white';
+  for (let index in pixelColorido) {
+    pixelColorido[index].style.backgroundColor = 'white';
+  }
+}
