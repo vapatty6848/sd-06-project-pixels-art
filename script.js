54e1e4f9194cd9c6e625763591d500e3d1a3ea0a
CleberFontinele <cleberfontinele2387@gmail.com>
2020-07-29 01:25:20 -0300

    8 - Ao clicar em um pixel dentro do quadro após selecionar uma cor na paleta, o pixel deve ser preenchido com esta cor.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 window.onclick = function () {
+    const selection = document.querySelector('.selected');
     if (event.target.className === 'color') {
-        event.target.classList.add('selected');
-        let selection = document.querySelector('.selected');
-        selection.classList.remove('selected');
-    }
-}
+    event.target.classList.add('selected');
+    selection.classList.remove('selected');
+    selection = document.querySelector('.selected');
+  }
+  if (event.target.className === '.pixel') {
+    event.target.style.backgroundColor = selection.id;  
+ }
+};
+  
