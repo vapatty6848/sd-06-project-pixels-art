5f744ad5ca9dbd19a48b3264502150be69085f29
Aloízio <aloiziobsc@gmail.com>
2020-07-30 12:11:30 -0300

    mudando class selected

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-//teste
-window.onload = function () {
-    let corSelecionada = document.querySelector("#black");
-    corSelecionada.classList.add("selected");
+let corPreta = document.querySelector("#black");
+let corVermelha = document.querySelector("#red");
+let corAzul = document.querySelector("#blue");
+let corVerde = document.querySelector("#green");
+
+function removeClassSelected () {
+  let marcadoComSelected = document.getElementsByClassName('selected');
+  if (marcadoComSelected.item(0) === document.querySelector("#black")) {
+    corPreta.classList.remove("selected")
+  }
+  if (marcadoComSelected.item(0) === document.querySelector("#red")) {
+    corVermelha.classList.remove("selected")
+  }
+  if (marcadoComSelected.item(0) === document.querySelector("#blue")) {
+    corAzul.classList.remove("selected")
+  }
+  if (marcadoComSelected.item(0) === document.querySelector("#green")) {
+    corVerde.classList.remove("selected")
+  }
 }
+//Eventos de Clique Paleta-----------------------------------INÍCIO
+corPreta.addEventListener("click", function(){
+    removeClassSelected ();
+    corPreta.classList.add("selected");
+});
+
+corVermelha.addEventListener("click", function(){
+    removeClassSelected ();
+    corVermelha.classList.add("selected");
+});
+
+corAzul.addEventListener("click", function(){
+    removeClassSelected ();
+    corAzul.classList.add("selected");
+});
+
+corVerde.addEventListener("click", function(){
+    removeClassSelected ();
+    corVerde.classList.add("selected");
+});
+//-------------------------------------------------Eventos de clique Paleta FIM
