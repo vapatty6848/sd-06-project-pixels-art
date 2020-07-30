d390f1ee81ab21b871f50f0f676f62750adaf756
Paulo Cesar Frazão <67616536+paulo-frazao@users.noreply.github.com>
2020-07-30 18:58:37 -0300

    Delete tempCodeRunnerFile.js
    erro cricação de arquivo

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-const paletaCores = ['black', 'red', 'blue', 'green'];
-var p, n, tmp;
-for (p = paletaCores.length; p;) {
-    n = Math.random() * p-1 | 0;
-    tmp = paletaCores[n];
-    paletaCores[n] = paletaCores[p];
-    paletaCores[p] = tmp;
-}
-console.log(paletaCores)
