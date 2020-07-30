c3822139d801b752e0dd996a1a7ae482c2aac179
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2020-07-30 15:32:18 -0300

    javaScript com Html

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    const palette = document.querySelector('#color-palette');
+    const cor = palette.children;
+    cor[0].style.backgroundColor = 'black';
+    cor[1].style.backgroundColor = 'red';
+    cor[2].style.backgroundColor = 'orange';
+    cor[3].style.backgroundColor = 'green';
+    
