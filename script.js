ae231314ba16a8cb9b135d6c256fae9e9daf4978
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2020-07-30 15:41:43 -0300

    Adicionando conteudo ao java script

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ window.onload = function () {
     const palette = document.querySelector('#color-palette');
     const cor = palette.children;
     cor[0].style.backgroundColor = 'black';
-    cor[1].style.backgroundColor = 'red';
-    cor[2].style.backgroundColor = 'orange';
-    cor[3].style.backgroundColor = 'green';
-    
+    cor[1].style.backgroundColor = 'blue';
+    cor[2].style.backgroundColor = 'yellow';
+    cor[3].style.backgroundColor = 'red';
+
+    for (let i = 0; i < 25; i += 1) {
+        const square = document.createElement('div');
+        square.className = 'pixel';
+        document.querySelector('#pixel-board').appendChild(square);
+        }
