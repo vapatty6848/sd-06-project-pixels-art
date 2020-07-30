51a05dc569cf2d4e750c468fa508e9ac5cfd19db
ilanribeiro <ilanribeiro@gmail.com>
2020-07-30 18:01:59 -0300

    build a button to clear all pixel board

--- script.js
+++ script.js
@@ -2,53 +2,59 @@
 const colors = ['black', 'green', 'blue', 'orange'];
 const colorPallet = document.getElementsByClassName('color-palette')[0];
 const pixelBoard = document.querySelector('.pixel-board');
-let selectedColor;
 
-window.onload = function() {
-  // build color pallet based in quantity colors specified in array colors;
-  function buildPallet(color) {
-    let buildedDiv;
-    for (let index = 0; index < color.length; index += 1) {
-      buildedDiv = document.createElement('div');
-      buildedDiv.className = 'color';
-      buildedDiv.style.backgroundColor = color[index];
-      colorPallet.appendChild(buildedDiv);
+// build color pallet based in quantity colors specified in array colors;
+function buildPallet(color) {
+  let buildedDiv;
+  for (let index = 0; index < color.length; index += 1) {
+    buildedDiv = document.createElement('div');
+    buildedDiv.className = 'color';
+    buildedDiv.style.backgroundColor = color[index];
+    colorPallet.appendChild(buildedDiv);
 
-      if(color[index] === 'black') {
-        buildedDiv.classList.add('selected');
-      }
+    if (color[index] === 'black') {
+      buildedDiv.classList.add('selected');
     }
   }
-  buildPallet(colors);
+};
+buildPallet(colors);
 
-
-
-
-  // build pixel board
-  let n = 5
-
-  function buildPixelBoard(n) {
-    for (let index1 = 1; index1 <= n; index1 += 1) {
-      let buildPix;
-      for (index2 = 1; index2 <= n; index2 += 1) {
-        buildPix = document.createElement('div');
-        buildPix.className = 'pixel';
-        buildPix.style.backgroundColor = 'white';
-        pixelBoard.appendChild(buildPix);
-      }
+// build pixel board
+const n = 5;
+function buildPixelBoard(n) {
+  for (let index1 = 1; index1 <= n; index1 += 1) {
+    let buildPix;
+    for (let index2 = 1; index2 <= n; index2 += 1) {
+      buildPix = document.createElement('div');
+      buildPix.className = 'pixel';
+      buildPix.style.backgroundColor = 'white';
+      pixelBoard.appendChild(buildPix);
     }
   }
-  buildPixelBoard(n);
-}
+};
+buildPixelBoard(n);
 
+// builded clicks behavior in pixel boxes or in color boxes
 window.onclick = function() {
-  let selectedColor = document.querySelector('.selected');
-  if(event.target.className === 'color') {
-    newSelectedColor = event.target.classList.add('selected');
-    selectedColor.classList.remove('selected')
-  }
+  const selectedColor = document.querySelector('.selected');
+  if (event.target.className === 'color') {
+    event.target.classList.add('selected');
+    selectedColor.classList.remove('selected');
+  };
 
-  if(event.target.className === 'pixel') {
+  if (event.target.className === 'pixel') {
     event.target.style.backgroundColor = selectedColor.style.backgroundColor;
+  };
+};
+
+// build a button to clear pixel board
+const toClearBoard = document.querySelector('.clear-board');
+const pixelsInBoard = document.querySelectorAll('.pixel');
+
+toClearBoard.addEventListener('click', clearAllBoard);
+
+function clearAllBoard() {
+  for (let indexClear = 0; indexClear < pixelsInBoard.length; indexClear += 1) {
+    pixelsInBoard[indexClear].style.backgroundColor = 'white';
   }
-}
+};
