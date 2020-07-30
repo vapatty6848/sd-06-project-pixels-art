0c399272a57791b89bb7d34d75630950ff0b2f98
fefarina <wfelipefarina@gmail.com>
2020-07-30 13:55:30 -0300

    Update all files

--- script.js
+++ script.js
@@ -1,115 +1,110 @@
-let pixel = document.querySelectorAll('.pixel');
+
+let buttonSizeBoard = document.querySelector('#generate-board');
+let pixelBoard = document.querySelector('#pixel-board')
+let lineBoard = document.querySelectorAll('.line');
 let color = document.querySelectorAll('.color');
-let selected = document.querySelector('selected');
-let colorPallete = document.querySelector('#color-palette')
-let color2 = document.querySelector('.color2');
-let color3 = document.querySelector('.color3');
-let color4 = document.querySelector('.color4');
+let colorPallet = document.querySelector('#color-palette')
+let pixels = document.querySelectorAll('.pixel');
+let buttonClear = document.querySelector('#clear-board');
+let colorSelect = "";
 
 
-function generateAleatoryColor() {
-    let color = '#';
 
-    for (let i = 0; i < 3; i++) {
-        let sub = Math.floor(Math.random() * 256).toString(16);
-        color += (sub.length == 1 ? '0' + sub : sub);
+
+
+includeLine = () => {
+    let inputSizeBoard = document.querySelector('#board-size');
+    let divs = document.querySelectorAll('#pixel-board div');
+    let pixels = document.querySelectorAll('.pixel');
+
+    if (inputSizeBoard.value < 5) {
+        inputSizeBoard.value = 5
+    } else if (inputSizeBoard.value > 50) {
+        inputSizeBoard.value = 50
     }
-    return color
-}
 
-for (let i = 0; i < color.length; i++) {
-    color[0].style.backgroundColor = "#000"
-    color[i].style.backgroundColor = generateAleatoryColor();
-}
 
-function selectedColor() {
-    for (let i = 0; i < color.length; i++) {
-        color[0].classList.add('selected');
-        for (let i = 0; i < pixel.length; i++) {
-            pixel[i].addEventListener('click', function () {
-                pixel[i].className = 'pixel color1'
-            })
-        };
-        color[i].addEventListener('click', function () {
-            for (let index = 0; index < color.length; index++) {
-                if (color[index].classList.contains('selected')) {
-                    color[index].classList.remove('selected')
+    for (let i = 0; i < inputSizeBoard.value; i++) {
+        let newLine = document.createElement('div')
+        newLine.classList.add("line")
+        pixelBoard.appendChild(newLine)
+
+        for (let index = 0; index < inputSizeBoard.value; index++) {
+            let newCell = document.createElement('div');
+            newCell.classList.add('pixel');
+            newLine.appendChild(newCell);
+        }
+    }
+
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].remove();
+    }
+
+    for (let i = 0; i < pixels.length; i++) {
+        pixels[i].addEventListener('click', function () {
+            pixels[i].style.backgroundColor = colorSelect;
+        })
+    };
+
+
+    selectedColor();
+    
+};
+
+buttonSizeBoard.addEventListener('click', includeLine);
+
+generateAleatoryColor = () => {
+    let colors = '#';
+
+    for (let i = 0; i < 4; i++) {
+        let sub = Math.floor(Math.random() * 256).toString(16);
+        colors += (sub.length == 1 ? '0' + sub : sub);
+        color[i].style.backgroundColor = colors;
+        color[0].style.backgroundColor = "#000";
+    }
+};
+window.onload = generateAleatoryColor();
+
+
+selectedColor = () => {
+    let pixels = document.querySelectorAll('.pixel');
+
+    for (let index = 0; index < color.length; index++) {
+        color[0].classList.add('selected')
+
+        color[index].addEventListener('click', function () {
+            for (let i = 0; i < color.length; i++) {
+                if (color[i].classList.contains('selected')) {
+                    color[i].classList.remove('selected')
                 } else {
-                    color[i].classList.add('selected')
-                }
-
-                for (let i = 0; i < pixel.length; i++) {
-                    pixel[i].addEventListener('click', function () {
-                        if (color[0].classList.contains('selected')) {
-                            pixel[i].className = 'pixel color1'
-                            pixel[i].style.backgroundColor = color[0].style.backgroundColor
-                        } else if (color[1].classList.contains('selected')) {
-                            pixel[i].className = 'pixel color2'
-                            pixel[i].style.backgroundColor = color[1].style.backgroundColor
-                        } else if (color[2].classList.contains('selected')) {
-                            pixel[i].className = 'pixel color3';
-                            pixel[i].style.backgroundColor = color[2].style.backgroundColor
-                        } else {
-                            pixel[i].className = 'pixel color4';
-                            pixel[i].style.backgroundColor = color[3].style.backgroundColor
-
-                        }
-                    })
+                    color[index].classList.add('selected')
                 }
             }
-
+            colorSelect = color[index].style.backgroundColor;
         });
+    }
+
+    for (let i = 0; i < pixels.length; i++) {
+        pixels[i].addEventListener('click', function () {
+            pixels[i].style.backgroundColor = colorSelect;
+        })
     };
+
+
 
 };
 
-// Criando botao ClearBoard
-
-let button = document.querySelector('#clear-board');
-let clearBoard = document.querySelector('#pixel-board');
-let whitePixel = document.querySelector('.white');
-
-function buttonClear() {
-    button.addEventListener('click', function () {
-        for (let i = 0; i < pixel.length; i++) {
-            pixel[i].className = 'pixel white'
-            pixel[i].style.backgroundColor = ''
-        }
-    });
-}
-/////
-
-// Limitando quadros de pixels
-let inputSize = document.querySelector('#board-size');
-let buttonInputSize = document.querySelector('#generate-board')
-let valueInput;
-
-function checkValue() {
-    buttonInputSize.addEventListener('click', function () {
-        if (inputSize.value === '') {
-            alert("Board Inválido")
-        }else if (inputSize.value < 5) {
-            inputSize.value = 5;
-        }else if (inputSize.value > 50) {
-            inputSize.value = 50;
-        }
-                 
-        valueInput = inputSize.value
-        console.log(valueInput)
-    })
-}
-
-
-checkValue();
-
-
-
-
-
-
-
-///
-generateAleatoryColor()
-buttonClear();
 selectedColor();
-// whitePixels();
+
+
+clearPixelBoard = () => {
+    for (let i = 0; i < pixels.length; i++) {
+        pixels[i].style.backgroundColor = '#fff';
+        console.log(pixels[i])
+    };
+};
+buttonClear.addEventListener('click', clearPixelBoard)
+
+
+
+
