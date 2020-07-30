4d964a6009bcf2d90d3b9ee3c974121f81e98c0b
Danilo <dansantosalfa@gmail.com>
2020-07-30 19:00:25 -0300

    Desenvolvendo mais o script de seleção de cores

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 window.onload = function () {
+    let colorPalette = document.querySelectorAll('color');
     let colorSelected = document.querySelector('.selected');
     let buttonClear = document.querySelector('#clear-board');
     let allPixel = document.getElementsByTagName('td');
+
+    function SelectColor() {
+        for (let index = 0; index < colorPalette.length; index += 1) {
+            colorPalette[index].classList.remove('selected');
+        }
+        this.classList.add('selected');
+        colorSelected = document.querySelector('.selected');
+    }
 
     function printPixel() {
         this.className = 'pixel ${colorSelected.classList[1]}'
@@ -14,8 +23,7 @@ window.onload = function () {
 
     buttonClear.addEventListener('click', function () {
         for (let index = 0; index < allPixel.length; index += 1) {
-            allPixel[index].className = 'pixel';
-            
+            allPixel[index].className = 'pixel';            
         }
     });
 };
