85129488028987a5444ed51dd4b2cd3b0d4f2139
Alana Monteiro <lanaah.15@gmail.com>
2020-07-30 16:20:50 -0300

    teoricamente finalizado

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let selectedPalette = document.querySelectorAll(".selected")[0];
-let currentColor = selectedPalette.style.background;
+let currentColor = "black";
 
 function colorSelect() {
     selectedPalette.classList.remove("selected");
     selectedPalette = event.target;
     selectedPalette.classList.add("selected");
-    currentColor = selectedPalette.style.background;
-    console.log(selectedPalette);
-    console.log(currentColor);
-}
+    let colorId = selectedPalette.id;
+    let colorDiv = document.getElementById(colorId);
+    currentColor = colorDiv.style.backgroundColor;
+    }
+
+    function colorSet(){
+       let newPixel = event.target;
+      newPixel.classList.add("paint");
+      let newColor = document.getElementsByClassName("paint")[0];
+      newColor.setAttribute("style", "background: " + currentColor);
+      newPixel.classList.remove("paint");
+    }
