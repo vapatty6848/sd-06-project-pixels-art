14e3734f0a6fd33776ff7c11596ced12e0357e45
Rods <rods.leite27@gmail.com>
2020-07-29 21:09:39 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const pixels = document.querySelectorAll('.pixel');
 const colors = document.querySelectorAll('.color');
 const clearBoard = document.getElementById('clear-board')
+const inputSize = document.getElementById('board-size')
+const vqvButton = document.getElementById('generate-board')
+const pixelBoard = document.getElementById('pixel-board')
 let = corSelecionada = 'black';
 colors[0].classList.add('selected');
 
@@ -17,8 +19,8 @@ for (let cor = 0; cor < colors.length; cor++) {
 
   })
 }
-
 //Pinta o pixel com a cor selecionada
+const pixels = document.querySelectorAll('.pixel');
 for(let pixel = 0; pixel < pixels.length; pixel += 1){
   pixels[pixel].addEventListener('click', function () {
     pixels[pixel].style.backgroundColor = corSelecionada;
@@ -32,6 +34,26 @@ clearBoard.addEventListener('click', function () {
   }
 })
 
+vqvButton.addEventListener('click', function () {
+  if (inputSize.value < 5) {
+    inputSize.value = 5
+  } else if (inputSize.value > 7) {
+    inputSize.value = 7
+  }
+  let table = document.createElement('table')
+  document.getElementsByTagName('article')[0].appendChild(table)
+  let tbody = document.createElement('tbody')
+  table.appendChild(tbody)
+  for (let i = 0; i < inputSize.value; i+= 1) {
+    let linha = document.createElement('tr')
+    tbody.appendChild(linha)
+    for (let i2 = 0; i2 < inputSize.value; i2+= 1) {
+      let square = document.createElement('td')
+      linha.appendChild(square) 
+      square.classList.add('pixel')
+    }
+  }
+})
 
 
 
