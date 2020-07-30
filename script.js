e6866e5fe3ebe0c327916255a0836d32a1250a3a
Matheus Dias <matheuscara2000@gmail.com>
2020-07-30 17:08:05 -0300

    Adicionando Ultimos quesitos

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
-//load inicial da pagina
-window.onload = desenhandoBlocos();
+// variaveis
+const colorPalette = document.querySelector('#color-palette');
+const tableColor = document.getElementById('pixel-board');
+const corSelected = document.getElementsByClassName('color');
 
-//Functions
-function desenhandoBlocos(){
-    let bloco = document.getElementById("pixel-board")
-    for (let i=0;i<5;i++){
-        let linha = document.createElement("div")
-        linha.style.height="40px"
-        linha.style.width="210px"
-        bloco.appendChild(linha)
-        for (let o=0;o<5;o++){
-            let blocoUnico=document.createElement("div")
-            blocoUnico.className="pixel"
-            blocoUnico.style.backgroundColor="white"
-            blocoUnico.style.width="40px"
-            blocoUnico.style.height="40px"
-            blocoUnico.style.border="1px solid black"
-            blocoUnico.style.display="inline-block"
-            linha.appendChild(blocoUnico)
-        }
-    }
+
+const colors = ['black', 'blue', 'yellow', 'red'];
+
+// functions
+for (let i = 0; i < colors.length; i += 1) {
+  const createBoxColor = document.createElement('li');
+  createBoxColor.className = 'color';
+  createBoxColor.style.backgroundColor = colors[i];
+  colorPalette.appendChild(createBoxColor);
 }
-function limpaBlocos(){
-    let pixelLimpo = document.getElementsByClassName('pixel')
-    for( let i=0; i< pixelLimpo.length ; i+=1){
-        pixelLimpo[i].style.backgroundColor = 'white';
-    }
+window.onload = function () {
+  colorSelected[0].className += ' selected';
+};
+
+for (let i = 0; i < 5; i += 1) {
+  const createTr = document.createElement('tr');
+  createTr.className = 'tabela';
+  tableColor.appendChild(createTr);
+  for (let index = 0; index < 5; index += 1) {
+    const createTd = document.createElement('td');
+    createTd.className = 'pixel';
+    createTd.backgroundColor = 'white';
+    createTr.appendChild(createTd);
+  }
 }
-//Variaveis
-let colorIninit = document.querySelector('.selected')
-let corDeDentro = document.getElementById('pixel-board')
-let limpaBloco = document.querySelector('#clear-board');
-
-//Eventos 
-corDeDentro.addEventListener('click',function(event){
-    if(event.target.id != "pixel-board"){
-        event.target.style.backgroundColor = colorIninit.id;
-    }
-})
-limpaBloco.addEventListener('click',limpaBlocos);
+function changeSelected(select) {
+    const selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    select.className += ' selected';
+}
 
 
+//events
+colorPalette.addEventListener('click', function (e) {
+  changeSelected(e.target);
+});
 
-
-
+tableColor.addEventListener('click', function (e) {
+  e.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+});
