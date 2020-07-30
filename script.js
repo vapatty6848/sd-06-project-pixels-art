6431fb661516c63249569fb5cc6dae3058d8b644
DekoPossas <andrepossaster@gmail.com>
2020-07-30 18:34:56 -0300

    adicinei function de pintar. formateis os codumentos, e coloquei os stilos em ordem alfabetica.

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
 // Variaveis
 
-const paletColor = document.querySelector('#color-palette');
-const arrayCollor = ['black', 'red', 'blue', 'yellow'];
+const paletColor = document.querySelector("#color-palette");
+const arrayCollor = ["black", "red", "blue", "yellow"];
 
-for (let index = 0; index < arrayCollor.length; index += 1){
-  const createBoxCollor = document.createElement('li');
-  createBoxCollor.className = 'color';
+for (let index = 0; index < arrayCollor.length; index += 1) {
+  const createBoxCollor = document.createElement("li");
+  createBoxCollor.className = "color";
   createBoxCollor.style.backgroundColor = arrayCollor[index];
   paletColor.appendChild(createBoxCollor);
 }
 
-const tableCollor = document.querySelector('#pixel-board');
+const tableCollor = document.querySelector("#pixel-board");
 
-for(let i = 0; i < 5; i += 1) {
-  const createBoxLine = document.createElement('tr');
-  createBoxLine.className = 'pixel-line';
+for (let i = 0; i < 5; i += 1) {
+  const createBoxLine = document.createElement("tr");
+  createBoxLine.className = "pixel-line";
   tableCollor.appendChild(createBoxLine);
-  for(let c = 0; c < 5; c+= 1) {
-    const createBoxCl = document.createElement('td');
-    createBoxCl.className = 'pixel';
+  for (let c = 0; c < 5; c += 1) {
+    const createBoxCl = document.createElement("td");
+    createBoxCl.className = "pixel";
     createBoxLine.appendChild(createBoxCl);
   }
 }
 
 window.onload = function () {
-  const colors = document.getElementsByClassName('color');
-  colors[0].className += ' selected';
-}
+  const colors = document.getElementsByClassName("color");
+  colors[0].className += " selected";
+};
 
 function colorSelect(newSelect) {
-  const colorSelected = document.querySelector('.selected');
-  colorSelected.classList.remove('selected');
-  newSelect.className += ' selected';
+  const colorSelected = document.querySelector(".selected");
+  colorSelected.classList.remove("selected");
+  newSelect.className += " selected";
 }
 
-paletColor.addEventListener('click', function(e) {
+paletColor.addEventListener("click", function (e) {
   colorSelect(e.target);
+});
+
+tableCollor.addEventListener("click", function (e) {
+  e.target.style.backgroundColor = document.querySelector(
+    ".selected"
+  ).style.backgroundColor;
+  console.log(document.querySelector(".selected").style.backgroundColor);
 });
