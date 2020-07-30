ae2cf57663b4078eecedf5a9b7bf6f01bcb0e9d0
Pablo Santos <pablo.trybe@gmail.com>
2020-07-29 21:57:30 -0300

    Implementando funçoes finais

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
-const colorSelected = document.querySelector('selected');
-function getColor(e) {
+function getColor(color) {
+  localStorage.setItem('colorSelected', color);
+  if (localStorage.getItem('colorSelected') === 'black') {
+    document.getElementById('black').classList.add('selected');
+    document.getElementById('pink').classList.remove('selected');
+    document.getElementById('red').classList.remove('selected');
+    document.getElementById('blue').classList.remove('selected');
+  } else if (localStorage.getItem('colorSelected') === 'pink') {
+    document.getElementById('black').classList.remove('selected');
+    document.getElementById('pink').classList.add('selected');
+    document.getElementById('red').classList.remove('selected');
+    document.getElementById('blue').classList.remove('selected');
+  } else if (localStorage.getItem('colorSelected' === 'red')) {
+    document.getElementById('black').classList.remove('selected');
+    document.getElementById('pink').classList.remove('selected');
+    document.getElementById('red').classList.add('selected');
+    document.getElementById('blue').classList.remove('selected');
+  } else {
+    document.getElementById('black').classList.remove('selected');
+    document.getElementById('pink').classList.remove('selected');
+    document.getElementById('red').classList.remove('selected');
+    document.getElementById('blue').classList.add('selected');
+  }
+}
+const btnBlack = document.getElementById('black');
+btnBlack.addEventListener('click', function () {
+  getColor('black');
+});
+const btnPink = document.getElementById('pink');
+btnPink.addEventListener('click', function () {
+  getColor('pink');
+});
+const btnRed = document.getElementById('red');
+btnRed.addEventListener('click', function () {
+  getColor('red');
+});
+const btnBlue = document.getElementById('blue');
+btnBlue.addEventListener('click', function () {
+  getColor('blue');
+});
+
+function setColor(pixel, color) {
+  pixel.style.backgroundColor = color;
+}
+const pixelSelected = document.getElementsByClassName('pixel');
+for (let i = 0; i < pixelSelected.length; i += 1) {
+  pixelSelected[i].onclick = function () {
+    setColor(pixelSelected[i], localStorage.getItem('colorSelected'));
+  }
 }
