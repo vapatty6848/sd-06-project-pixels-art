fc398fce3bb16174fffb8fd5dfcd2c4d31d7b59e
DekoPossas <andrepossaster@gmail.com>
2020-07-30 16:37:08 -0300

    salvei e reenviei

--- script.js
+++ script.js
@@ -3,9 +3,22 @@
 const paletColor = document.querySelector('#color-palette');
 const arrayCollor = ['black', 'red', 'blue', 'yellow'];
 
-for (let i = 0; i < arrayCollor.length; i += 1){
+for (let index = 0; index < arrayCollor.length; index += 1){
   const createBoxCollor = document.createElement('li');
-  createBoxCollor.className = 'color';
-  createBoxCollor.style.backgroundColor = arrayCollor[i];
+  createBoxCollor.className = 'collor';
+  createBoxCollor.style.backgroundColor = arrayCollor[index];
   paletColor.appendChild(createBoxCollor);
+}
+
+const tableCollor = document.querySelector('#pixel-board');
+
+for(let i = 0; i < 5; i += 1) {
+  const createBoxLine = document.createElement('tr');
+  createBoxLine.className = 'pixel-line';
+  tableCollor.appendChild(createBoxLine);
+  for(let c = 0; c < 5; c+= 1) {
+    const createBoxCl = document.createElement('td');
+    createBoxCl.className = 'pixel';
+    createBoxLine.appendChild(createBoxCl);
+  }
 }
