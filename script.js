fdde83874e1f65522e1d9a7c10898be511db71ba
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2020-07-30 16:06:20 -0300

    finalizando JS

--- script.js
+++ script.js
@@ -3,11 +3,37 @@ window.onload = function () {
     const cor = palette.children;
     cor[0].style.backgroundColor = 'black';
     cor[1].style.backgroundColor = 'blue';
-    cor[2].style.backgroundColor = 'yellow';
-    cor[3].style.backgroundColor = 'red';
-
+    cor[2].style.backgroundColor = 'orange';
+    cor[3].style.backgroundColor = 'yellow';
     for (let i = 0; i < 25; i += 1) {
-        const square = document.createElement('div');
-        square.className = 'pixel';
-        document.querySelector('#pixel-board').appendChild(square);
+        const div = document.createElement('div');
+        div.className = 'pixel';
+        document.querySelector('#pixel-board').appendChild(div);
+    }
+    function chClass(event) {
+        const pixelColor = document.querySelector('.selected');
+        const nPixelColor = event.target;
+        pixelColor.classList.remove('selected');
+        nPixelColor.classList.add('selected');
+    }
+    const square = document.querySelector('#pixel-board');
+    let color = 'black';
+    colocarCor(color);
+    function colocarCor(color) {
+        square.addEventListener('click', function (event) {
+            event.target.style.backgroundColor = color;
+        });
+    }
+    palette.addEventListener('click', function (event) {
+        chClass(event);
+        color = event.target.style.backgroundColor;
+        colocarCor(color);
+    });
+    const clear = document.querySelector('#clear-board');
+    const pixel = square.children;
+    clear.addEventListener('click', function () {
+        for (let i = 0; i < pixel.length; i += 1) {
+            pixel[i].style.backgroundColor = 'white';
         }
+    });
+};
