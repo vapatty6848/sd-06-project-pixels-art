2e4ff6ca7d201024df531f347164a0b693635262
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-07-30 16:47:48 -0300

    Modificação da criação dos pixels por javascript

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 window.onload = function () {
-    function handlePallet (event){
-        let currentDiv = document.querySelector('.selected');
-        let newDiv = event.target;
-        currentDiv.classList.remove('selected');
-        newDiv.classList.add('selected');
-        let bkgrdColor = div.style.backgroundColor;
+    let selected = '';
+    createPixelBoard(5);
+      document.getElementById('pixel-board').addEventListener('click', alternativeClickPixel);
+      document.getElementById('color-palette').addEventListener('click', alternativeSelectColor);
+      document.getElementById('clear-board').addEventListener('click', clearBoard);
+      document.getElementById('generate-board').addEventListener('click', setBoardSize);
+      selected = window.getComputedStyle(document.getElementsByClassName('color')[0], null).getPropertyValue('background-color');
+    
+    
+    function alternativeClickPixel(event) {
+      event.target.style.backgroundColor = selected;
     }
-    function createPallet (color) {
-        let palletDiv = document.createElement('div');
-        palletDiv.style.backgroundColor = color;
-        palletDiv.className = 'pallet-item';
-        palletDiv.addEventListener('click', handlePallet);
-        if (color === 'black') {
-            palletDiv.classList.add('selected');
+    
+    function alternativeSelectColor(event) {
+      for (let item = 0; item < document.getElementsByClassName('color').length; item += 1) {
+        document.getElementsByClassName('color')[item].classList.remove('selected');
+      }
+      event.target.classList.add('selected');
+      selected = window.getComputedStyle(document.getElementsByClassName('selected')[0], null).getPropertyValue('background-color');
+    }
+    
+    function clearBoard() {
+      for (let element = 0; element < document.getElementsByClassName('pixel').length; element += 1) {
+        document.getElementsByClassName('pixel')[element].style.backgroundColor = 'white';
+      }
+    }
+    
+    function createPixelBoard(parameter) {
+        for (let line = 0; line < parseInt(parameter, 10); line += 1) {
+          const linha = document.createElement('div');
+          linha.classList.add('tr');
+          document.getElementById('pixel-board').appendChild(linha);
+          for (let column = 0; column < parameter; column += 1) {
+            const coluna = document.createElement('div');
+            coluna.classList.add('td', 'pixel');
+            document.getElementsByClassName('tr')[line].appendChild(coluna);
+          }
         }
-        return palletDiv;
     }
 }
+    
