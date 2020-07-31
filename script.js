eb3ce8217fc618f7886fccb5080504c13ea3ff5b
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-07-31 12:21:04 -0300

    Add function to user define a size board

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const mainPalette = document.getElementById('color-palette');
 const boardGrid = document.getElementById('pixel-board');
 const btnClear = document.getElementById('clear-board');
 let presentColor = 'black';
+const numberBoardSize = document.querySelector('.board-size');
+const btnGenerateBoard = document.querySelector('.generate-board');
 
 // Adiciona novas opções de cores;
 const generateColor = ['black', 'red', 'blue', 'green'];
@@ -13,10 +15,10 @@ function selectColor(event) {
   selectedColor.classList.remove('selected');
   currentColor.classList.add('selected');
   presentColor = currentColor.style.backgroundColor;
-};
+}
 
 // Cria novos pixels de cores para a paleta;
-function createPixelColor(color) {
+function createPixelPalette(color) {
   const palettePixel = document.createElement('div');
   palettePixel.style.backgroundColor = color;
   palettePixel.className = 'color';
@@ -26,32 +28,57 @@ function createPixelColor(color) {
     palettePixel.classList.add('selected');
   }
   return palettePixel;
-};
+}
 
 // Adiciona as novas cores a paleta;
 function generatePalette(colorOptions) {
   for (let index = 0; index < colorOptions.length; index += 1) {
-    const paletteOptions = createPixelColor(colorOptions[index]);
+    const paletteOptions = createPixelPalette(colorOptions[index]);
     mainPalette.appendChild(paletteOptions);
   }
-};
+}
 generatePalette(generateColor);
+
+// Verifica se a quantidade de elementos e limpa o board;
+function verifyElementsNumbers(numberElements) {
+  const pixels = document.querySelectorAll('.pixel');
+  if (numberElements === '') {
+    alert('Board inválido!');
+    numberElements = 5;
+    numberBoardSize.value = 5;
+  }
+  if (numberElements < 5) {
+    numberElements = 5;
+    numberBoardSize.value = 5;
+  }
+  if (numberElements > 50) {
+    numberElements = 50;
+    numberBoardSize.value = 50;
+  }
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].remove();
+  }
+  return numberElements;
+}
 
 // Cria o grid de pixel
 function createGrid(numberElements) {
+  numberElements = verifyElementsNumbers(numberBoardSize.value);
   for (let index = 0; index < (numberElements ** 2); index += 1) {
     const pixel = document.createElement('div');
     boardGrid.appendChild(pixel);
+    boardGrid.style.display = 'grid';
+    boardGrid.style.gridTemplateColumns = `repeat(${numberElements}, 40px)`;
     pixel.className = 'pixel';
   }
-};
-createGrid(5);
+}
+btnGenerateBoard.addEventListener('click', createGrid);
 
 // Pinta os pixels da cor selecionada;
 function printPixel(event) {
   const selectedPixel = event.target;
   selectedPixel.style.backgroundColor = presentColor;
-};
+}
 boardGrid.addEventListener('click', printPixel);
 
 // Deixa o grid em branco;
