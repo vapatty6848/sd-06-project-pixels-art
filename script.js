6cdcf29c2e9732ae9effeee2db865e7df13ee0e1
Zeca Costa <jccostaso@gmail.com>
2020-07-31 07:01:31 -0300

    Atualizaçao para inclusao da funcionalidade 'preencher pixels do board' com selected color.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+let selectedColor = 'black'
+
 window.onload = function() {
   addColorPallet(['black', 'red', 'blue', 'green']);
   createBoardRows();
   addButtonEvent();
-};
+  pixelBoardSelection();
+}
 
-// COLOR PALLET AREA
+// COLOR PALLET
 // Função para adicionar os itens ao pallet
 function addColorPallet(colors) {
   const colorPalletWrap = document.querySelector('#color-palette');
@@ -30,13 +33,14 @@ function createPalletItem(color) {
 
 // função para tratar do evento
 function handlePalletItemEvent(event) {
-  const lastSelectedDiv = document.querySelector('.selected')
+  const lastSelectedDiv = document.querySelector('.selected');
   const nowSelectedDiv = event.target;
   lastSelectedDiv.classList.remove('selected');
   nowSelectedDiv.classList.add('selected');
+  selectedColor = nowSelectedDiv.style.backgroundColor
 }
 
-// PIXEL BOARD AREA
+// PIXEL BOARD
 // Função adicionar rows
 function createBoardRows() {
   const PixelBoardContainer = document.querySelector('#pixel-board');
@@ -46,7 +50,7 @@ function createBoardRows() {
     rowDiv = document.createElement('section');
     rowDiv.classList.add('board-row' + index);
     PixelBoardContainer.appendChild(rowDiv);
-    let boardRowContainer = document.querySelector('.board-row' + [index]);
+    const boardRowContainer = document.querySelector('.board-row' + [index]);
 
     for (let j = 1; j <= 5; j += 1) {
       const pixelDiv = createPixel();
@@ -59,6 +63,16 @@ function createPixel() {
   const pixelDiv = document.createElement('div');
   pixelDiv.classList.add('pixel');
   return pixelDiv;
+}
+ // Função para selecionar board e criar um evento
+function pixelBoardSelection() {
+  const pixelsBoard = document.querySelector('#pixel-board');
+  pixelsBoard.addEventListener('click', handlePixelsEvent);
+}
+
+function handlePixelsEvent(event) {
+  const pixelSelect = event.target;
+  pixelSelect.style.backgroundColor = selectedColor;
 }
 
 // BUTTON
