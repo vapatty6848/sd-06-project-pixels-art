5893c09ba40672ac3958970ae63217639777b89a
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-07-31 11:06:06 -0300

    Resolução Requisito Bonus 12

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ window.onload = function () {
   let myColor03 = document.getElementById("color03");
   let myColor04 = document.getElementById("color04");
 
-  // Cor preta para paleta 01
-  myColor01.style.backgroundColor = "rgb(0 , 0 , 0)";
+  // Atribui Cor preta para paleta 01
+  let colorSelected = "rgb(0 , 0 , 0)";
+  myColor01.style.backgroundColor = colorSelected;
   myColor01.addEventListener("click", function () {
 		chageSelected(myColor01);
   });	
@@ -16,41 +17,22 @@ window.onload = function () {
   generateRandomColor(myColor03);
   generateRandomColor(myColor04);
 
+    // gera cores aleatórias 
   function generateRandomColor(color) {
-		let col1 = Math.floor(Math.random() * (250));
-		let col2 = Math.floor(Math.random() * (250));
-		let col3 = Math.floor(Math.random() * (250));
+    let col1 = Math.floor(Math.random() * (250));
+    let col2 = Math.floor(Math.random() * (250));
+    let col3 = Math.floor(Math.random() * (250));
     color.style.backgroundColor = "rgb(" + col1 + " , " + col2 + " , " + col3 + ")";  
     color.addEventListener("click", function () {
-    	chageSelected(color);
+      chageSelected(color);
     });
   }
-  
-  
-  // myColor02.style.backgroundColor = "rgb(155 , 50 , 8)";  
-	// myColor02.addEventListener("click", function () {
-	// 	chageSelected(myColor02);
-	// });
 
-  
-  // myColor03.style.backgroundColor = "rgb(198 , 168 , 108)";  
-	// myColor03.addEventListener("click", function () {
-	// 	chageSelected(myColor03);
-	// });	
-
-  
-  // myColor04.style.backgroundColor = "rgb(45 , 75 , 139)";
-	// myColor04.addEventListener("click", function () {
-	// 	chageSelected(myColor04);
-  // });	
-  
-  let colorSelected = "rgb(0 , 0 , 0)";
-  
+  // Muda a classe selected
   function chageSelected(newSelected){
     let oldSelected = document.querySelector(".selected");
     oldSelected.classList.remove("selected");
     newSelected.classList.add("selected");
-
     colorSelected = newSelected.style.backgroundColor;
   }
 
@@ -69,5 +51,6 @@ window.onload = function () {
       myItemPixel.style.backgroundColor = "rgb(255 , 255 , 255)";
     }
   });
-
 }
+
+
