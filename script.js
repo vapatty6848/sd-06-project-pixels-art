bf33e8fafab5b75a867683983605af447fea3b7d
ilanribeiro <ilanribeiro@gmail.com>
2020-07-31 15:05:54 -0300

    fixing code climate

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-
+// Constants
 const colors = ['black', 'green', 'blue', 'orange'];
-const colorPallet = document.getElementsByClassName('color-palette')[0];
-const pixelBoard = document.querySelector('.pixel-board');
 
 // build color pallet based in quantity colors specified in array colors;
 function buildPallet(color) {
   let buildedDiv;
+  let colorPallet = document.getElementsByClassName('color-palette')[0];
+
   for (let index = 0; index < color.length; index += 1) {
     buildedDiv = document.createElement('div');
     buildedDiv.className = 'color';
@@ -16,12 +16,14 @@ function buildPallet(color) {
       buildedDiv.classList.add('selected');
     }
   }
-};
+}
 buildPallet(colors);
 
 // build pixel board
-const n = 5;
+const linesAndColumns = 5;
 function buildPixelBoard(n) {
+  let pixelBoard = document.querySelector('.pixel-board');
+
   for (let index1 = 1; index1 <= n; index1 += 1) {
     let buildPix;
     for (let index2 = 1; index2 <= n; index2 += 1) {
@@ -31,30 +33,32 @@ function buildPixelBoard(n) {
       pixelBoard.appendChild(buildPix);
     }
   }
-};
-buildPixelBoard(n);
+}
+buildPixelBoard(linesAndColumns);
 
 // builded clicks behavior in pixel boxes or in color boxes
-window.onclick = function() {
+window.onclick = function () {
   const selectedColor = document.querySelector('.selected');
   if (event.target.className === 'color') {
     event.target.classList.add('selected');
     selectedColor.classList.remove('selected');
-  };
+  }
 
   if (event.target.className === 'pixel') {
     event.target.style.backgroundColor = selectedColor.style.backgroundColor;
-  };
+  }
 };
 
 // build a button to clear pixel board
 const toClearBoard = document.querySelector('.clear-board');
-const pixelsInBoard = document.querySelectorAll('.pixel');
-
-toClearBoard.addEventListener('click', clearAllBoard);
 
 function clearAllBoard() {
+  let pixelsInBoard = document.querySelectorAll('.pixel');
+
   for (let indexClear = 0; indexClear < pixelsInBoard.length; indexClear += 1) {
     pixelsInBoard[indexClear].style.backgroundColor = 'white';
   }
-};
+}
+
+// add click event to the ClearBoard button
+toClearBoard.addEventListener('click', clearAllBoard);
