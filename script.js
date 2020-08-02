ff119ba1f8890842303d96600a89afcd90ec7722
gussta <saideles.silveira@gmail.com>
2020-08-02 10:05:57 -0300

    function button clear correcao e alguns codeclimates

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-window.onclick = function() {
-  let palleteColor = document.querySelector(".selected");
-  if (event.target.className === "color") {
-    palleteColor.classList.remove("selected");
-    event.target.classList.add("selected");
+window.onclick = function () {
+  const palleteColor = document.querySelector('.selected');
+  if (event.target.className === 'color') {
+    palleteColor.classList.remove('selected');
+    event.target.classList.add('selected');
   }
 
-  if (event.target.className === "pixel") {
+  if (event.target.className === 'pixel') {
     event.target.style.backgroundColor = palleteColor.id;
   }
 }
 
 function clearBoard() {
-  const pixelBoard = document.querySelectorAll('.pixel');
+const pixelBoard = document.querySelectorAll('.pixel');
   for (let i = 0; i < pixelBoard.length; i += 1) {
-  pixelBoard[i].style.backgroundColor = 'white';
-  pixelBoard[i].style.color = 'white';
+    pixelBoard[i].style.backgroundColor = 'white';
+    pixelBoard[i].style.color = 'white';
   }
 }
