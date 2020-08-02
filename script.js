293304519d8fe7463a624da3bc3bf2f3068de9a2
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2020-08-01 21:15:51 -0300

    criando divs dinamicamente com jse tentando passar algum requisito

--- script.js
+++ script.js
@@ -15,25 +15,12 @@ window.onload = function() {
 
     createColorPallet(['black', 'red', 'blue', 'yellow']);
 
+    createPixelsBoard(5, 5);
 
 
-    let corClasse = document.querySelectorAll(".color")
-    let selectedColor = document.querySelector(".selected")
 
-    document.querySelectorAll(".color").forEach(item => {
-
-        item.addEventListener("click", function() {
-            item.add.classList(".selected")
-            document.querySelectorAll(".selected")
-        })
-
-    })
 
 }
-
-
-
-
 
 
 
@@ -62,7 +49,7 @@ function createColorPallet(colors) {
 
         if (color === "black") {
 
-            palleteItemDiv.classList.add(".selected");
+            palleteItemDiv.classList.add("selected");
         }
 
         return palleteItemDiv;
@@ -73,11 +60,31 @@ function createColorPallet(colors) {
 function palleteItemEvent() {
 
     let OldSelectedDiv = document.querySelector(".selected");
-
-
     let divCurrentSelectedDiv = event.target;
 
-    let backgroundColor = div.style.backgroundColor;
+    OldSelectedDiv.classList.remove("selected");
+
+    divCurrentSelectedDiv.classList.add("selected");
+
+    let backgroundColor = divCurrentSelectedDiv.style.backgroundColor;
+}
+
+function createPixelsBoard(lines, columns) {
+
+    let DivPixelsBoard = document.querySelector("#pixel-board");
+
+    for (let index = 0; index < lines; index++) {
+
+        var divLine = document.createElement("div");
+        divLine.classList.add('divLine')
+
+        for (let index = 0; index < columns; index++) {
+            let divPixels = document.createElement("div");
+            divPixels.className = "pixel";
+            divLine.appendChild(divPixels);
+        }
+        DivPixelsBoard.appendChild(divLine);
+    }
 
 
 
