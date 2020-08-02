495c6c22bb4c33dc75dbd7974e903c1dbbaa454a
Veruska Bittencourt <sousaveruska@gmail.com>
2020-08-02 18:11:52 -0300

    complemento css e js

--- script.js
+++ script.js
@@ -4,12 +4,54 @@ window.onload = function () {
     document.getElementById('red').style.backgroundColor = 'red';
     document.getElementById('green').style.backgroundColor = 'green';
     document.getElementById('yellow').style.backgroundColor = 'yellow';
-}
-function  colorChange  ( )  {
-    const  oldPixelSelected  =  documento . querySelector ( '.selected' ) ;
-    const  currentPixelSelected  =  evento . alvo ;
-    oldPixelSelected . classList . remove ( 'selecionado' ) ;
-    currentPixelSelected . classList . add ( 'selecionado' ) ;
-    selectedColor  =  currentPixelSelected . estilo . backgroundColor ;
-}      
 
+    function selecionaCor () {
+        const oldSelected = document.querySelector('.selected');
+        const currentSelected = event.target;
+        oldSelected.classList.remove('selected');
+        currentSelected.classList.add('selected');
+        selectedColor = currentSelected.style.backgroundColor;
+    }   
+    const colorPalette = document.querySelector('#color-palette');
+    colorPalette.addEventListener('click', selecionaCor);
+
+    function selecionaQuadrado () {
+        event.target.style.backgroundColor = selectedColor;
+    }
+    const pixelFrame = document.querySelector('#pixel-board');
+    pixelFrame.addEventListener('click', selecionaQuadrado);
+
+    function clearQuadrado () {
+        const pixelFrame = document.getElementsByClassName('pixel');
+        for(let i = 0; i < pixelFrame.length; i += 1) {
+            pixelFrame[i].style.backgroundColor = 'white';
+        }
+    }
+    const clearBoard = document.getElementById('clear-board');
+    clearBoard.addEventListener('click', clearQuadrado)
+
+
+
+    function createFramePixel () {
+        const num = document.getElementById('board-size').value;
+        const aux = document.getElementById('pixel-board');
+        while(aux.firstChild) {
+            aux.firstChild.remove();
+        }
+        for(let i = 0; i < num.length; i += 1) {
+            const aux = document.getElementById('pixel-board');
+            let divLinePixel = document.createElement('div');
+            divLinePixel.classList.add('line-pixel');
+            aux.appendChild(divLinePixel);
+            for(let a = 0; a < num.length; a += 1) {
+                const aux1 = document.querySelector('.line-pixel');
+                let divPixel = document.createElement('div');
+                divPixel.className = 'pixel';
+                aux1.appendChild(divPixel);
+            }
+        }
+    }
+
+    const generateBoard = document.getElementById('generate-board');
+    generateBoard.addEventListener('click', createFramePixel);    
+}
