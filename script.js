5cdcebacd665eeb5b76b87bed790b420e37ad896
Veruska Bittencourt <sousaveruska@gmail.com>
2020-08-01 22:41:21 -0300

    começando pelo html/js/css

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//
+window.onload = function () {
+    let selectedColor = 'black';
+    document.getElementById('black').style.backgroundColor = 'black';
+    document.getElementById('red').style.backgroundColor = 'red';
+    document.getElementById('green').style.backgroundColor = 'green';
+    document.getElementById('yellow').style.backgroundColor = 'yellow';
