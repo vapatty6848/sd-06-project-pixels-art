42ab8f0e4c2e78e0e355437b87fabcb6605853b9
FlavioSugano <flavio.sugano@gmail.com>
2020-08-01 22:07:51 -0300

    Alteração no script.js para realizar a seleção da cor da paleta através da troca de classe

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 window.onload = function () {
-  let selectedColor = 'black';
   let palette = document.getElementsByClassName('color');
+  let selectedColor = document.getElementsByClassName('selected')[0].style.backgroundColor;
   let pixelBoard = document.getElementsByClassName('pixel');
   let clear = document.querySelector('#clear-board');
-  
-  // captura a cor após o click na paleta de cores
-  for (let i = 0; i < palette.length; i += 1) {
-    palette[i].addEventListener('click', function () {
-      selectedColor = palette[i].style.backgroundColor;
-      console.log(selectedColor)
-    })
+
+  function clearPixelBoard () {
+    for (let i = 0; i < pixelBoard.length; i += 1) {
+      pixelBoard[i].style.backgroundColor = 'white';
+    }
   }
 
+  function changeColor (event) {
+    let color = event.target
+
+    if (color.classList.contains('selected')) {
+      selectedColor = color.style.backgroundColor;
+    } else {
+      document.getElementsByClassName('selected')[0].classList.remove('selected');
+      selectedColor = color.style.backgroundColor;
+      color.classList.add('selected');
+    }
+  }
+
+  // troca as cores inseridas no pixel board por branco
+  clear.addEventListener('click', clearPixelBoard)
+
+  // captura a cor após o click na paleta de cores
+  for (let i = 0; i < palette.length; i += 1) {
+    palette[i].addEventListener('click', changeColor)
+  }
+
+  // cada div da classe pixel ganha a cor que estiver selecionada na paleta após o clique
   for (let i = 0; i < pixelBoard.length; i += 1) {
-    pixelBoard[i].addEventListener('click', function () {
+    pixelBoard[i].addEventListener('click', function() {
       pixelBoard[i].style.backgroundColor = selectedColor;
     })
   }
-
-  // limpa as cores do pixel-board
-  clear.addEventListener('click', function () {
-    for (let i = 0; i < pixelBoard.length; i += 1) { 
-      pixelBoard[i].style.backgroundColor = 'white';
-    }
-  })
 }
