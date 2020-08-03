2e1c1d4dfa5fd6787d4f75dfcc9440e9c896c8c9
lucasvr39 <lucasveras1994@gmail.com>
2020-08-03 11:22:19 -0300

    add events and functions for req 10 and 11

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let selectedColor = 'black';
 const pixelBoard = document.querySelector('#pixel-board');
+const btnGenerateBoard = document.getElementById('generate-board');
+const boardSizeInput = document.getElementById('board-size');
 
 // handles the event to get selected pixel and change div's class
 
@@ -35,21 +37,13 @@ function generateRandomColor() {
 
 function createColorPalette(colorArray) {
   const colorPaletteContainer = document.getElementById('color-palette');
-
   for (let index = 0; index < colorArray.length; index += 1) {
     const pixelDiv = createPaletteColorDiv(colorArray[index]);
     colorPaletteContainer.appendChild(pixelDiv);
   }
 }
 
-window.onload = createColorPalette([
-  'black',
-  generateRandomColor(),
-  generateRandomColor(),
-  generateRandomColor(),
-]);
-
-// handle event to change div's background color and make the pixel art
+// add event to change div's background color and make the pixel art
 
 pixelBoard.addEventListener('click', function (event) {
   const divPixel = event.target;
@@ -70,17 +64,49 @@ btnClear.addEventListener('click', function () {
 
 // get board size user input to generate the board pixels
 
-const btnGenerateBoard = document.getElementById('generate-board');
-btnGenerateBoard.addEventListener('click', function () {
-  let userInput = document.getElementById('board-size').value;
-  if (!userInput) {
+function createPixelDiv(userInput) {
+  for (let line = 0; line < userInput; line += 1) {
+    const createPixelLine = document.createElement('div');
+    createPixelLine.className = 'pixel-line';
+    pixelBoard.appendChild(createPixelLine);
+    for (let pixel = 0; pixel < userInput; pixel += 1) {
+      const createNewPixel = document.createElement('div');
+      createNewPixel.className = 'pixel';
+      createPixelLine.appendChild(createNewPixel);
+    }
+  }
+}
+
+function createBoard(userInput) {
+  if (userInput === '') {
     alert('Board inválido!');
   }
-
   if (userInput < 5) {
     userInput = 5;
   } else if (userInput > 50) {
     userInput = 50;
   }
-  // return function(userInput);
+  createPixelDiv(userInput);
+}
+
+// call functions to create color palette div and the pixel board after page load
+
+window.onload = function () {
+  createColorPalette([
+    'black',
+    generateRandomColor(),
+    generateRandomColor(),
+    generateRandomColor(),
+  ]);
+  createBoard(5);
+};
+
+// add event to generate the pixel board
+
+btnGenerateBoard.addEventListener('click', function () {
+  const pixelLines = document.querySelectorAll('.pixel-line');
+  for (let i = 0; i < pixelLines.length; i += 1) {
+    pixelBoard.removeChild(pixelLines[i]);
+  }
+  createBoard(boardSizeInput.value);
 });
