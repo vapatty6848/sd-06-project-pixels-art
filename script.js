13f903bffcd4d3d1ed36d50ab2e86def23a10764
lucasvr39 <lucasveras1994@gmail.com>
2020-08-02 21:02:17 -0300

    add button and first function for bonus req 10

--- script.js
+++ script.js
@@ -1,70 +1,86 @@
 let selectedColor = 'black';
+const pixelBoard = document.querySelector('#pixel-board');
 
-window.onload = function () {
-  // handles the event to get selected pixel and change div's class
+// handles the event to get selected pixel and change div's class
 
-  function handlePalettePixelColor(event) {
-    const lastSelectedDiv = document.querySelector('.selected');
-    const currentPixelDiv = event.target;
-    lastSelectedDiv.classList.remove('selected');
-    currentPixelDiv.classList.add('selected');
-    selectedColor = currentPixelDiv.style.backgroundColor;
+function handlePalettePixelColor(event) {
+  const lastSelectedDiv = document.querySelector('.selected');
+  const currentPixelDiv = event.target;
+  lastSelectedDiv.classList.remove('selected');
+  currentPixelDiv.classList.add('selected');
+  selectedColor = currentPixelDiv.style.backgroundColor;
+}
+
+// creates the colored div in the color palette
+
+function createPaletteColorDiv(color) {
+  const addPixelDiv = document.createElement('div');
+  addPixelDiv.style.backgroundColor = color;
+  addPixelDiv.className = 'color';
+  addPixelDiv.addEventListener('click', handlePalettePixelColor);
+  if (color === 'black') {
+    addPixelDiv.classList.add('selected');
   }
+  return addPixelDiv;
+}
 
-  // creates the colored div in the color palette
+// generate random color
 
-  function createPaletteColorDiv(color) {
-    const addPixelDiv = document.createElement('div');
-    addPixelDiv.style.backgroundColor = color;
-    addPixelDiv.className = 'color';
-    addPixelDiv.addEventListener('click', handlePalettePixelColor);
-    if (color === 'black') {
-      addPixelDiv.classList.add('selected');
-    }
-    return addPixelDiv;
+function generateRandomColor() {
+  const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return randomColor;
+}
+
+// creates color palette
+
+function createColorPalette(colorArray) {
+  const colorPaletteContainer = document.getElementById('color-palette');
+
+  for (let index = 0; index < colorArray.length; index += 1) {
+    const pixelDiv = createPaletteColorDiv(colorArray[index]);
+    colorPaletteContainer.appendChild(pixelDiv);
   }
+}
 
-  // generate random color
+window.onload = createColorPalette([
+  'black',
+  generateRandomColor(),
+  generateRandomColor(),
+  generateRandomColor(),
+]);
 
-  function generateRandomColor() {
-    let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    return randomColor;
-  }
-
-  // creates color palette
-
-  function createColorPalette(colorArray) {
-    const colorPaletteContainer = document.getElementById('color-palette');
-
-    for (let index = 0; index < colorArray.length; index += 1) {
-      const pixelDiv = createPaletteColorDiv(colorArray[index]);
-      colorPaletteContainer.appendChild(pixelDiv);
-    }
-  }
-
-  createColorPalette([
-    'black',
-    generateRandomColor(),
-    generateRandomColor(),
-    generateRandomColor(),
-  ]);
-};
 // handle event to change div's background color and make the pixel art
 
-document
-  .querySelector('#pixel-board')
-  .addEventListener('click', function (event) {
-    const divPixel = event.target;
-    divPixel.style.backgroundColor = selectedColor;
-  });
+pixelBoard.addEventListener('click', function (event) {
+  const divPixel = event.target;
+  divPixel.style.backgroundColor = selectedColor;
+});
 
 // add event to clean the board
 
 const btnClear = document.querySelector('#clear-board');
 btnClear.addEventListener('click', function () {
   for (let i = 0; i < document.querySelectorAll('.pixel').length; i += 1) {
-    document
-      .querySelectorAll('.pixel')
-      [i].style.removeProperty('background-color');
+    // prettier-ignore
+    document.querySelectorAll('.pixel')[i]
+      .style
+      .removeProperty('background-color');
   }
+});
+
+// get board size user input to generate the board pixels
+
+const btnGenerateBoard = document.getElementById('generate-board');
+btnGenerateBoard.addEventListener('click', function () {
+  let userInput = document.getElementById('board-size').value;
+  if (!userInput) {
+    alert('Board inválido!');
+  }
+
+  if (userInput < 5) {
+    userInput = 5;
+  } else if (userInput > 50) {
+    userInput = 50;
+  }
+  // return function(userInput);
 });
