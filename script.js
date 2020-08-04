f1e3fb3c64e5d901778de30491578a8cf0927b32
Macarena Rodriguez <macarenar697@gmail.com>
2020-08-04 20:15:58 -0300

    Se refaturo a maneira de criar quadrado- Se crio bao e input para funcao de escolher tamanho quadrado

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 window.onload = function () {
+  let tamanhoQuadro = 5;
+  window.onload = criarQuadrado(tamanhoQuadro);
   //  Criacao da paleta com os correspondentes cores
   const palette = document.querySelector('#color-palette');
   const colorsArray = palette.children;
@@ -6,12 +8,18 @@ window.onload = function () {
   colorsArray[1].style.backgroundColor = 'red';
   colorsArray[2].style.backgroundColor = 'orange';
   colorsArray[3].style.backgroundColor = 'green';
+
+
+  //  Quadrado
+  
+
   //  Criacao de quadrado
-  for (let i = 0; i < 25; i += 1) {
-    const div = document.createElement('div');
-    div.className = 'pixel';
-    document.querySelector('#pixel-board').appendChild(div);
-  }
+  //for (let i = 0; i < 25; i += 1) {
+  //  const div = document.createElement('div');
+  //  div.className = 'pixel';
+  //  document.querySelector('#pixel-board').appendChild(div);
+  //}
+  
   //  Cambiar o elemento que contem a class selected
   function mudarClass(event) {
     const corSelecionado = document.querySelector('.selected');
@@ -19,27 +27,58 @@ window.onload = function () {
     corSelecionado.classList.remove('selected');
     corNovoSelecionado.classList.add('selected');
   }
+
   //  colocar cor na palete
-  const quadrado = document.querySelector('#pixel-board');
+  let quadrado = document.querySelector('#pixel-board');
   let color = 'black';
   colocarCor(color);
+
   function colocarCor(color) {
     quadrado.addEventListener('click', function (event) {
       event.target.style.backgroundColor = color;
     });
   }
+
   //  Seleccionar cor
   palette.addEventListener('click', function (event) {
     mudarClass(event);
     color = event.target.style.backgroundColor;
     colocarCor(color);
   });
+
   //  Funcao botao apagar os cored do quadrado
   const botaoLimpar = document.querySelector('#clear-board');
-  const pixel = quadrado.children;
+  let board = document.querySelectorAll('.pixel');
   botaoLimpar.addEventListener('click', function () {
-    for (let i = 0; i < pixel.length; i += 1) {
-      pixel[i].style.backgroundColor = 'white';
+    for (let i = 0; i < board.length; i += 1) {
+      board[i].style.backgroundColor = 'white';
     }
   });
+
+  //  Botao fazer quadrado tamanho definido
+  const botaoGenerer = document.querySelector('#generate-board');
+  
+  botaoGenerer.addEventListener('click', function () {
+    //  Limpa os quadrados
+    let quadrado = document.querySelector('#pixel-board').children;
+    quadradoTr = quadrado.length;
+    for (let i = 0; i < quadradoTr; i += 1){
+      quadrado[0].remove();
+    }
+    let tamanhoQuadro = document.querySelector('#board-size').value;
+    criarQuadrado(tamanhoQuadro)
+  });
+
+
+  function criarQuadrado(tamanhoQuadro) {
+    for (let i = 0; i < tamanhoQuadro; i += 1) {
+      let tr = document.createElement('tr');
+      document.querySelector('#pixel-board').appendChild(tr);
+      for (let e = 0; e < tamanhoQuadro; e += 1) {
+        const td = document.createElement('td');
+        td.classList = ('pixel');
+        document.querySelectorAll('tr')[i].appendChild(td);
+      }
+    }
+  }
 };
