f7255af7b4a4d7911ae1ccdae7fda1b7d82172ac
Aurelio Scarante <aurelioscarante25@gmail.com>
2020-08-06 03:21:33 -0300

    Finalizando script.js

---
+++ script.js
@@ -0,0 +1,56 @@
+window.onload = function() {
+  let selectedColor = "black";
+  
+  let colorBlack = document.querySelector(".preto");
+  colorBlack.addEventListener("click", function(){
+      selectedColor = "black";
+      colorBlack.classList.add("selected");
+      colorBlue.classList.remove("selected");
+      colorYellow.classList.remove("selected");
+      colorGreen.classList.remove("selected");
+  })
+
+  let colorBlue = document.querySelector(".azul");
+  colorBlue.addEventListener("click", function(){
+      selectedColor = "blue";
+      colorBlack.classList.remove("selected");
+      colorBlue.classList.add("selected");
+      colorYellow.classList.remove("selected");
+      colorGreen.classList.remove("selected");
+  })
+
+  let colorYellow = document.querySelector(".amarelo");
+  colorYellow.addEventListener("click", function(){
+      selectedColor = "yellow";
+      colorBlack.classList.remove("selected");
+      colorBlue.classList.remove("selected");
+      colorYellow.classList.add("selected");
+      colorGreen.classList.remove("selected");
+  })
+
+  let colorGreen = document.querySelector(".verde");
+  colorGreen.addEventListener("click", function(){
+      selectedColor = "green";
+      colorBlack.classList.remove("selected");
+      colorBlue.classList.remove("selected");
+      colorYellow.classList.remove("selected");
+      colorGreen.classList.add("selected");
+  })
+
+  let clickPixel = document.querySelectorAll(".pixel");
+  
+  for (let i = 0; i < clickPixel.length; i += 1){
+      clickPixel[i].addEventListener("click", function(){
+          clickPixel[i].style.backgroundColor = selectedColor;
+      })
+  }
+
+  let buttonClear = document.querySelector(".btn");
+  buttonClear.addEventListener("click", function() {
+      for (let i = 0; i < clickPixel.length; i += 1){
+              clickPixel[i].style.backgroundColor = "white";
+          }
+      })
+  
+  
+}
