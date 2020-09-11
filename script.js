6cecacd4f91e261a4fb830aba8bb29dd65798737
Lais Gurgel <laisgurgelf@gmail.com>
2020-09-11 12:56:38 -0300

    Codeclimate

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 window.onload = function () {
-    const colorPalette = document.querySelector('#color-palette');
-    const pixelsBoard = document.getElementById('pixel-board');
-    const clearButton = document.getElementById('clear-board');
-  
-  
-    function changeClass(event) {
-      document.querySelector('.selected').classList.remove('selected');
-      event.target.classList.add('selected');
+  const colorPalette = document.querySelector('#color-palette');
+  const pixelsBoard = document.getElementById('pixel-board');
+  const clearButton = document.getElementById('clear-board');
+
+
+  function changeClass(event) {
+    document.querySelector('.selected').classList.remove('selected');
+    event.target.classList.add('selected');
+  }
+  colorPalette.addEventListener('click', changeClass);
+
+  function pixelColoring(event) {
+    const selectedColor = document.querySelector('.selected').style.backgroundColor;
+    event.target.style.backgroundColor = selectedColor;
+  }
+  pixelsBoard.addEventListener('click', pixelColoring);
+
+  function clearBoard() {
+    const pixels = document.querySelectorAll('.pixel');
+    for (let i = 0; i < pixels.length; i += 1) {
+      pixels[i].style.backgroundColor = '';
     }
-    colorPalette.addEventListener('click', changeClass);
-  
-    function pixelColoring(event) {
-      const selectedColor = document.querySelector('.selected').style.backgroundColor;
-      event.target.style.backgroundColor = selectedColor;
-    }
-    pixelsBoard.addEventListener('click', pixelColoring);
-  
-    function clearBoard() {
-      const pixels = document.querySelectorAll('.pixel');
-      for (let i = 0; i < pixels.length; i += 1) {
-        pixels[i].style.backgroundColor = '';
-      }
-    }
-    clearButton.addEventListener('click', clearBoard);
-  
+  }
+  clearButton.addEventListener('click', clearBoard);
 };
